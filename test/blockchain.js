678ed4e7d0995a987b75a84584f663e1a6034b7c
Brenton Gunning <brentongunning@gmail.com>
2020-01-01 15:55:24 -0800

    Update test names in blockchain.js

--- test/blockchain.js
+++ test/blockchain.js
@@ -15,23 +15,23 @@ const { Api } = Run
 
 describe('Blockchain', () => {
   describe('constructor', () => {
-    it('return mainnet blockchain', () => {
+    it('should return mainnet blockchain for main network', () => {
       expect(createRun({ network: 'main' }).blockchain).not.to.equal(undefined)
     })
 
-    it('return testnet blockchain', () => {
+    it('should return testnet blockchain for test network', () => {
       expect(createRun({ network: 'test' }).blockchain).not.to.equal(undefined)
     })
 
-    it('return scaling blockchain', () => {
+    it('should return scaling blockchain for stn network', () => {
       expect(createRun({ network: 'stn' }).blockchain).not.to.equal(undefined)
     })
 
-    it('return mock blockchain', () => {
+    it('should return mock blockchain for mock network', () => {
       expect(createRun({ network: 'mock' }).blockchain).not.to.equal(undefined)
     })
 
-    it('throw if bad network', () => {
+    it('should throw for bad network', () => {
       expect(() => createRun({ network: 'bitcoin' })).to.throw()
     })
   })
@@ -42,43 +42,43 @@ function runBlockchainTestSuite (blockchain, privateKey, sampleTx,
   const address = privateKey.toAddress().toString()
 
   describe('broadcast', () => {
-    it('send to self', async () => {
+    it('should support sending to self', async () => {
       const utxos = (await blockchain.utxos(address)).slice(0, 1)
       const tx = new Transaction().from(utxos).change(address).fee(250).sign(privateKey)
       await blockchain.broadcast(tx)
     })
 
-    it('throw if missing input', async () => {
+    it('should throw if missing input', async () => {
       const utxos = await blockchain.utxos(address)
       const utxo = { ...utxos[0], vout: 999 }
       const tx = new Transaction().from(utxo).change(address).fee(250).sign(privateKey)
       await expect(blockchain.broadcast(tx)).to.be.rejectedWith(errors.missingInput)
     })
 
-    it('throw if no inputs', async () => {
+    it('should throw if no inputs', async () => {
       const tx = new Transaction().to(address, 100)
       await expect(blockchain.broadcast(tx)).to.be.rejectedWith(errors.noInputs)
     })
 
-    it('throw if no outputs', async () => {
+    it('should throw if no outputs', async () => {
       const utxos = await blockchain.utxos(address)
       const tx = new Transaction().from(utxos).sign(privateKey)
       await expect(blockchain.broadcast(tx)).to.be.rejectedWith(errors.noOutputs)
     })
 
-    it('throw if fee too low', async () => {
+    it('should throw if fee too low', async () => {
       const utxos = await blockchain.utxos(address)
       const tx = new Transaction().from(utxos).change(address).fee(0).sign(privateKey)
       await expect(blockchain.broadcast(tx)).to.be.rejectedWith(errors.feeTooLow)
     })
 
-    it('throw if not signed', async () => {
+    it('should throw if not signed', async () => {
       const utxos = await blockchain.utxos(address)
       const tx = new Transaction().from(utxos).change(address).fee(250)
       await expect(blockchain.broadcast(tx)).to.be.rejectedWith(errors.notFullySigned)
     })
 
-    it('throw if duplicate input', async () => {
+    it('should throw if duplicate input', async () => {
       const utxos = await blockchain.utxos(address)
       const tx = new Transaction().from(utxos).from(utxos).change(address).sign(privateKey)
       await expect(blockchain.broadcast(tx)).to.be.rejectedWith(errors.duplicateInput)
@@ -86,31 +86,31 @@ function runBlockchainTestSuite (blockchain, privateKey, sampleTx,
   })
 
   describe('fetch', () => {
-    it('return transaction', async () => {
+    it('should get pre-existing transaction', async () => {
       const tx = await blockchain.fetch(sampleTx.txid)
       expect(tx.hash).to.equal(sampleTx.txid)
     })
 
-    it('has time', async () => {
+    it('should set time', async () => {
       const tx = await blockchain.fetch(sampleTx.txid)
       expect(tx.time).not.to.equal(undefined)
       expect(tx.time > new Date('January 3, 2009')).to.equal(true)
       expect(tx.time <= Date.now()).to.equal(true)
     })
 
-    it('caches repeated calls', async () => {
+    it('should cache repeated calls', async () => {
       const requests = []
       for (let i = 0; i < 100; i++) requests.push(blockchain.fetch(sampleTx.txid))
       await Promise.all(requests)
     })
 
-    it('throw if nonexistant', async () => {
+    it('should throw if nonexistant', async () => {
       const bad = '0000000000000000000000000000000000000000000000000000000000000000'
       const requests = [bad, bad, bad].map(txid => blockchain.fetch(txid))
       await expect(Promise.all(requests)).to.be.rejectedWith()
     })
 
-    it('in mempool and unspent', async () => {
+    it('should set spent information for transaction in mempool and unspent', async () => {
       const utxos = await blockchain.utxos(address)
       const tx = new Transaction().from(utxos).change(address).sign(privateKey)
       await blockchain.broadcast(tx)
@@ -138,7 +138,7 @@ function runBlockchainTestSuite (blockchain, privateKey, sampleTx,
       expect(tx3.confirmations).to.equal(0)
     })
 
-    it('in mempool and spent', async () => {
+    it('should set spent information for transaction in mempool and spent', async () => {
       const utxos = await blockchain.utxos(address)
       const tx = new Transaction().from(utxos).change(address).sign(privateKey)
       await blockchain.broadcast(tx)
@@ -159,7 +159,7 @@ function runBlockchainTestSuite (blockchain, privateKey, sampleTx,
       }
     })
 
-    it('in block and spent', async () => {
+    it('should set spent information for transaction in block and spent', async () => {
       const tx = await blockchain.fetch(sampleTx.txid)
       for (let i = 0; i < sampleTx.vout.length; i++) {
         if (supportsSpentTxIdInBlocks) {
@@ -182,7 +182,7 @@ function runBlockchainTestSuite (blockchain, privateKey, sampleTx,
   })
 
   describe('utxos', () => {
-    it('return utxos if some', async () => {
+    it('should return utxos', async () => {
       const utxos = await blockchain.utxos(address)
       expect(utxos.length > 0).to.equal(true)
       expect(utxos[0].txid).not.to.equal(undefined)
@@ -191,13 +191,13 @@ function runBlockchainTestSuite (blockchain, privateKey, sampleTx,
       expect(utxos[0].satoshis).not.to.equal(undefined)
     })
 
-    it('return empty list if none', async () => {
+    it('should return empty list if no utxos', async () => {
       const address = new PrivateKey(privateKey.network).toAddress()
       const utxos = await blockchain.utxos(address)
       expect(utxos.length).to.equal(0)
     })
 
-    it('dont return spent outputs', async () => {
+    it('should not return spent outputs', async () => {
       const prevUtxos = await blockchain.utxos(address)
       const tx = new Transaction().from(prevUtxos[0]).change(address).fee(250).sign(privateKey)
       await blockchain.broadcast(tx)
@@ -207,13 +207,13 @@ function runBlockchainTestSuite (blockchain, privateKey, sampleTx,
       expect(utxos[prevUtxos.length - 1].vout).to.equal(0)
     })
 
-    it('caches repeated calls', async () => {
+    it('should cache repeated calls', async () => {
       const requests = []
       for (let i = 0; i < 100; i++) requests.push(blockchain.utxos(address))
       await Promise.all(requests)
     })
 
-    it('throws for invalid address', async () => {
+    it('should throw for invalid address', async () => {
       const requests = ['123', '123', '123'].map(addr => blockchain.utxos(addr))
       await expect(Promise.all(requests)).to.be.rejectedWith()
     })
