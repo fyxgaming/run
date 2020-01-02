e2e3163a037d0f3c7f106e867c37e87b012d5da4
Brenton Gunning <brentongunning@gmail.com>
2020-01-01 17:12:02 -0800

    Update test names in expect.js

--- test/expect.js
+++ test/expect.js
@@ -13,7 +13,7 @@ describe('expect', () => {
   beforeEach(() => run.activate())
   beforeEach(() => run.blockchain.block())
 
-  it('toBe', () => {
+  it('should support toBe', () => {
     expect(() => Run.expect(1).toBe(1)).not.to.throw()
     expect(() => Run.expect('hello').toBe('hello')).not.to.throw()
     expect(() => Run.expect(null).toBe(null)).not.to.throw()
@@ -23,7 +23,7 @@ describe('expect', () => {
     expect(() => Run.expect(null).not.toBe(null)).to.throw('expected value not to be null but was null')
   })
 
-  it('toEqual', () => {
+  it('should support toEqual', () => {
     expect(() => Run.expect(1).toEqual(1)).not.to.throw()
     expect(() => Run.expect(true).toEqual(true)).not.to.throw()
     expect(() => Run.expect({}).toEqual({})).not.to.throw()
@@ -35,7 +35,7 @@ describe('expect', () => {
     expect(() => Run.expect(new class A {}()).not.toEqual({ })).to.throw('expected value not to be equal to {} but was {}')
   })
 
-  it('toBeInstanceOf', () => {
+  it('should support toBeInstanceOf', () => {
     class A {}
     class B extends A {}
     expect(() => Run.expect(new A()).toBeInstanceOf(A)).not.to.throw()
@@ -47,7 +47,7 @@ describe('expect', () => {
     expect(() => Run.expect(new A()).not.toBeInstanceOf(A)).to.throw('expected value not to be an instance of A but was {}')
   })
 
-  it('toBeDefined', () => {
+  it('should support toBeDefined', () => {
     expect(() => Run.expect(1).toBeDefined()).not.to.throw()
     expect(() => Run.expect(undefined).toBeDefined()).to.throw('expected value to be defined but was undefined')
     expect(() => Run.expect().not.toBeDefined()).not.to.throw()
@@ -55,14 +55,14 @@ describe('expect', () => {
     expect(() => Run.expect(0).not.toBeDefined()).to.throw('expected value not to be defined but was 0')
   })
 
-  it('toBeNull', () => {
+  it('should support toBeNull', () => {
     expect(() => Run.expect(null).toBeNull()).not.to.throw()
     expect(() => Run.expect(0).toBeNull()).to.throw('expected value to be null but was 0')
     expect(() => Run.expect(false).not.toBeNull()).not.to.throw()
     expect(() => Run.expect(null).not.toBeNull()).to.throw('expected value not to be null but was null')
   })
 
-  it('toBeNumber', () => {
+  it('should support toBeNumber', () => {
     expect(() => Run.expect(0).toBeNumber()).not.to.throw()
     expect(() => Run.expect(5).toBeNumber()).not.to.throw()
     expect(() => Run.expect(1.1).toBeNumber()).not.to.throw()
@@ -74,7 +74,7 @@ describe('expect', () => {
     expect(() => Run.expect(5).not.toBeNumber()).to.throw('expected value not to be a number but was 5')
   })
 
-  it('toBeInteger', () => {
+  it('should support toBeInteger', () => {
     expect(() => Run.expect(0).toBeInteger()).not.to.throw()
     expect(() => Run.expect(1).toBeInteger()).not.to.throw()
     expect(() => Run.expect(1.1).toBeInteger()).to.throw('expected value to be an integer but was 1.1')
@@ -84,7 +84,7 @@ describe('expect', () => {
     expect(() => Run.expect(5).not.toBeInteger()).to.throw('expected value not to be an integer but was 5')
   })
 
-  it('toBeLessThan', () => {
+  it('should support toBeLessThan', () => {
     expect(() => Run.expect(0).toBeLessThan(1)).not.to.throw()
     expect(() => Run.expect(-1.2).toBeLessThan(-1.1)).not.to.throw()
     expect(() => Run.expect(false).toBeLessThan(0)).to.throw('expected value to be less than 0 but was false')
@@ -92,7 +92,7 @@ describe('expect', () => {
     expect(() => Run.expect(-1).not.toBeLessThan(0)).to.throw('expected value not to be less than 0 but was -1')
   })
 
-  it('toBeLessThanOrEqualTo', () => {
+  it('should support toBeLessThanOrEqualTo', () => {
     expect(() => Run.expect(1).toBeLessThanOrEqualTo(1)).not.to.throw()
     expect(() => Run.expect(-1.2).toBeLessThanOrEqualTo(-1.1)).not.to.throw()
     expect(() => Run.expect(false).toBeLessThanOrEqualTo(0)).to.throw('expected value to be less than or equal to 0 but was false')
@@ -100,7 +100,7 @@ describe('expect', () => {
     expect(() => Run.expect(0).not.toBeLessThanOrEqualTo(0)).to.throw('expected value not to be less than or equal to 0 but was 0')
   })
 
-  it('toBeGreaterThan', () => {
+  it('should support toBeGreaterThan', () => {
     expect(() => Run.expect(1).toBeGreaterThan(0)).not.to.throw()
     expect(() => Run.expect(-1.1).toBeGreaterThan(-1.2)).not.to.throw()
     expect(() => Run.expect(false).toBeGreaterThan(0)).to.throw('expected value to be greater than 0 but was false')
@@ -108,7 +108,7 @@ describe('expect', () => {
     expect(() => Run.expect(0).not.toBeGreaterThan(-1)).to.throw('expected value not to be greater than -1 but was 0')
   })
 
-  it('toBeGreaterThanOrEqualTo', () => {
+  it('should support toBeGreaterThanOrEqualTo', () => {
     expect(() => Run.expect(1).toBeGreaterThanOrEqualTo(1)).not.to.throw()
     expect(() => Run.expect(-1.1).toBeGreaterThanOrEqualTo(-1.2)).not.to.throw()
     expect(() => Run.expect(false).toBeGreaterThanOrEqualTo(0)).to.throw('expected value to be greater than or equal to 0 but was false')
@@ -116,21 +116,21 @@ describe('expect', () => {
     expect(() => Run.expect(0).not.toBeGreaterThanOrEqualTo(0)).to.throw('expected value not to be greater than or equal to 0 but was 0')
   })
 
-  it('toBeBoolean', () => {
+  it('should support toBeBoolean', () => {
     expect(() => Run.expect(true).toBeBoolean()).not.to.throw()
     expect(() => Run.expect(1).toBeBoolean()).to.throw('expected value to be a boolean but was 1')
     expect(() => Run.expect('true').not.toBeBoolean()).not.to.throw()
     expect(() => Run.expect(false).not.toBeBoolean()).to.throw('expected value not to be a boolean but was false')
   })
 
-  it('toBeString', () => {
+  it('should support toBeString', () => {
     expect(() => Run.expect('hello').toBeString()).not.to.throw()
     expect(() => Run.expect(true).toBeString()).to.throw('expected value to be a string but was true')
     expect(() => Run.expect(1).not.toBeString()).not.to.throw()
     expect(() => Run.expect('hello').not.toBeString()).to.throw('expected value not to be a string but was hello')
   })
 
-  it('toBeObject', () => {
+  it('should support toBeObject', () => {
     expect(() => Run.expect({}).toBeObject()).not.to.throw()
     expect(() => Run.expect([1, 2, 3]).toBeObject()).not.to.throw()
     expect(() => Run.expect(null).toBeObject()).to.throw('expected value to be an object but was null')
@@ -140,7 +140,7 @@ describe('expect', () => {
     expect(() => Run.expect({}).not.toBeObject()).to.throw('expected value not to be an object but was {}')
   })
 
-  it('toBeArray', () => {
+  it('should support toBeArray', () => {
     expect(() => Run.expect([]).toBeArray()).not.to.throw()
     expect(() => Run.expect(new Array(1)).toBeArray()).not.to.throw()
     expect(() => Run.expect({}).toBeArray()).to.throw('expected value to be an array but was {}')
@@ -149,7 +149,7 @@ describe('expect', () => {
     expect(() => Run.expect([1, 2]).not.toBeArray()).to.throw('expected value not to be an array but was [1,2]')
   })
 
-  it('toBeClass', () => {
+  it('should support toBeClass', () => {
     expect(() => Run.expect(class A {}).toBeClass()).not.to.throw()
     expect(() => Run.expect(class {}).toBeClass()).not.to.throw()
     expect(() => Run.expect(function f () {}).toBeClass()).to.throw('expected value to be a class but was')
@@ -158,7 +158,7 @@ describe('expect', () => {
     expect(() => Run.expect(class A {}).not.toBeClass()).to.throw('expected value not to be a class but was')
   })
 
-  it('toBeFunction', () => {
+  it('should support toBeFunction', () => {
     expect(() => Run.expect(function f () {}).toBeFunction()).not.to.throw()
     expect(() => Run.expect(() => {}).toBeFunction()).not.to.throw()
     expect(() => Run.expect(class A {}).toBeFunction()).to.throw('expected value to be a function but was class A {}')
@@ -167,7 +167,7 @@ describe('expect', () => {
     expect(() => Run.expect(() => {}).not.toBeFunction()).to.throw('expected value not to be a function but was () => {}')
   })
 
-  it.skip('deploy', async () => {
+  it.skip('should deploy', async () => {
     await deploy(Run.expect)
   })
 })
