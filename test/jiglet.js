d39d6781a0e75f9e063291731462814a957baed9
Brenton Gunning <brentongunning@gmail.com>
2020-02-06 00:43:02 -0800

    Load jiglets

--- test/jiglet.js
+++ test/jiglet.js
@@ -2,14 +2,20 @@ const { describe, it } = require('mocha')
 const { createRun, Run } = require('./helpers')
 const { Jiglet } = Run
 
-class TwetchPost extends Jiglet { }
+class TwetchPost extends Jiglet {
+  init (location, text) {
+    this.location = location
+    this.text = text
+  }
+}
 
 class TwetchLoader {
   static async load (location, blockchain) {
     const txid = location.slice(0, 64)
     const tx = await blockchain.fetch(txid)
-    console.log(JSON.stringify(tx.outputs[0]))
-    return new TwetchPost()
+    const BProtocolIdentifier = '19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut'
+    if (tx.outputs[0].script.chunks[2].buf.toString('utf8') !== BProtocolIdentifier) return
+    return new TwetchPost(location, tx.outputs[0].script.chunks[3].buf.toString('utf8'))
   }
 }
 
@@ -22,8 +28,9 @@ Run.protocol.install(TwetchLoader)
 
 describe('Jiglet', () => {
   it('should load a twetch post', async () => {
-    console.log('hello')
     const post = await run.load('b446cb6e6187e79f95bc85df7d0e8332873f055d6b63bc29c049584917cceda0_o0')
     console.log(post)
+    // console.log(JSON.stringify(tx.outputs[0].script.toString('utf8')))
+    // console.log(JSON.stringify(tx.outputs[0]._scriptBuffer.toString('utf8')))
   })
 })
