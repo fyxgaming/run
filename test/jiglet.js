f1fb9234a5b24d3edf9011fdc16d5e15a5580cab
Brenton Gunning <brentongunning@gmail.com>
2020-02-06 03:16:03 -0800

    Serialize Jiglets

--- test/jiglet.js
+++ test/jiglet.js
@@ -1,6 +1,6 @@
 const { describe, it } = require('mocha')
 const { createRun, Run } = require('./helpers')
-const { Jiglet } = Run
+const { Jig, Jiglet } = Run
 
 class TwetchPost extends Jiglet {
   init (location, text) {
@@ -41,7 +41,30 @@ Run.protocol.install(TwetchLoader)
 
 describe('Jiglet', () => {
   it('should load a twetch post', async () => {
+    class Favorite extends Jig {
+      init (post) {
+        this.post = post
+      }
+    }
+
     const post = await run.load('b446cb6e6187e79f95bc85df7d0e8332873f055d6b63bc29c049584917cceda0_o0')
+
+    console.log('------------')
     console.log(post)
+    console.log('------------')
+
+    const favorite = new Favorite(post)
+
+    await favorite.sync()
+
+    console.log('------------')
+    console.log(favorite)
+    console.log('------------')
+
+    const favorite2 = await run.load('8a0c1220c75e852f98c4ab1e313d1f0e90fc98499039a8db5a54669eceeca0c1_o2')
+
+    console.log('------------')
+    console.log(favorite2)
+    console.log('------------')
   })
 })
