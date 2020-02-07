4a4bda7c54ed0941cbe31f4eceb045062396490a
Brenton Gunning <brentongunning@gmail.com>
2020-02-06 16:10:59 -0800

    Fix loading jiglet

--- test/jiglet.js
+++ test/jiglet.js
@@ -63,7 +63,7 @@ describe('Jiglet', () => {
     console.log(favorite)
     console.log('------------')
 
-    const favorite2 = await run.load(favorite.location)
+    // const favorite2 = await run.load(favorite.location)
     */
     const favorite2 = await run.load('140a35a53523eb5538665847ecb4e1e8817e8dae2988cc2ba19e6da017f65c0c_o2')
 
