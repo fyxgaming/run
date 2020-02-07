ece5d60459f262d38f278bad823d9579f246556e
Brenton Gunning <brentongunning@gmail.com>
2020-02-07 15:57:07 -0800

    Make more readable

--- test/berry.js
+++ test/berry.js
@@ -57,13 +57,27 @@ describe('Berry', () => {
     await run.load(favorite.location)
   }).timeout(10000)
 
-  it.only('should load post without protocol from blockchain', async () => {
+  it('should load favorite without protocol from blockchain', async () => {
     const location = '59925b22090824f1573a6ef650249d7b1730ce519a5e88594730fc0d50faedd9_o2'
     const favorite = await run.load(location)
     console.log(favorite)
   })
 
-  // Move the protocol manager inside of Run
-  // Try loading a long-form location, with a specific protocol
-  // Test subloads
+  it.only('should load post with protocol from blockchain', async () => {
+    const location = '1e9c751bfa14e2f8c7f980ac005932224f3644273265e2740ccb0c0245acb18b_o1://b446cb6e6187e79f95bc85df7d0e8332873f055d6b63bc29c049584917cceda0'
+    const post = await run.load(location)
+    console.log(post)
+  })
+
+  it('should support loading berries from berries', () => {
+    // TODO
+  })
+
+  it('should support multiple fetches per berry', () => {
+    // TODO
+  })
+
+  it('should throw if pluck more than one berry', () => {
+    // TODO
+  })
 })
