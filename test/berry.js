85725853399da2e26839e54b61d5e5b7a1ac64b3
Brenton Gunning <brentongunning@gmail.com>
2020-02-07 15:54:24 -0800

    Load post from blockchain

--- test/berry.js
+++ test/berry.js
@@ -1,4 +1,8 @@
 const { describe, it } = require('mocha')
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+// const { expect } = chai
 const { createRun, Run } = require('./helpers')
 const { Jig, Berry } = Run
 
@@ -28,39 +32,35 @@ Post.protocol = Twetch
 
 const run = createRun({ network: 'main' })
 
-describe('Berry', () => {
-  it('should deploy and load a twetch post', async () => {
-    /*
-    class Favorite extends Jig {
-      init (post) {
-        this.post = post
-      }
-    }
+class Favorite extends Jig {
+  init (post) {
+    this.post = post
+  }
+}
 
+describe('Berry', () => {
+  it('should fail to deploy if protocol is undeployed', async () => {
+    const txid = 'b446cb6e6187e79f95bc85df7d0e8332873f055d6b63bc29c049584917cceda0'
+    const post = await run.load(txid, { protocol: Twetch })
+    const favorite = new Favorite(post)
+    await favorite.sync()
+  })
+
+  it('should deploy and load a twetch post', async () => {
     await run.deploy(Twetch)
     const txid = 'b446cb6e6187e79f95bc85df7d0e8332873f055d6b63bc29c049584917cceda0'
     const post = await run.load(txid, { protocol: Twetch })
-    console.log(post)
-
-    // console.log(Favorite.name)
     const favorite = new Favorite(post)
     await favorite.sync()
-    // run.state.cache.clear()
-    console.log(favorite.location)
-    const favorite2 = await run.load(favorite.location)
-    console.log(favorite2)
-    */
+    await run.load(favorite.location)
+    run.state.cache.clear()
+    await run.load(favorite.location)
   }).timeout(10000)
 
-  it('should fail to deploy if protocol is undeployed', () => {
-
-  })
-
-  it('should load post without protocol from blockchain', async () => {
+  it.only('should load post without protocol from blockchain', async () => {
     const location = '59925b22090824f1573a6ef650249d7b1730ce519a5e88594730fc0d50faedd9_o2'
     const favorite = await run.load(location)
     console.log(favorite)
-    console.log(Jig)
   })
 
   // Move the protocol manager inside of Run
