7145efc80a3d725ffbc96368f6b9443b27412630
Brenton Gunning <brentongunning@gmail.com>
2020-03-19 14:05:14 -0700

    Finish mockchain perf tests

--- test/module/mockchain.js
+++ test/module/mockchain.js
@@ -5,7 +5,7 @@
  */
 
 const { PrivateKey, Transaction, Script, crypto } = require('bsv')
-const { describe, it, before, beforeEach } = require('mocha')
+const { describe, it } = require('mocha')
 const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
 chai.use(chaiAsPromised)
@@ -17,12 +17,12 @@ const { Mockchain } = Run.module
 // Globals
 // ------------------------------------------------------------------------------------------------
 
-  const mockchain = new Mockchain()
-  const privkey = new PrivateKey('testnet')
-  const address = privkey.toAddress()
-  const script = Script.fromAddress(address).toBuffer()
-  const scriptHash = crypto.Hash.sha256(script).toString('hex')
-  mockchain.fund(address, 100000000)
+const mockchain = new Mockchain()
+const privkey = new PrivateKey('testnet')
+const address = privkey.toAddress()
+const script = Script.fromAddress(address).toBuffer()
+const scriptHash = crypto.Hash.sha256(script).toString('hex')
+mockchain.fund(address, 100000000)
 
 // ------------------------------------------------------------------------------------------------
 // Mockchain Functional Tests
@@ -97,28 +97,26 @@ if (perf) {
       expect(start < 10).to.equal(true)
       expect(end < 10).to.equal(true)
     }).timeout(30000)
-  })
-}
-
-/*
 
     it('should support fast utxo queries', async () => {
       // Generate 10 private keys and fund their addresses
-      const privateKeys = []; const addresses = []
-      for (let i = 0; i < 10; i++) { privateKeys.push(new PrivateKey()) }
-      privateKeys.forEach(privateKey => addresses.push(privateKey.toAddress()))
-      addresses.forEach(address => run.blockchain.fund(address, 100000))
+      const privateKeys = []
+      for (let i = 0; i < 10; i++) { privateKeys.push(new PrivateKey('testnet')) }
+      const addresses = privateKeys.map(privateKey => privateKey.toAddress())
+      const scriptHashes = addresses.map(address => Script.fromAddress(address))
+        .map(script => crypto.Hash.sha256(script.toBuffer()).toString('hex'))
+      addresses.forEach(address => mockchain.fund(address, 100000))
 
       // Send from each address to the next, 1000 times
       const measures = []
       for (let i = 0; i < 1000; i++) {
         const before = new Date()
-        const utxos = await run.blockchain.utxos(addresses[i % 10])
+        const utxos = await mockchain.utxos(scriptHashes[i % 10])
         measures.push(new Date() - before)
         const tx = new Transaction().from(utxos).to(addresses[(i + 1) % 10], 1000)
           .change(addresses[i % 10]).sign(privateKeys[i % 10])
-        await run.blockchain.broadcast(tx)
-        run.blockchain.block()
+        await mockchain.broadcast(tx)
+        mockchain.block()
       }
 
       // Get an average time to query utxos() at the start and end, and check it didn't change much
@@ -129,4 +127,3 @@ if (perf) {
     }).timeout(30000)
   })
 }
-*/
