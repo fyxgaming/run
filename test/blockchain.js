f433f0c77bf95e3f298ffb202a1674fa5ba228e3
Brenton Gunning <brentongunning@gmail.com>
2020-03-21 14:29:25 -0700

    WIP blockchain tests

--- test/blockchain.js
+++ test/blockchain.js
@@ -4,17 +4,18 @@
  * Universal blockchain API test suite
  */
 
-// const bsv = require('bsv')
+const bsv = require('bsv')
 const { describe, it } = require('mocha')
-// const { Run } = require('./config')
+const { Run } = require('./config')
 
-// const run = new Run()
+const { blockchain } = new Run()
 
 describe('Blockchain', () => {
   describe('broadcast', () => {
     it('should support sending to self', async () => {
-    //   const tx = await payFor(new bsv.Transaction(), privateKey, blockchain)
-    //   await blockchain.broadcast(tx)
+      // const tx = await payFor(new bsv.Transaction(), privateKey, blockchain)
+      const tx = new bsv.Transaction()
+      await blockchain.broadcast(tx)
     })
   })
 })
