78f450bbf62506f09a8d4d431606b2f281abd1d5
Brenton Gunning <brentongunning@gmail.com>
2020-03-21 22:19:06 -0700

    Copy remaining blockchain suite tests

--- test/kernel/blockchain.js
+++ test/kernel/blockchain.js
@@ -94,6 +94,154 @@ describe('Blockchain', () => {
       await expect(blockchain.broadcast(tx)).to.be.rejectedWith(errors.duplicateInput)
     })
   })
+
+  /*
+  describe('fetch', () => {
+    it('should get pre-existing transaction', async () => {
+      const tx = await blockchain.fetch(sampleTx.txid)
+      expect(tx.hash).to.equal(sampleTx.txid)
+    })
+
+    it('should set time', async () => {
+      const tx = await blockchain.fetch(sampleTx.txid)
+      expect(tx.time).not.to.equal(undefined)
+      expect(tx.time > new Date('January 3, 2009')).to.equal(true)
+      expect(tx.time <= Date.now()).to.equal(true)
+    })
+
+    it('should cache repeated calls', async () => {
+      const requests = []
+      for (let i = 0; i < 100; i++) requests.push(blockchain.fetch(sampleTx.txid))
+      await Promise.all(requests)
+    })
+
+    it('should throw if nonexistant', async () => {
+      const bad = '0000000000000000000000000000000000000000000000000000000000000000'
+      const requests = [bad, bad, bad].map(txid => blockchain.fetch(txid))
+      await expect(Promise.all(requests)).to.be.rejectedWith()
+    })
+
+    it('should set spent information for transaction in mempool and unspent', async () => {
+      const tx = await payFor(new bsv.Transaction(), privateKey, blockchain)
+      await blockchain.broadcast(tx)
+      function sleep (ms) { return new Promise(resolve => setTimeout(resolve, ms)) }
+      const tx2 = await blockchain.fetch(tx.hash)
+      // check the cached copy
+      expect(tx2.outputs[0].spentTxId).to.equal(null)
+      expect(tx2.outputs[0].spentIndex).to.equal(null)
+      expect(tx2.outputs[0].spentHeight).to.equal(null)
+      // check the uncached copy
+      if (blockchain instanceof BlockchainServer) {
+        await sleep(indexingLatency)
+        blockchain.cache.transactions.clear()
+      }
+      const tx3 = await blockchain.fetch(tx.hash)
+      if (supportsSpentTxIdInMempool) {
+        expect(tx3.outputs[0].spentTxId).to.equal(null)
+        expect(tx3.outputs[0].spentIndex).to.equal(null)
+        expect(tx3.outputs[0].spentHeight).to.equal(null)
+      } else {
+        expect(tx3.outputs[0].spentTxId).to.equal(undefined)
+        expect(tx3.outputs[0].spentIndex).to.equal(undefined)
+        expect(tx3.outputs[0].spentHeight).to.equal(undefined)
+      }
+      expect(tx3.confirmations).to.equal(0)
+    })
+
+    it('should set spent information for transaction in mempool and spent', async () => {
+      const tx = await payFor(new bsv.Transaction(), privateKey, blockchain)
+      await blockchain.broadcast(tx)
+      function sleep (ms) { return new Promise(resolve => setTimeout(resolve, ms)) }
+      if (blockchain instanceof BlockchainServer) {
+        await sleep(indexingLatency)
+        blockchain.cache.transactions.clear()
+      }
+      const firstInput = tx.inputs[0]
+      const prev = await blockchain.fetch(firstInput.prevTxId.toString('hex'))
+      if (supportsSpentTxIdInMempool) {
+        expect(prev.outputs[firstInput.outputIndex].spentTxId).to.equal(tx.hash)
+        expect(prev.outputs[firstInput.outputIndex].spentIndex).to.equal(0)
+        expect(prev.outputs[firstInput.outputIndex].spentHeight).to.equal(-1)
+      } else {
+        expect(prev.outputs[firstInput.outputIndex].spentTxId).to.equal(undefined)
+        expect(prev.outputs[firstInput.outputIndex].spentIndex).to.equal(undefined)
+        expect(prev.outputs[firstInput.outputIndex].spentHeight).to.equal(undefined)
+      }
+    })
+
+    it('should set spent information for transaction in block and spent', async () => {
+      const tx = await blockchain.fetch(sampleTx.txid)
+      for (let i = 0; i < sampleTx.vout.length; i++) {
+        if (supportsSpentTxIdInBlocks) {
+          expect(tx.outputs[i].spentTxId).to.equal(sampleTx.vout[i].spentTxId)
+          expect(tx.outputs[i].spentIndex).to.equal(sampleTx.vout[i].spentIndex)
+          expect(tx.outputs[i].spentHeight).to.equal(sampleTx.vout[i].spentHeight)
+        } else {
+          expect(tx.outputs[0].spentTxId).to.equal(undefined)
+          expect(tx.outputs[0].spentIndex).to.equal(undefined)
+          expect(tx.outputs[0].spentHeight).to.equal(undefined)
+        }
+      }
+      expect(tx.time).to.equal(sampleTx.time)
+      if (sampleTx.blockhash) {
+        expect(tx.blockhash).to.equal(sampleTx.blockhash)
+        expect(tx.blocktime).to.equal(sampleTx.blocktime)
+        expect(tx.confirmations > sampleTx.minConfirmations).to.equal(true)
+      }
+    })
+
+    it('should keep spent info when force fetch', async () => {
+      const privateKey2 = new bsv.PrivateKey(privateKey.network)
+      const address2 = privateKey2.toAddress()
+      const tx1 = await payFor(new bsv.Transaction().to(address2, 1000).sign(privateKey), privateKey, blockchain)
+      await blockchain.broadcast(tx1)
+      const utxo = { txid: tx1.hash, vout: 0, script: tx1.outputs[0].script, satoshis: 1000 }
+      const tx2 = (await payFor(new bsv.Transaction().from(utxo), privateKey, blockchain)).sign(privateKey2)
+      await blockchain.broadcast(tx2)
+      const tx1b = await blockchain.fetch(tx1.hash, true)
+      expect(tx1b.outputs[0].spentTxId).to.equal(tx1.outputs[0].spentTxId)
+      expect(tx1b.outputs[0].spentIndex).to.equal(tx1.outputs[0].spentIndex)
+      expect(tx1b.outputs[0].spentHeight).to.equal(tx1.outputs[0].spentHeight)
+    })
+  })
+
+  describe('utxos', () => {
+    it('should return utxos', async () => {
+      const utxos = await blockchain.utxos(address)
+      expect(utxos.length > 0).to.equal(true)
+      expect(utxos[0].txid).not.to.equal(undefined)
+      expect(utxos[0].vout).not.to.equal(undefined)
+      expect(utxos[0].script).not.to.equal(undefined)
+      expect(utxos[0].satoshis).not.to.equal(undefined)
+    })
+
+    it('should return empty list if no utxos', async () => {
+      const address = new bsv.PrivateKey(privateKey.network).toAddress()
+      const utxos = await blockchain.utxos(address)
+      expect(utxos.length).to.equal(0)
+    })
+
+    it('should not return spent outputs', async () => {
+      const tx = await payFor(new bsv.Transaction(), privateKey, blockchain)
+      await blockchain.broadcast(tx)
+      const utxos = await blockchain.utxos(address)
+      expect(utxos.some(utxo => utxo.txid === tx.inputs[0].prevTxId.toString() &&
+        utxo.vout === tx.inputs[0].outputIndex)).to.equal(false)
+      expect(utxos.some(utxo => utxo.txid === tx.hash && utxo.vout === 0)).to.equal(true)
+    })
+
+    it('should cache repeated calls', async () => {
+      const requests = []
+      for (let i = 0; i < 100; i++) requests.push(blockchain.utxos(address))
+      await Promise.all(requests)
+    })
+
+    it('should throw for invalid script hash', async () => {
+      const requests = ['z', '%', []].map(addr => blockchain.utxos(addr))
+      await expect(Promise.all(requests)).to.be.rejected
+    })
+  })
+  */
 })
 
 // ------------------------------------------------------------------------------------------------
