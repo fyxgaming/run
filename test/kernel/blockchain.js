9848ff998368fa61a97f03ae6fce3fc2f89d491f
Brenton Gunning <brentongunning@gmail.com>
2020-03-21 22:16:36 -0700

    should throw if no outputs

--- test/kernel/blockchain.js
+++ test/kernel/blockchain.js
@@ -70,13 +70,13 @@ describe('Blockchain', () => {
       await expect(blockchain.broadcast(tx)).to.be.rejectedWith(errors.noInputs)
     })
 
-    /*
     it('should throw if no outputs', async () => {
-      const utxos = await blockchain.utxos(address)
-      const tx = new bsv.Transaction().from(utxos).sign(privateKey)
+      const utxo = (await blockchain.utxos(purse.address))[0]
+      const tx = new bsv.Transaction().from(utxo).sign(purse.bsvPrivateKey)
       await expect(blockchain.broadcast(tx)).to.be.rejectedWith(errors.noOutputs)
     })
 
+    /*
     it('should throw if fee too low', async () => {
       const utxos = await blockchain.utxos(address)
       const tx = new bsv.Transaction().from(utxos).change(address).fee(0).sign(privateKey)
