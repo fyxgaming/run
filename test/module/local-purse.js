732247783d9c99c342864c6cd81897d56e21c8f2
Brenton Gunning <brentongunning@gmail.com>
2020-03-22 21:22:18 -0700

    fee per kb tests

--- test/module/local-purse.js
+++ test/module/local-purse.js
@@ -87,26 +87,24 @@ describe('Purse', () => {
     })
 
     describe('feePerKb', () => {
-      /*
       it('should support passing in valid feePerKb', () => {
-        expect(new Purse({ blockchain: run.blockchain, feePerKb: 1.5 }).feePerKb).to.equal(1.5)
-        expect(new Purse({ blockchain: run.blockchain, feePerKb: 1000 }).feePerKb).to.equal(1000)
-        expect(new Purse({ blockchain: run.blockchain, feePerKb: Number.MAX_SAFE_INTEGER }).feePerKb).to.equal(Number.MAX_SAFE_INTEGER)
-        expect(new Purse({ blockchain: run.blockchain, feePerKb: 0 }).feePerKb).to.equal(0)
+        expect(new LocalPurse({ blockchain: run.blockchain, feePerKb: 1.5 }).feePerKb).to.equal(1.5)
+        expect(new LocalPurse({ blockchain: run.blockchain, feePerKb: 1000 }).feePerKb).to.equal(1000)
+        expect(new LocalPurse({ blockchain: run.blockchain, feePerKb: Number.MAX_SAFE_INTEGER }).feePerKb).to.equal(Number.MAX_SAFE_INTEGER)
+        expect(new LocalPurse({ blockchain: run.blockchain, feePerKb: 0 }).feePerKb).to.equal(0)
       })
 
       it('should throw if pass in invalid feePerKb', () => {
-        expect(() => new Purse({ blockchain: run.blockchain, feePerKb: -1 })).to.throw('Option feePerKb must be non-negative: -1')
-        expect(() => new Purse({ blockchain: run.blockchain, feePerKb: NaN })).to.throw('Option feePerKb must be finite: NaN')
-        expect(() => new Purse({ blockchain: run.blockchain, feePerKb: Number.POSITIVE_INFINITY })).to.throw('Option feePerKb must be finite: Infinity')
-        expect(() => new Purse({ blockchain: run.blockchain, feePerKb: false })).to.throw('Invalid feePerKb option: false')
-        expect(() => new Purse({ blockchain: run.blockchain, feePerKb: null })).to.throw('Invalid feePerKb option: null')
+        expect(() => new LocalPurse({ blockchain: run.blockchain, feePerKb: -1 })).to.throw('feePerKb must be non-negative: -1')
+        expect(() => new LocalPurse({ blockchain: run.blockchain, feePerKb: NaN })).to.throw('feePerKb must be finite: NaN')
+        expect(() => new LocalPurse({ blockchain: run.blockchain, feePerKb: Number.POSITIVE_INFINITY })).to.throw('feePerKb must be finite: Infinity')
+        expect(() => new LocalPurse({ blockchain: run.blockchain, feePerKb: false })).to.throw('Invalid feePerKb: false')
+        expect(() => new LocalPurse({ blockchain: run.blockchain, feePerKb: null })).to.throw('Invalid feePerKb: null')
       })
 
       it('should default to 1000 if not specified', () => {
-        expect(new Purse({ blockchain: run.blockchain }).feePerKb).to.equal(1000)
+        expect(new LocalPurse({ blockchain: run.blockchain }).feePerKb).to.equal(1000)
       })
-      */
     })
 
     describe('blockchain', () => {
