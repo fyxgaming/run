9669ec3e6fd914444fadd12684a6eb3bcb0502e9
Brenton Gunning <brentongunning@gmail.com>
2020-03-25 19:00:12 -0700

    Util tests

--- test/util/index.js
+++ test/util/index.js
@@ -6,20 +6,53 @@
 
 const { describe, it } = require('mocha')
 const { Run } = require('../config')
-const { _name } = Run._util
+const { expect } = require('chai')
+const { _bsvNetwork, _name } = Run._util
 
 describe('util', () => {
-  it.only('_name', () => {
-    console.log(_name('Hello, world!'))
-    console.log(_name(123))
-    console.log(_name(false))
-    console.log(_name(undefined))
-    console.log(_name(null))
-    console.log(_name([1, 2, 3]))
-    console.log(_name({ a: 1 }))
-    console.log(_name(class A {}))
-    console.log(_name(function f () {}))
-    console.log(_name(() => {}))
-    console.log(_name(Symbol.unscopables))
+  describe('_bsvNetwork', () => {
+    it('should return appropriate network', () => {
+      expect(_bsvNetwork('main')).to.equal('mainnet')
+      expect(_bsvNetwork('mainnet')).to.equal('mainnet')
+      expect(_bsvNetwork('mainSideChain')).to.equal('mainnet')
+      expect(_bsvNetwork('test')).to.equal('testnet')
+      expect(_bsvNetwork('mock')).to.equal('testnet')
+      expect(_bsvNetwork('stn')).to.equal('testnet')
+    })
+  })
+
+  describe('_name', () => {
+    it('should create short names', () => {
+      // Strings
+      expect(_name('')).to.equal('""')
+      expect(_name('abc')).to.equal('"abc"')
+      expect(_name('Hello, world!')).to.equal('"Hello, wor…"')
+      // Booleans
+      expect(_name(true)).to.equal('true')
+      expect(_name(false)).to.equal('false')
+      // Numbers
+      expect(_name(1)).to.equal('1')
+      expect(_name(-1)).to.equal('-1')
+      expect(_name(1.5)).to.equal('1.5')
+      expect(_name(NaN)).to.equal('NaN')
+      expect(_name(-Infinity)).to.equal('-Infinity')
+      // Symbols
+      expect(_name(Symbol.iterator)).to.equal('Symbol(Symbol.iterator)')
+      expect(_name(Symbol.unscopables)).to.equal('Symbol(Symbol.unscopables)')
+      // Undefined
+      expect(_name(undefined)).to.equal('undefined')
+      // Objects
+      expect(_name(null)).to.equal('null')
+      expect(_name({})).to.equal('[object Object]')
+      expect(_name({ a: 1 })).to.equal('[object Object]')
+      expect(_name([1, 2, 3])).to.equal('[object Array]')
+      expect(_name(new class Dragon {}())).to.equal('[object Dragon]')
+      // Functions
+      expect(_name(function f () { })).to.equal('f')
+      expect(_name(class A { })).to.equal('A')
+      expect(_name(function () { })).to.equal('[anonymous function]')
+      expect(_name(() => { })).to.equal('[anonymous function]')
+      expect(_name(class { })).to.equal('[anonymous class]')
+    })
   })
 })
