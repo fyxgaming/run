a2fc757eb30a5cef9f2a00de538ca8f81fd9ce65
Brenton Gunning <brentongunning@gmail.com>
2020-03-25 23:29:12 -0700

    Clean up test

--- test/kernel/code.js
+++ test/kernel/code.js
@@ -10,16 +10,16 @@ const { Run } = require('../config')
 describe('Code', () => {
   it.only('should deploy', async () => {
     const run = new Run({ logger: console })
-    await run.deploy(function render () { return 1 })
+    // await run.deploy(function render () { return 1 })
     await run.deploy(class Dragon {})
-    await run.deploy(x => x)
-    await run.deploy(class {})
+    // await run.deploy(x => x)
+    // await run.deploy(class {})
 
     // TODO: Detect undeployable
     // await run.deploy('class A { }')
 
-    class B { }
-    B.locationMocknet = '123'
-    await run.deploy(B)
+    // class B { }
+    // B.locationMocknet = '123'
+    // await run.deploy(B)
   })
 })
