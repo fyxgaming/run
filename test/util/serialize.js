996d0a4c1bb750bde9f8b621ceaa03aac05befd1
Brenton Gunning <brentongunning@gmail.com>
2020-03-29 20:59:53 -0700

    Serialize progress

--- test/util/serialize.js
+++ test/util/serialize.js
@@ -6,13 +6,28 @@
 
 const { describe, it } = require('mocha')
 const { Run } = require('../config')
-const { serialize, deserialize } = Run._util
+const { _serialize } = Run._util
+const DeterministicRealm = require('@runonbitcoin/sandbox')
+
+const realm = new DeterministicRealm()
+const compartment = realm.makeCompartment()
+const Object = compartment.evaluate('Object')
+const Array = compartment.evaluate('Array')
 
 describe('util', () => {
   describe('serialize', () => {
     it('test', () => {
-      console.log(serialize)
-      console.log(deserialize)
+      const o = Object.create(Object.prototype)
+      o.n = 1
+      o.m = o
+      const p = Array.from([o, o])
+      console.log(JSON.stringify(_serialize(p)))
+
+      console.log(JSON.stringify(_serialize({ $hello: 'world' })))
+
+      console.log(JSON.stringify(_serialize(new Set([1, 'a', 2, {}]))))
+
+      console.log(JSON.stringify(_serialize(new Map([[1, 2], [{}, 'o']]))))
     })
   })
 })
