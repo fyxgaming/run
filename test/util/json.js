652a67c87f18b6291eca28f9c37f91186be977f1
Brenton Gunning <brentongunning@gmail.com>
2020-03-29 22:27:59 -0700

    Progress

--- test/util/json.js
+++ test/util/json.js
@@ -26,19 +26,25 @@ const options = { _hostIntrinsics, _sandboxIntrinsics }
 describe('util', () => {
   describe('_toTokenJson', () => {
     it('test', () => {
+      console.log(JSON.stringify(_toTokenJson({ n: 1 })))
+      console.log(JSON.stringify(_toTokenJson({ $hello: 'world' })))
+      console.log(JSON.stringify(_toTokenJson(new Set([1, 'a', 2, {}]))))
+      console.log(JSON.stringify(_toTokenJson(new Map([[1, 2], [{}, 'o']]))))
+      const s = new Set()
+      s.x = 1
+      console.log(JSON.stringify(_toTokenJson(s)))
       const o = Object.create(Object.prototype)
       o.n = 1
       o.m = o
       const p = Array.from([o, o])
       console.log(JSON.stringify(_toTokenJson(p)))
-      console.log(JSON.stringify(_toTokenJson({ $hello: 'world' })))
-      console.log(JSON.stringify(_toTokenJson(new Set([1, 'a', 2, {}]))))
-      console.log(JSON.stringify(_toTokenJson(new Map([[1, 2], [{}, 'o']]))))
     })
   })
 
   describe('_fromTokenJson', () => {
     it('test', () => {
+      console.log(JSON.stringify(_fromTokenJson(_toTokenJson({ n: 1 }))))
+
       const x = { $hello: 'world' }
       const y = _toTokenJson(x, { options, _outputIntrinsics: _sandboxIntrinsics })
       console.log(y)
@@ -46,6 +52,19 @@ describe('util', () => {
       console.log(y.constructor === _sandboxIntrinsics.Object)
       const z = _fromTokenJson(y, options)
       console.log(z)
+
+      console.log(_fromTokenJson(_toTokenJson(new Set([1, 'a', 2, {}]))))
+      console.log(_fromTokenJson(_toTokenJson(new Map([[1, 2], [{}, 'o']]))))
+
+      const s = new Set()
+      s.x = 1
+      console.log(_fromTokenJson(_toTokenJson(s)).x)
+
+      // const o = Object.create(Object.prototype)
+      // o.n = 1
+      // o.m = o
+      // const p = Array.from([o, o])
+      // console.log(JSON.stringify(_fromTokenJson(_toTokenJson(p))))
     })
   })
 })
