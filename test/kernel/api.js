dd09d37e27f78aabf4aef7d6be90684e96bf1a06
Brenton Gunning <brentongunning@gmail.com>
2020-04-12 17:11:44 -0700

    Basic tests

--- test/kernel/api.js
+++ test/kernel/api.js
@@ -5,41 +5,59 @@
  */
 
 const { describe, it } = require('mocha')
+const { expect } = require('chai')
 const { Run } = require('../env/config')
 const { Blockchain, Purse, Logger, State, Lock, Owner } = Run.api
 
 // ------------------------------------------------------------------------------------------------
-// Api
+// Blockchain Api
 // ------------------------------------------------------------------------------------------------
 
-describe('Api', () => {
-  describe('Blockchain', () => {
-    it('test', () => {
-      console.log('hello')
+describe('Blockchain API', () => {
+  describe('instanceof', () => {
+    it('should not match non-objects', () => {
+      expect(0 instanceof Blockchain).to.equal(false)
     })
   })
+})
 
-  describe('Purse', () => {
-    // TODO
+describe('Purse API ', () => {
+  describe('instanceof', () => {
+    it('should match non-objects', () => {
+      expect(0 instanceof Purse).to.equal(false)
+    })
   })
+})
 
-  describe('Logger', () => {
-    // TODO
+describe('Logger', () => {
+  describe('instanceof', () => {
+    it('should match non-objects', () => {
+      expect(0 instanceof Logger).to.equal(false)
+    })
   })
+})
 
-  describe('State', () => {
-    // TODO
-
+describe('State', () => {
+  describe('instanceof', () => {
+    it('should match non-objects', () => {
+      expect(0 instanceof State).to.equal(false)
+    })
   })
+})
 
-  describe('Lock', () => {
-    // TODO
-
+describe('Lock', () => {
+  describe('instanceof', () => {
+    it('should match non-objects', () => {
+      expect(0 instanceof Lock).to.equal(false)
+    })
   })
+})
 
-  describe('Owner', () => {
-    // TODO
-
+describe('Owner', () => {
+  describe('instanceof', () => {
+    it('should match non-objects', () => {
+      expect(0 instanceof Owner).to.equal(false)
+    })
   })
 })
 
