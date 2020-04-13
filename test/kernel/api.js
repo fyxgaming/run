2dc48cdadfb9618e5602218f1cb5346664895479
Brenton Gunning <brentongunning@gmail.com>
2020-04-12 17:15:01 -0700

    Latest

--- test/kernel/api.js
+++ test/kernel/api.js
@@ -17,6 +17,12 @@ describe('Blockchain API', () => {
   describe('instanceof', () => {
     it('should not match non-objects', () => {
       expect(0 instanceof Blockchain).to.equal(false)
+      expect(true instanceof Blockchain).to.equal(false)
+      expect('blockchain' instanceof Blockchain).to.equal(false)
+      expect(null instanceof Blockchain).to.equal(false)
+      expect(undefined instanceof Blockchain).to.equal(false)
+      expect(Symbol.hasInstance instanceof Blockchain).to.equal(false)
+      expect((() => {}) instanceof Blockchain).to.equal(false)
     })
   })
 })
@@ -25,38 +31,68 @@ describe('Purse API ', () => {
   describe('instanceof', () => {
     it('should match non-objects', () => {
       expect(0 instanceof Purse).to.equal(false)
+      expect(true instanceof Purse).to.equal(false)
+      expect('blockchain' instanceof Purse).to.equal(false)
+      expect(null instanceof Purse).to.equal(false)
+      expect(undefined instanceof Purse).to.equal(false)
+      expect(Symbol.hasInstance instanceof Purse).to.equal(false)
+      expect((() => {}) instanceof Purse).to.equal(false)
     })
   })
 })
 
-describe('Logger', () => {
+describe('Logger API', () => {
   describe('instanceof', () => {
     it('should match non-objects', () => {
       expect(0 instanceof Logger).to.equal(false)
+      expect(true instanceof Logger).to.equal(false)
+      expect('blockchain' instanceof Logger).to.equal(false)
+      expect(null instanceof Logger).to.equal(false)
+      expect(undefined instanceof Logger).to.equal(false)
+      expect(Symbol.hasInstance instanceof Logger).to.equal(false)
+      expect((() => {}) instanceof Logger).to.equal(false)
     })
   })
 })
 
-describe('State', () => {
+describe('State API', () => {
   describe('instanceof', () => {
     it('should match non-objects', () => {
       expect(0 instanceof State).to.equal(false)
+      expect(true instanceof State).to.equal(false)
+      expect('blockchain' instanceof State).to.equal(false)
+      expect(null instanceof State).to.equal(false)
+      expect(undefined instanceof State).to.equal(false)
+      expect(Symbol.hasInstance instanceof State).to.equal(false)
+      expect((() => {}) instanceof State).to.equal(false)
     })
   })
 })
 
-describe('Lock', () => {
+describe('Lock API', () => {
   describe('instanceof', () => {
     it('should match non-objects', () => {
       expect(0 instanceof Lock).to.equal(false)
+      expect(true instanceof Lock).to.equal(false)
+      expect('blockchain' instanceof Lock).to.equal(false)
+      expect(null instanceof Lock).to.equal(false)
+      expect(undefined instanceof Lock).to.equal(false)
+      expect(Symbol.hasInstance instanceof Lock).to.equal(false)
+      expect((() => {}) instanceof Lock).to.equal(false)
     })
   })
 })
 
-describe('Owner', () => {
+describe('Owner API', () => {
   describe('instanceof', () => {
     it('should match non-objects', () => {
       expect(0 instanceof Owner).to.equal(false)
+      expect(true instanceof Owner).to.equal(false)
+      expect('blockchain' instanceof Owner).to.equal(false)
+      expect(null instanceof Owner).to.equal(false)
+      expect(undefined instanceof Owner).to.equal(false)
+      expect(Symbol.hasInstance instanceof Owner).to.equal(false)
+      expect((() => {}) instanceof Owner).to.equal(false)
     })
   })
 })
