1131427bada02b798b348fd9ca6769f273c54076
Brenton Gunning <brentongunning@gmail.com>
2020-04-13 12:41:24 -0700

    Group lock test outlines

--- test/extra/group-lock.js
+++ test/extra/group-lock.js
@@ -23,6 +23,35 @@ describe('GroupLock', () => {
     a.set()
     await a.sync()
   })
+
+  it('should support 2-3 multisig', async () => {
+    // TODO
+  })
+
+  // Move to local owner?
+  it('should not sign if already signed', () => {
+    // TODO
+  })
+
+  it('should throw if pubkeys is not array', () => {
+    // TODO
+  })
+  
+  it('should throw if pubkeys is empty', () => {
+    // TODO
+  })
+
+  it('should throw if pubkeys are not valid hex strings', () => {
+    // TODO
+  })
+
+  it('should throw if m is out of range', () => {
+    // TODO
+  })
+
+  it('should throw if more than 16 pubkeys', () => {
+    // TODO
+  })
 })
 
 // ------------------------------------------------------------------------------------------------
