4ecc03c3fcd8ee98a9e623bcf0d83ba25d1884af
Brenton Gunning <brentongunning@gmail.com>
2020-04-13 18:31:16 -0700

    GroupLock fix

--- test/extra/group-lock.js
+++ test/extra/group-lock.js
@@ -4,17 +4,19 @@
  * Tests for lib/extra/group-lock.js
  */
 
-const { describe, it } = require('mocha')
+const { describe, it, beforeEach } = require('mocha')
+const { Run } = require('../env/config')
+const { Jig, GroupLock } = Run
 
 // ------------------------------------------------------------------------------------------------
 // GroupLock
 // ------------------------------------------------------------------------------------------------
 
 describe('GroupLock', () => {
+  const run = new Run()
+  beforeEach(() => run.activate())
+
   it('should support 1-1 multisig', async () => {
-    const { Run } = require('../env/config')
-    const { Jig, GroupLock } = Run
-    const run = new Run()
     class A extends Jig {
       init (owner) { this.owner = owner }
       set () { this.n = 1 }
@@ -25,7 +27,32 @@ describe('GroupLock', () => {
   })
 
   it('should support 2-3 multisig', async () => {
-    // TODO
+    const run2 = new Run()
+    const run3 = new Run()
+    class A extends Jig {
+      init (owner) { this.owner = owner }
+      set () { this.n = 1 }
+    }
+
+    // Create a jig with a 2-3 group owner
+    run.activate()
+    const a = new A(new GroupLock([run.owner.pubkey, run2.owner.pubkey, run3.owner.pubkey], 2))
+    await a.sync()
+
+    // Sign with pubkey 1 and export tx
+    run.transaction.begin()
+    a.set()
+    await run.transaction.pay()
+    await run.transaction.sign()
+    const tx = run.transaction.export()
+    run.transaction.rollback()
+
+    // Sign with pubkey 2 and broadcast
+    run2.activate()
+    await run2.transaction.import(tx)
+    await run2.transaction.sign()
+    run2.transaction.end()
+    await run2.sync()
   })
 
   // Move to local owner?
@@ -36,7 +63,7 @@ describe('GroupLock', () => {
   it('should throw if pubkeys is not array', () => {
     // TODO
   })
-  
+
   it('should throw if pubkeys is empty', () => {
     // TODO
   })
