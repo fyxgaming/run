8e9b81bf5afc84b8b10305cb4a43020bef576df1
Brenton Gunning <brentongunning@gmail.com>
2020-04-13 19:56:01 -0700

    No more than 16

--- test/extra/group-lock.js
+++ test/extra/group-lock.js
@@ -8,6 +8,7 @@ const { describe, it } = require('mocha')
 const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
 chai.use(chaiAsPromised)
+const { PrivateKey } = require('bsv')
 const { expect } = chai
 const { Run } = require('../env/config')
 const { GroupLock } = Run
@@ -17,23 +18,39 @@ const { GroupLock } = Run
 // ------------------------------------------------------------------------------------------------
 
 describe('GroupLock', () => {
-  it('should throw if pubkeys is not array', () => {
-    // TODO
+  it('should generate script 1-1', () => {
+
   })
 
-  it('should throw if pubkeys is empty', () => {
-    // TODO
+  it('should generate script 3-5', () => {
+
   })
+
+  it('should generate script 16-16', () => {
+
+  })
+
+  it('should throw if pubkeys is not non-empty array', () => {
+    expect(() => new GroupLock(null, 1).script).to.throw('pubkeys not an array')
+    expect(() => new GroupLock({}, 1).script).to.throw('pubkeys not an array')
+    expect(() => new GroupLock([], 1).script).to.throw('pubkeys must have at least one entry')
+  })
+
+  it('should throw if more than 16 pubkeys', () => {
+    const pubkeys = []
+    for (let i = 0; i < 17; i++) {
+      pubkeys.push(new PrivateKey().publicKey.toString())
+    }
+    expect(() => new GroupLock(pubkeys, 1).script).to.throw('No more than 16 pubkeys allowed')
+  })
+
+  // Duplicates
 
   it('should throw if pubkeys are not valid hex strings', () => {
     // TODO
   })
 
   it('should throw if m is out of range', () => {
-    // TODO
-  })
-
-  it('should throw if more than 16 pubkeys', () => {
     // TODO
   })
 })
