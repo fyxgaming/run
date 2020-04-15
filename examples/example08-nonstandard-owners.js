c03b7319af5876915ecf0214ac11a299e933c583
Brenton Gunning <brentongunning@gmail.com>
2020-04-15 11:21:17 -0700

    Updated examples

--- examples/example08-nonstandard-owners.js
+++ examples/example08-nonstandard-owners.js
@@ -2,21 +2,26 @@ const bsv = require('bsv')
 const Run = require('../dist/run.node.min')
 const { asm } = Run
 
+// Create a custom output script that to spend must input the result of 2+2
 class TwoPlusTwoLock {
   get script () { return asm('OP_2 OP_2 OP_ADD OP_EQUAL') }
-  static get deps() { return { asm } }
 }
 
+TwoPlusTwoLock.deps { asm }
+
+// Create a NonStandardOwner that is capable of signing these custom output scripts
 class NonStandardOwner {
   next () { return new TwoPlusTwoLock() }
 
   async sign (tx, locks) {
+    // Find each input that is a TwoPlusTwo lock and sign it with 4
     tx.inputs
       .filter((_, n) => locks[n] instanceof TwoPlusTwoLock)
       .forEach(input => input.setScript('OP_4'))
   }
 }
 
+// A basic jig that we can update the properties on
 class Dragon extends Jig {
   setName(name) { this.name = name }
 }
