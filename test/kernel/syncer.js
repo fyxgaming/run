abd0df31bf91a212b126541255fcb5f9c2aa004c
Brenton Gunning <brentongunning@gmail.com>
2020-04-26 15:14:45 -0700

    Add two tests

--- test/kernel/syncer.js
+++ test/kernel/syncer.js
@@ -40,7 +40,43 @@ describe('Syncer', () => {
       run.deactivate()
     })
 
-    it('sync with nothing queued returns immediately', async () => {
+    it('should not force-fetch transactions just broadcasted (payment channels)', async () => {
+      const run = new Run()
+      spy(run.blockchain)
+      class A extends Jig { set (n) { this.n = n } }
+      const a = new A()
+      await run.sync()
+
+      run.transaction.begin()
+      a.set(1)
+      await run.transaction.pay()
+      await run.transaction.sign()
+      const tx = run.transaction.export()
+      run.transaction.rollback()
+
+      await run.transaction.import(tx)
+      const jig = run.transaction.actions[0].target
+      run.transaction.end()
+      await jig.sync()
+
+      const txid = jig.location.slice(0, 64)
+      expect(run.blockchain.fetch.calledWith(txid, false)).to.equal(true)
+
+      run.deactivate()
+    })
+
+    it('should not fetch transactions just broadcasted when forward = false', async () => {
+      const run = new Run()
+      spy(run.blockchain)
+      class A extends Jig { }
+      const a = new A()
+      await a.sync({ forward: false })
+      const txid = a.location.slice(0, 64)
+      expect(run.blockchain.fetch.calledWith(txid, false)).to.equal(false)
+      run.deactivate()
+    })
+
+    it('should return immediately when nothing is queued', async () => {
       const run = new Run()
       await run.sync()
       run.deploy(class A {})
