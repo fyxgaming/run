358c606ab20074052b721e284ee98b1a0b3abfb5
Brenton Gunning <brentongunning@gmail.com>
2020-04-27 21:11:31 -0700

    Rest tests

--- test/util/rest.js
+++ test/util/rest.js
@@ -12,7 +12,11 @@ const REST = unmangle(unmangle(Run)._util)._REST
 describe('rest', () => {
   describe('_get', () => {
     it('should return json', async () => {
-      console.log(await REST._get('https://api.run.network/v1/test/status'))
+      // console.log(await REST._get('https://api.run.network/v1/test/status'))
+      // console.log(await REST._get('https://api.run.network/v1/test/utxos/mhjSrMHzs97CCvhPtBn1qbfuJqqfmMzWQ4'))
+      console.log(await REST._get('https://api.run.network/v1/test/abc'))
+      // console.log(await REST._get('123'))
+      // console.log(await REST._get('http://api.run.network/v1/test/status'))
     })
   })
 })
