765912308160eb9b2884f79af463c87b66481504
Brenton Gunning <brentongunning@gmail.com>
2020-04-28 02:16:27 -0700

    Fix lint

--- test/util/rest.js
+++ test/util/rest.js
@@ -12,7 +12,7 @@ const REST = unmangle(unmangle(unmangle(Run)._util)._REST)
 describe('rest', () => {
   describe('_get', () => {
     it.skip('should return json', async () => {
-      // console.log(await REST._get('https://api.run.network/v1/test/status'))
+      console.log(await REST._get('https://api.run.network/v1/test/status'))
       // console.log(await REST._get('https://api.run.network/v1/test/utxos/mhjSrMHzs97CCvhPtBn1qbfuJqqfmMzWQ4'))
       // console.log(await REST._get('https://api.run.network/v1/test/abc'))
       // console.log(await REST._get('https://api.run.network/v1/test/tx/1234'))
