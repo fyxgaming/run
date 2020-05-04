a150d2363c0ac6aeb6bbefcfca74a2e19001f757
Brenton Gunning <brentongunning@gmail.com>
2020-05-04 15:16:03 -0700

    First purse.broadcast test

--- test/kernel/purse.js
+++ test/kernel/purse.js
@@ -5,6 +5,10 @@
  */
 
 const { describe, it } = require('mocha')
+const { expect } = require('chai')
+const { spy } = require('sinon')
+const { Run } = require('../env/config')
+const { Jig } = Run
 
 // ------------------------------------------------------------------------------------------------
 // Purse tests
@@ -13,13 +17,40 @@ const { describe, it } = require('mocha')
 describe('Purse', () => {
   describe('pay', () => {
     it('test', () => {
-      console.log('hello')
+      // TODO
     })
   })
 
   describe('broadcast', () => {
-    it('test', () => {
-      console.log('hello')
+    it('should be called with finalized transaction', async () => {
+      // TODO
+    })
+
+    it('should be called after before actual broadcast', async () => {
+      const run = new Run()
+      // Hook purse.broadcast to check that we are called after sign() and before broadcast()
+      run.purse.broadcast = tx => {
+        return new Promise((resolve, reject) => {
+          expect(run.owner.sign.called).to.equal(true)
+          expect(run.blockchain.broadcast.called).to.equal(false)
+          resolve()
+        })
+      }
+      // Listen for calls to our modules
+      spy(run.purse)
+      spy(run.blockchain)
+      spy(run.owner)
+      // Create and sync a jig
+      class Dragon extends Jig { }
+      const dragon = new Dragon()
+      await dragon.sync()
+      // Check that our broadcast was called
+      expect(run.purse.broadcast.called).to.equal(true)
+      expect(run.blockchain.broadcast.called).to.equal(true)
+    })
+
+    it('should log but still broadcast tx if errors are thrown', async () => {
+      // TODO
     })
   })
 })
