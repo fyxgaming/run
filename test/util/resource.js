1976ff1ffbacbfafa21a12a74ccf68abc58704f6
Brenton Gunning <brentongunning@gmail.com>
2020-05-26 13:00:56 -0700

    resource tests

--- test/util/resource.js
+++ test/util/resource.js
@@ -21,8 +21,91 @@ const {
 // ------------------------------------------------------------------------------------------------
 
 describe('_location', () => {
-  // TODO
-  console.log(_location)
+  const USER = 1
+  const JIG = 2
+  const BERRY = 4
+
+  it('should parse valid locations', () => {
+    // Jigs
+    expect(_location('abc_o0')).to.deep.equal({ txid: 'abc', vout: 0 })
+    expect(_location('abc_i1')).to.deep.equal({ txid: 'abc', vin: 1 })
+    // Partial jigs
+    expect(_location('_o10')).to.deep.equal({ vout: 10 })
+    expect(_location('_i20')).to.deep.equal({ vin: 20 })
+    // Berries
+    expect(_location('abc_o0_')).to.deep.equal({ txid: 'abc', vout: 0, path: '' })
+    expect(_location('abc_o0_def')).to.deep.equal({ txid: 'abc', vout: 0, path: 'def' })
+    expect(_location('abc_o0_def_o2')).to.deep.equal({ txid: 'abc', vout: 0, path: 'def_o2' })
+    // Partial berries
+    expect(_location('_o0_def')).to.deep.equal({ vout: 0, path: 'def' })
+    expect(_location('_i0_def_o2')).to.deep.equal({ vin: 0, path: 'def_o2' })
+    // Errors
+    expect(_location('err://')).to.deep.equal({ error: '' })
+    expect(_location('err://Something bad happened')).to.deep.equal({ error: 'Something bad happened' })
+    // Temporary locations
+    expect(_location('tmp://abc_o1')).to.deep.equal({ txid: 'abc', vout: 1, temp: true })
+    expect(_location('tmp://_i2')).to.deep.equal({ vin: 2, temp: true })
+  })
+
+  it('should throw for invalid locations', () => {
+    // Invalid types
+    expect(() => _location()).to.throw()
+    expect(() => _location(1)).to.throw()
+    expect(() => _location({})).to.throw()
+    expect(() => _location(null)).to.throw()
+    // Bad structure
+    expect(() => _location('abc')).to.throw()
+    expect(() => _location('abc_')).to.throw()
+    expect(() => _location('abc_o')).to.throw()
+    expect(() => _location('abc_i')).to.throw()
+    expect(() => _location('abc_0')).to.throw()
+    expect(() => _location('_abc_o0')).to.throw()
+    expect(() => _location('tmp://abc_o')).to.throw()
+    expect(() => _location('tmp://abc_0')).to.throw()
+    // Invalid chars
+    expect(() => _location('$_o1')).to.throw()
+    expect(() => _location('abc_o*')).to.throw()
+    expect(() => _location('abc-o1')).to.throw()
+    // Bad protocols
+    expect(() => _location('tmp:abc_o1')).to.throw()
+    expect(() => _location('temp://abc_o1')).to.throw()
+    expect(() => _location('err:/abc_o1')).to.throw()
+    expect(() => _location('error://abc_o1')).to.throw()
+  })
+
+  it('should accept matching flags', () => {
+    // Jig
+    _location('abc_o1', JIG)
+    _location('_o1', JIG)
+    _location('_i2', JIG)
+    _location('tmp://abc_o1', JIG)
+    // Berry
+    _location('abc_o1_', BERRY)
+    _location('abc_o1_def', BERRY)
+    _location('_i1_def', BERRY)
+    // User
+    _location('abc_o1', USER)
+    _location('abc_o1_def', USER)
+    // Jig and User
+    _location('abc_o1', USER)
+  })
+
+  it('should throw when flags dont match', () => {
+    // Jig
+    expect(() => _location('abc_o1_', JIG)).to.throw()
+    expect(() => _location('error://', JIG)).to.throw()
+    // Berry
+    expect(() => _location('abc_o1', BERRY)).to.throw()
+    expect(() => _location('error://', BERRY)).to.throw()
+    expect(() => _location('tmp://abc_o1_def', BERRY)).to.throw()
+    // User
+    expect(() => _location('_o1', USER)).to.throw()
+    expect(() => _location('_i1', USER)).to.throw()
+    expect(() => _location('_o1_def', USER)).to.throw()
+    // Jig and User
+    expect(() => _location('abc_o1_', USER | JIG)).to.throw()
+    expect(() => _location('_o1', USER | JIG)).to.throw()
+  })
 })
 
 // ------------------------------------------------------------------------------------------------
