632a1a9ea471fd23d0eac94348161080f35a31ad
Brenton Gunning <brentongunning@gmail.com>
2020-06-02 18:01:56 -0700

    New code

---
+++ test/kernel/v2/code.js
@@ -0,0 +1,24 @@
+/**
+ * code.js
+ *
+ * Tests for lib/kernel/code.js
+ */
+
+const { describe, it } = require('mocha')
+const { Run } = require('../../env/config')
+const Code = Run._Code
+
+// ------------------------------------------------------------------------------------------------
+// Code
+// ------------------------------------------------------------------------------------------------
+
+describe('Code', () => {
+  it('basic test', () => {
+        new Run() // eslint-disable-line
+    class A {}
+    const ACode = new Code(A)
+    console.log(ACode)
+  })
+})
+
+// ------------------------------------------------------------------------------------------------
