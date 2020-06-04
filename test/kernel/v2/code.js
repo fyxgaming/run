7153fdd0d957f32998701e767aa315f49cf1299b
Brenton Gunning <brentongunning@gmail.com>
2020-06-04 16:14:10 -0700

    code test reorg

--- test/kernel/v2/code.js
+++ test/kernel/v2/code.js
@@ -32,6 +32,186 @@ describe('Code', () => {
       new Code(f) // eslint-disable-line
     })
 
+    it('throws if not a valid type', () => {
+      new Run() // eslint-disable-line
+      expect(() => new Code()).to.throw('Cannot install')
+      expect(() => new Code(0)).to.throw('Cannot install')
+      expect(() => new Code({})).to.throw('Cannot install')
+      expect(() => new Code('class A {}')).to.throw('Cannot install')
+      expect(() => new Code(null)).to.throw('Cannot install')
+    })
+
+    it('throw if anonymous', () => {
+      new Run() // eslint-disable-line
+      expect(() => new Code(() => {})).to.throw('Cannot install')
+      expect(() => new Code(class {})).to.throw('Cannot install')
+    })
+
+    it('throws if create built-in type', () => {
+      new Run() // eslint-disable-line
+      expect(() => new Code(Object)).to.throw('Cannot install Object')
+      expect(() => new Code(Date)).to.throw('Cannot install Date')
+      expect(() => new Code(Uint8Array)).to.throw('Cannot install')
+      expect(() => new Code(Math.sin)).to.throw('Cannot install sin')
+      expect(() => new Code(parseInt)).to.throw('Cannot install parseInt')
+    })
+
+    it('creates parents', () => {
+      new Run() // eslint-disable-line
+      class A { }
+      class B extends A { }
+      class C extends B { }
+      const CC = new Code(C)
+      const CB = new Code(B)
+      const CA = new Code(A)
+      expect(Object.getPrototypeOf(CC)).to.equal(CB)
+      expect(Object.getPrototypeOf(CB)).to.equal(CA)
+    })
+
+    it('throws if prototype inheritance', () => {
+      new Run() // eslint-disable-line
+      function A () { }
+      function B () { }
+      B.prototype = Object.create(A.prototype)
+      expect(() => new Code(B)).to.throw('Cannot install B')
+    })
+
+    it('creates only once', () => {
+      new Run() // eslint-disable-line
+      class A { }
+      const CA1 = new Code(A)
+      const CA2 = new Code(A)
+      expect(CA1).to.equal(CA2)
+    })
+
+    it('throws if error creating dependent code', () => {
+      new Run() // eslint-disable-line
+      class A { }
+      A.Date = Date
+      expect(() => new Code(A)).to.throw('Cannot install Date')
+      expect(Code._get(A)).to.equal(undefined)
+      expect(Code._get(Date)).to.equal(undefined)
+    })
+
+    it('throws if contains reserved properties', () => {
+      new Run() // eslint-disable-line
+      class A { }
+      A.toString = () => 'hello'
+      expect(() => new Code(A)).to.throw('Cannot install A')
+      class B { static deploy () { } }
+      expect(() => new Code(B)).to.throw('Cannot install B')
+      class C { }
+      C.upgrade = 1
+      expect(() => new Code(C)).to.throw('Cannot install C')
+      class D { }
+      D.sync = undefined
+      expect(() => new Code(D)).to.throw('Cannot install D')
+      class E { static get release () { } }
+      expect(() => new Code(E)).to.throw('Cannot install E')
+    })
+
+    it('should create code for props', () => {
+      new Run() // eslint-disable-line
+      class A { }
+      class B { }
+      A.B = B
+      const CA = new Code(A)
+      expect(CA.B).to.equal(new Code(B))
+    })
+
+    it('should install circular prop code', () => {
+      new Run() // eslint-disable-line
+      class A { }
+      class B { }
+      A.B = B
+      B.A = A
+      const CA = new Code(A)
+      const CB = new Code(B)
+      expect(CA.B).to.equal(CB)
+      expect(CB.A).to.equal(CA)
+    })
+
+    it('should install circular parent-child code', () => {
+      new Run() // eslint-disable-line
+      class B { }
+      class A extends B { }
+      B.A = A
+      const CA = new Code(A)
+      const CB = new Code(B)
+      expect(Object.getPrototypeOf(CA)).to.equal(CB)
+      expect(CB.A).to.equal(CA)
+    })
+
+    it('installs parent that is code jig', () => {
+      new Run() // eslint-disable-line
+      class B { }
+      const CB = new Code(B)
+      class A extends CB { }
+      B.deps = { A }
+      const CA = new Code(A)
+      expect(Object.getPrototypeOf(CA)).to.equal(CB)
+    })
+
+    // parent deps not in children
+    // parent presets don't go down
+    // test read only
+    // Parent can be an anonymous class?
+    // Parent can be a function?
+
+    /*
+    it('does not duplicate parents', () => {
+      // Parent
+    })
+    */
+  })
+
+  describe('deps', () => {
+    it('should make deps globals', () => {
+      new Run() // eslint-disable-line
+      class A { }
+      function f () { return A }
+      f.deps = { A }
+      const sf = new Code(f)
+      expect(sf()).to.equal(new Code(A))
+    })
+
+    it('should support non-resource deps', () => {
+      new Run() // eslint-disable-line
+      class A {
+        static n () { return n } // eslint-disable-line
+        static o () { return o } // eslint-disable-line
+      }
+      A.deps = { n: 1, o: { a: [] } }
+      const CA = new Code(A)
+      expect(CA.n()).to.equal(1)
+      expect(CA.o()).not.to.equal(A.deps.o)
+      expect(CA.o()).to.deep.equal(A.deps.o)
+    })
+
+    it('should set deps on code jig', () => {
+      new Run() // eslint-disable-line
+      class A { }
+      class B { }
+      A.deps = { B }
+      const CA = new Code(A)
+      expect(CA.deps.B).to.equal(new Code(B))
+    })
+
+    it('throws if deps invalid', () => {
+      new Run() // eslint-disable-line
+      class A { }
+      A.deps = null
+      expect(() => new Code(A)).to.throw('Cannot install A')
+      A.deps = '123'
+      expect(() => new Code(A)).to.throw('Cannot install A')
+      A.deps = []
+      expect(() => new Code(A)).to.throw('Cannot install A')
+      A.deps = new class Deps {}()
+      expect(() => new Code(A)).to.throw('Cannot install A')
+    })
+  })
+
+  describe('presets', () => {
     it('creates with resource presets', () => {
       const run = new Run()
       const network = run.blockchain.network
@@ -50,7 +230,7 @@ describe('Code', () => {
       expect(typeof CA.presets).to.equal('undefined')
     })
 
-    it('clones object presets', () => {
+    it('clones objects in presets', () => {
       const run = new Run()
       const network = run.blockchain.network
       class A { }
@@ -94,26 +274,6 @@ describe('Code', () => {
       const CA = new Code(A)
       expect(CA.B).not.to.equal(B)
       expect(CA.B.toString()).to.equal(B.toString())
-    })
-
-    it('throws if not a valid type', () => {
-      new Run() // eslint-disable-line
-      expect(() => new Code()).to.throw('Cannot install')
-      expect(() => new Code(0)).to.throw('Cannot install')
-      expect(() => new Code({})).to.throw('Cannot install')
-      expect(() => new Code('class A {}')).to.throw('Cannot install')
-      expect(() => new Code(null)).to.throw('Cannot install')
-      expect(() => new Code(() => {})).to.throw('Cannot install')
-      expect(() => new Code(class {})).to.throw('Cannot install')
-    })
-
-    it('throws if create built-in type', () => {
-      new Run() // eslint-disable-line
-      expect(() => new Code(Object)).to.throw('Cannot install Object')
-      expect(() => new Code(Date)).to.throw('Cannot install Date')
-      expect(() => new Code(Uint8Array)).to.throw('Cannot install')
-      expect(() => new Code(Math.sin)).to.throw('Cannot install sin')
-      expect(() => new Code(parseInt)).to.throw('Cannot install parseInt')
     })
 
     it('throws if parent dependency mismatch', () => {
@@ -206,165 +366,15 @@ describe('Code', () => {
       A.presets = { [network]: { upgrade: () => {} } }
       expect(() => new Code(A)).to.throw()
     })
+  })
 
-    it('creates parents', () => {
-      new Run() // eslint-disable-line
-      class A { }
-      class B extends A { }
-      class C extends B { }
-      const CC = new Code(C)
-      const CB = new Code(B)
-      const CA = new Code(A)
-      expect(Object.getPrototypeOf(CC)).to.equal(CB)
-      expect(Object.getPrototypeOf(CB)).to.equal(CA)
-    })
-
-    it('throws if prototype inheritance', () => {
-      new Run() // eslint-disable-line
-      function A () { }
-      function B () { }
-      B.prototype = Object.create(A.prototype)
-      expect(() => new Code(B)).to.throw('Cannot install B')
-    })
-
-    it('creates only once', () => {
-      new Run() // eslint-disable-line
-      class A { }
-      const CA1 = new Code(A)
-      const CA2 = new Code(A)
-      expect(CA1).to.equal(CA2)
-    })
-
-    it('should set deps as globals', () => {
-      new Run() // eslint-disable-line
-      class A { }
-      function f () { return A }
-      f.deps = { A }
-      const sf = new Code(f)
-      expect(sf()).to.equal(new Code(A))
-    })
-
-    it('should support non-resource deps', () => {
-      new Run() // eslint-disable-line
-      class A {
-        static n () { return n } // eslint-disable-line
-        static o () { return o } // eslint-disable-line
-      }
-      A.deps = { n: 1, o: { a: [] } }
-      const CA = new Code(A)
-      expect(CA.n()).to.equal(1)
-      expect(CA.o()).not.to.equal(A.deps.o)
-      expect(CA.o()).to.deep.equal(A.deps.o)
-    })
-
-    it('should set deps on code jig', () => {
-      new Run() // eslint-disable-line
-      class A { }
-      class B { }
-      A.deps = { B }
-      const CA = new Code(A)
-      expect(CA.deps.B).to.equal(new Code(B))
-    })
-
-    it('throws if deps invalid', () => {
-      new Run() // eslint-disable-line
-      class A { }
-      A.deps = null
-      expect(() => new Code(A)).to.throw('Cannot install A')
-      A.deps = '123'
-      expect(() => new Code(A)).to.throw('Cannot install A')
-      A.deps = []
-      expect(() => new Code(A)).to.throw('Cannot install A')
-      A.deps = new class Deps {}()
-      expect(() => new Code(A)).to.throw('Cannot install A')
-    })
-
-    it('throws if error creating dependent code', () => {
-      new Run() // eslint-disable-line
-      class A { }
-      A.Date = Date
-      expect(() => new Code(A)).to.throw('Cannot install Date')
-      expect(Code._get(A)).to.equal(undefined)
-      expect(Code._get(Date)).to.equal(undefined)
-    })
-
-    it('throws if contains reserved properties', () => {
-      new Run() // eslint-disable-line
-      class A { }
-      A.toString = () => 'hello'
-      expect(() => new Code(A)).to.throw('Cannot install A')
-      class B { static deploy () { } }
-      expect(() => new Code(B)).to.throw('Cannot install B')
-      class C { }
-      C.upgrade = 1
-      expect(() => new Code(C)).to.throw('Cannot install C')
-      class D { }
-      D.sync = undefined
-      expect(() => new Code(D)).to.throw('Cannot install D')
-      class E { static get release () { } }
-      expect(() => new Code(E)).to.throw('Cannot install E')
-    })
-
-    it('should create code for props', () => {
-      new Run() // eslint-disable-line
-      class A { }
-      class B { }
-      A.B = B
-      const CA = new Code(A)
-      expect(CA.B).to.equal(new Code(B))
-    })
-
-    it('should install circular prop code', () => {
-      new Run() // eslint-disable-line
-      class A { }
-      class B { }
-      A.B = B
-      B.A = A
-      const CA = new Code(A)
-      const CB = new Code(B)
-      expect(CA.B).to.equal(CB)
-      expect(CB.A).to.equal(CA)
-    })
-
-    it('should install circular parent-child code', () => {
-      new Run() // eslint-disable-line
-      class B { }
-      class A extends B { }
-      B.A = A
-      const CA = new Code(A)
-      const CB = new Code(B)
-      expect(Object.getPrototypeOf(CA)).to.equal(CB)
-      expect(CB.A).to.equal(CA)
-    })
-
-    it('installs parent that is code jig', () => {
-      new Run() // eslint-disable-line
-      class B { }
-      const CB = new Code(B)
-      class A extends CB { }
-      B.deps = { A }
-      const CA = new Code(A)
-      expect(Object.getPrototypeOf(CA)).to.equal(CB)
-    })
-
+  describe('prototype', () => {
     it('should set prototype constructor to code jig', () => {
       new Run() // eslint-disable-line
       class A { }
       const CA = new Code(A)
       expect(CA.prototype.constructor).to.equal(CA)
     })
-
-    // parent deps not in children
-    // parent presets don't go down
-    // test read only
-    // Parent can be an anonymous class?
-    // Parent can be a function?
-
-    /*
-    it('does not duplicate parents', () => {
-      // Parent
-    })
-    */
   })
 
   describe('name', () => {
