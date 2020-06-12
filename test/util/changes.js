3f9469501694f7564770bace971902a8788598e1
Brenton Gunning <brentongunning@gmail.com>
2020-06-11 23:33:33 -0700

    delete

--- test/util/changes.js
+++ test/util/changes.js
@@ -17,8 +17,15 @@ const Changes = unmangle(unmangle(Run)._util)._Changes
 describe('Changes', () => {
   describe('set', () => {
     it('makes changes to objects', () => {
-      const o = {}
       const changes = unmangle(new Changes())
+      const o = {}
+      changes._set(o, o, 'a', 1)
+      expect(o.a).to.equal(1)
+    })
+
+    it('makes changes to arrays', () => {
+      const changes = unmangle(new Changes())
+      const o = {}
       changes._set(o, o, 'a', 1)
       expect(o.a).to.equal(1)
     })
@@ -26,6 +33,37 @@ describe('Changes', () => {
 
   describe('rollback', () => {
     it('rolls back object sets while preserving order', () => {
+
+    })
+  })
+
+  describe('diff', () => {
+    it('detects object adds', () => {
+      const changes = unmangle(new Changes())
+      const o = {}
+      changes._set(o, o, 'a', 1)
+      expect(changes._diff()).to.deep.equal(new Set([o]))
+    })
+
+    it('detects object sets', () => {
+      const changes = unmangle(new Changes())
+      const o = { a: 1 }
+      changes._set(o, o, 'a', 2)
+      expect(changes._diff()).to.deep.equal(new Set([o]))
+    })
+
+    it('detects object deletes', () => {
+      const changes = unmangle(new Changes())
+      const o = { a: 1 }
+      changes._delete(o, o, 'a')
+      expect(changes._diff()).to.deep.equal(new Set([o]))
+    })
+
+    it('detects object reorders', () => {
+
+    })
+
+    it('detects reverts', () => {
 
     })
   })
