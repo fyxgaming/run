c72c33075a53532f4e56287c616356981b90ad28
Brenton Gunning <brentongunning@gmail.com>
2020-06-26 15:51:06 -0700

    get tests running

--- test/util/bindings.js
+++ test/util/bindings.js
@@ -18,18 +18,14 @@ const { _location, _nonce, _satoshis, _owner } = unmangle(unmangle(Run)._util)
 
 describe('_location', () => {
   const USER = 1
-  const JIG = 2
-  const BERRY = 4
 
   it('should parse valid locations', () => {
     // Jigs
     expect(_location('abc_o0')).to.deep.equal({ txid: 'abc', vout: 0 })
-    expect(_location('abc_i1')).to.deep.equal({ txid: 'abc', vin: 1 })
-    expect(_location('abc_d0')).to.deep.equal({ txid: 'abc', vdel: 0 })
+    expect(_location('abc_d1')).to.deep.equal({ txid: 'abc', vdel: 0 })
     expect(_location('native://Jig')).to.deep.equal({ native: 'Jig' })
-    // Partial jigs
+    // Local jigs
     expect(_location('_o10')).to.deep.equal({ vout: 10 })
-    expect(_location('_i20')).to.deep.equal({ vin: 20 })
     expect(_location('_d1')).to.deep.equal({ vdel: 1 })
     // Berries
     expect(_location('abc_o0_')).to.deep.equal({ txid: 'abc', vout: 0, path: '' })
@@ -39,7 +35,7 @@ describe('_location', () => {
     // Partial berries
     expect(_location('_o0_def')).to.deep.equal({ vout: 0, path: 'def' })
     expect(_location('_i0_def_o2')).to.deep.equal({ vin: 0, path: 'def_o2' })
-    expect(_location('_d2_def')).to.deep.equal({ vdel: 2, path: 'def' })
+    expect(_location('_d2_')).to.deep.equal({ vdel: 2, path: '' })
     // Errors
     expect(_location('error://')).to.deep.equal({ error: '' })
     expect(_location('error://Something bad happened')).to.deep.equal({ error: 'Something bad happened' })
@@ -50,6 +46,8 @@ describe('_location', () => {
   })
 
   it('should throw for invalid locations', () => {
+    expect(_location('abc_i1')).to.deep.equal({ txid: 'abc', vin: 1 })
+    expect(_location('_i20')).to.deep.equal({ vin: 20 })
     // Invalid types
     expect(() => _location()).to.throw()
     expect(() => _location(1)).to.throw()
@@ -82,43 +80,17 @@ describe('_location', () => {
   })
 
   it('should accept matching flags', () => {
-    // Jig
-    _location('abc_o1', JIG)
-    _location('_o1', JIG)
-    _location('_i2', JIG)
-    _location('record://abc_o1', JIG)
-    _location('native://Jig', JIG)
-    _location('native://Berry', JIG)
-    // Berry
-    _location('abc_o1_', BERRY)
-    _location('abc_o1_def', BERRY)
-    _location('_i1_def', BERRY)
-    // User
     _location('abc_o1', USER)
     _location('abc_o1_def', USER)
     _location('abc_d2', USER)
     _location('native://Jig', USER)
     _location('native://Berry', USER)
-    // Jig and User
-    _location('abc_o1', USER)
   })
 
   it('should throw when flags dont match', () => {
-    // Jig
-    expect(() => _location('abc_o1_', JIG)).to.throw()
-    expect(() => _location('error://', JIG)).to.throw()
-    // Berry
-    expect(() => _location('abc_o1', BERRY)).to.throw()
-    expect(() => _location('error://', BERRY)).to.throw()
-    expect(() => _location('record://abc_o1_def', BERRY)).to.throw()
-    expect(() => _location('native://Berry', BERRY)).to.throw()
-    // User
     expect(() => _location('_o1', USER)).to.throw()
-    expect(() => _location('_i1', USER)).to.throw()
+    expect(() => _location('_d1', USER)).to.throw()
     expect(() => _location('_o1_def', USER)).to.throw()
-    // Jig and User
-    expect(() => _location('abc_o1_', USER | JIG)).to.throw()
-    expect(() => _location('_o1', USER | JIG)).to.throw()
   })
 })
 
