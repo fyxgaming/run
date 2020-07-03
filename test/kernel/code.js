44dbd49661be56922c4e5550834bf9e83141ed60
Brenton Gunning <brentongunning@gmail.com>
2020-07-02 21:20:35 -0700

    bring over the rest of the tests

--- test/kernel/code.js
+++ test/kernel/code.js
@@ -1,7 +1,7 @@
 /**
  * repository.js
  *
- * Tests for lib/kernel/repository.js
+ * Tests for lib/kernel/code.js and lib/kernel/repository.js
  */
 
 const { describe, it } = require('mocha')
@@ -463,8 +463,87 @@ describe('Repository', () => {
     })
   })
 
+  describe('sealed', () => {
+    it('allows unsealing', async () => {
+      const run = new Run()
+      class A { }
+      A.sealed = false
+      class B extends A { }
+      const CA = run.install(A)
+      await run.deploy(A)
+      run.deactivate()
+      const run2 = new Run()
+      await run2.deploy(B)
+      await CA.sync()
+      expect(CA.origin).to.equal(CA.location)
+    })
+
+    it('throws if invalid', () => {
+      const run = new Run()
+      class A { }
+      A.sealed = null
+      expect(() => run.install(A)).to.throw('sealed must be a boolean')
+      A.sealed = 1
+      expect(() => run.install(A)).to.throw('sealed must be a boolean')
+    })
+  })
+
+  describe('prototype', () => {
+    it('sets prototype constructor to code jig', () => {
+      const run = new Run()
+      class A { }
+      const CA = run.install(A)
+      expect(CA.prototype.constructor).to.equal(CA)
+    })
+  })
+
+  describe('name', () => {
+    it('returns class or function name', () => {
+      const run = new Run()
+      class A { }
+      expect(run.install(A).name).to.equal('A')
+      function f () { }
+      expect(run.install(f).name).to.equal('f')
+    })
+  })
+
+  describe('toString', () => {
+    it('returns same string as original code', () => {
+      const run = new Run()
+      class A { }
+      const CA = run.install(A)
+      expect(CA.toString()).to.equal(A.toString())
+      expect(A.toString().replace(/\s/g, '')).to.equal('classA{}')
+    })
+
+    it('returns same code as original code when there is a parent', () => {
+      const run = new Run()
+      class B { }
+      const CB = run.install(B)
+      class A extends CB { }
+      const CA = run.install(A)
+      expect(CA.toString().replace(/\s/g, '')).to.equal('classAextendsB{}')
+    })
+  })
+
   describe('deploy', () => {
-    it.skip('requires parent approval by default', () => {
+    // Does not deploy if already deployed
+
+    it('deploys parent and child', () => {
+      const run = new Run()
+      class A {}
+      class B extends A {}
+      const CB = run.install(B)
+      CB.deploy()
+      // const record = unmangle(stub(record))
+      // expect(record._deploy.called).to.equal(true)
+      expect(A.location.startsWith('record://'))
+      expect(B.location.startsWith('record://'))
+      expect(A.location.endsWith('_o2'))
+      expect(B.location.endsWith('_o1'))
+    })
+
+    it.skip('sealed by default', () => {
       const run = new Run()
       class A { }
       A.options = { utility: true }
@@ -474,6 +553,72 @@ describe('Repository', () => {
       const CC = run.install(C)
       CC.deploy()
       // TODO: Parent approval
+    })
+
+    it('deploys with custom lock', () => {
+      class L {
+        script () { return new Uint8Array() }
+        domain () { return 0 }
+      }
+      const run = new Run()
+      class A {
+        static send (to) { this.owner = to }
+      }
+      A.send = () => { throw new Error('Must call methods on jigs') }
+      const CA = run.install(A)
+      // A.send(1)
+      CA.send(new L())
+      CA.deploy()
+      console.log(A)
+      expect(A.location.startsWith('record://'))
+    })
+  })
+
+  describe('get', () => {
+  })
+
+  describe('methods', () => {
+    // Code functions are not available inside functions
+  })
+
+  describe('upgrade', () => {
+
+  })
+
+  describe('sync', () => {
+    // Only waits for current record
+    // TODO: Check records
+
+    it('deploys a class and syncs it', async () => {
+      const run = new Run()
+      class A {}
+      await run.deploy(A)
+      const A2 = await run.load(A.location)
+      expect(A2.toString()).to.equal(A.toString())
+      expect(A2.origin).to.equal(A.origin)
+      expect(A2.location).to.equal(A.location)
+      console.log(A2)
+    })
+
+    it('publishes after dependent transaction', async () => {
+      const run = new Run()
+
+      class A {}
+      class B extends A { }
+      A.B = B
+
+      await run.deploy(A)
+
+      const t0 = new Date()
+      const A2 = await run.load(A.location)
+      const B2 = await run.load(B.location)
+      console.log(new Date() - t0)
+
+      class C extends B2 { }
+      await run.deploy(C)
+
+      // Deploy C fails
+      console.log(C.location, A2)
     })
   })
 })
