b849dc32c1d88db15a8d9b7ef16152baef92e89e
Brenton Gunning <brentongunning@gmail.com>
2020-07-12 16:53:32 -0700

    Bring back util

--- test/index.js
+++ test/index.js
@@ -7,12 +7,13 @@
 require('./extra/group-lock.js')
 
 require('./kernel/api.js')
-require('./kernel/bindings.js')
 require('./kernel/blockchain')
 require('./kernel/code.js')
-require('./kernel/log.js')
-require('./kernel/misc.js')
-require('./kernel/standard-lock.js')
 
 require('./module/mockchain')
 require('./module/viewer')
+
+require('./util/bindings.js')
+require('./util/log.js')
+require('./util/misc.js')
+require('./util/standard-lock.js')
