3b428e1ddc162b1a7e43c1e4b0e9608b29c4a655
Brenton Gunning <brentongunning@gmail.com>
2020-07-15 01:44:37 -0700

    code test with pass through

--- test/kernel/code2/code.js
+++ test/kernel/code2/code.js
@@ -1,7 +1,9 @@
 const { describe, it } = require('mocha')
+const Run = require('../../env/run')
+const { Code2: Code } = Run
 
 describe('Code', () => {
   it('should create', () => {
-    console.log('hello')
+    console.log(Code)
   })
 })
