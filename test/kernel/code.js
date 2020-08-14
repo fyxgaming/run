42117a85d40137e5b680ce370ae0da892f92289c
Brenton Gunning <brentongunning@gmail.com>
2020-08-14 16:18:14 -0600

    changes deps

--- test/kernel/code.js
+++ test/kernel/code.js
@@ -677,12 +677,98 @@ describe('Code', () => {
 
     // ------------------------------------------------------------------------
 
-    it('changes deps', () => {
+    it('changes props', async () => {
+      const run = new Run()
+
+      class A { }
+      A.x = 1
+      const CA = run.deploy(A)
+      await CA.sync()
+
+      class B { }
+      B.y = 2
+
+      function test (CA) {
+        expect(typeof CA.x).to.equal('undefined')
+        expect(CA.y).to.equal(2)
+      }
+
+      expectTx({
+        nin: 1,
+        nref: 0,
+        nout: 1,
+        ndel: 0,
+        ncre: 0,
+        exec: [
+          {
+            op: 'UPGRADE',
+            data: [
+              { $jig: 0 },
+              'class B { }',
+              { y: 2 }
+            ]
+          }
+        ]
+      })
+
+      expect(CA.upgrade(B)).to.equal(CA)
+      await CA.sync()
+      test(CA)
+
+      const CA2 = await run.load(CA.location)
+      test(CA2)
+
+      run.cache = new LocalCache()
+      const CA3 = await run.load(CA.location)
+      test(CA3)
     })
 
     // ------------------------------------------------------------------------
 
-    it('changes props', () => {
+    it('changes deps', async () => {
+      const run = new Run()
+
+      function f () { return [typeof a, typeof b] }
+      f.deps = { a: 1 }
+      const cf = run.deploy(f)
+      await cf.sync()
+
+      function g () { return [typeof a, typeof b] }
+      g.deps = { b: 2 }
+
+      function test (cf) {
+        expect(cf()[0]).to.equal('undefined')
+        expect(cf()[1]).to.equal('number')
+      }
+
+      expectTx({
+        nin: 1,
+        nref: 0,
+        nout: 1,
+        ndel: 0,
+        ncre: 0,
+        exec: [
+          {
+            op: 'UPGRADE',
+            data: [
+              { $jig: 0 },
+              g.toString(),
+              { deps: { b: 2 } }
+            ]
+          }
+        ]
+      })
+
+      expect(cf.upgrade(g)).to.equal(cf)
+      await cf.sync()
+      test(cf)
+
+      const cf2 = await run.load(cf.location)
+      test(cf2)
+
+      run.cache = new LocalCache()
+      const cf3 = await run.load(cf.location)
+      test(cf3)
     })
 
     // ------------------------------------------------------------------------
@@ -693,6 +779,12 @@ describe('Code', () => {
       expect(() => Code.prototype.upgrade.call(class A { }, class A { })).to.throw(error)
       expect(() => Code.prototype.upgrade.call(null, class A { })).to.throw(error)
     })
+
+    // ------------------------------------------------------------------------
+
+    // Throws if illegal
+    // Upgrade multiple in a batch
+    // Cannot upgrade inside a method
   })
 
   describe.skip('upgrade', () => {
