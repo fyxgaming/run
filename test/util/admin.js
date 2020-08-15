89f5bbd16fb0326c623259e3d1d07ac1fba6c67d
Brenton Gunning <brentongunning@gmail.com>
2020-08-15 15:19:37 -0600

    supports nested sudo

--- test/util/admin.js
+++ test/util/admin.js
@@ -1,2 +1,51 @@
+/**
+ * admin.js
+ * 
+ * Tests for lib/util/admin.js
+ */
+
+const { describe, it } = require('mocha')
+const { expect } = require('chai')
+const Run = require('../env/run')
+const unmangle = require('../env/unmangle')
+const { _sudo } = require('../../lib/run')
+const { _admin, sudo } = unmangle(Run)
+
+// ------------------------------------------------------------------------------------------------
+// Admin
+// ------------------------------------------------------------------------------------------------
+
+describe('Admin', () => {
+    describe('_admin', () => {
+        it('should return false by default', () => {
+            expect(_admin()).to.equal(false)
+        })
+
+        it('should return true only within sudo', () => {
+            expect(_admin()).to.equal(false)
+            _sudo(() => {
+                expect(_admin()).to.equal(true)
+            })
+            expect(_admin()).to.equal(false)
+        })
+    })
+    
+    describe('_sudo', () => {
+        it('supports nested sudo', () => {
+            expect(_admin()).to.equal(false)
+            _sudo(() => {
+                expect(_admin()).to.equal(true)
+                _sudo(() => {
+                    expect(_admin()).to.equal(true)
+                })
+                expect(_admin()).to.equal(true)
+            })
+            expect(_admin()).to.equal(false)
+        })
+    })
+})
+
+// ------------------------------------------------------------------------------------------------
+
 // TODO
 // Then SEALED
