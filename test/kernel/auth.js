569194b21c7fcfa721c44aa12e127df43ff79947
Brenton Gunning <brentongunning@gmail.com>
2020-08-15 21:55:06 -0600

    auth and destroy rollback

--- test/kernel/auth.js
+++ test/kernel/auth.js
@@ -10,6 +10,7 @@ const { expect } = require('chai')
 const Run = require('../env/run')
 const { expectTx } = require('../env/misc')
 const unmangle = require('../env/unmangle')
+const { stub } = require('sinon')
 const { Code, LocalCache } = unmangle(Run)
 
 // ------------------------------------------------------------------------------------------------
@@ -22,142 +23,170 @@ describe('Auth', () => {
   // Deactivate the current run instance. This stops leaks across tests.
   afterEach(() => Run.instance && Run.instance.deactivate())
 
-  it('spends code', async () => {
-    const run = new Run()
+  // --------------------------------------------------------------------------
+  // Code
+  // --------------------------------------------------------------------------
 
-    class A { }
-    const CA = run.deploy(A)
-    await CA.sync()
+  describe('Code', () => {
+    it('spends code', async () => {
+      const run = new Run()
 
-    function test (CA) {
-      expect(CA.origin).not.to.equal(CA.location)
-    }
+      class A { }
+      const CA = run.deploy(A)
+      await CA.sync()
 
-    expectTx({
-      nin: 1,
-      nref: 0,
-      nout: 1,
-      ndel: 0,
-      ncre: 0,
-      exec: [
-        {
-          op: 'AUTH',
-          data: { $jig: 0 }
-        }
-      ]
+      function test (CA) {
+        expect(CA.origin).not.to.equal(CA.location)
+      }
+
+      expectTx({
+        nin: 1,
+        nref: 0,
+        nout: 1,
+        ndel: 0,
+        ncre: 0,
+        exec: [
+          {
+            op: 'AUTH',
+            data: { $jig: 0 }
+          }
+        ]
+      })
+
+      expect(CA.auth()).to.equal(CA)
+      await CA.sync()
+      test(CA)
+
+      const CA2 = await run.load(CA.location)
+      test(CA2)
+
+      run.cache = new LocalCache()
+      const CA3 = await run.load(CA.location)
+      test(CA3)
     })
 
-    expect(CA.auth()).to.equal(CA)
-    await CA.sync()
-    test(CA)
+    // --------------------------------------------------------------------------
 
-    const CA2 = await run.load(CA.location)
-    test(CA2)
+    it('cannot auth non-jig children', async () => {
+      const run = new Run()
 
-    run.cache = new LocalCache()
-    const CA3 = await run.load(CA.location)
-    test(CA3)
+      class A { }
+      const CA = run.deploy(A)
+      await CA.sync()
+
+      class B extends CA { }
+      expect(() => B.auth()).to.throw('Auth unavailable')
+    })
+
+    // --------------------------------------------------------------------------
+
+    it('throws if auth jig destroyed in another transaction', async () => {
+      const run = new Run()
+
+      class A { }
+      const CA = run.deploy(A)
+      await CA.sync()
+
+      CA.destroy()
+      await CA.sync()
+
+      expect(() => CA.auth()).to.throw('Cannot auth destroyed jig')
+    })
+
+    // --------------------------------------------------------------------------
+
+    it('auth jig not synced', async () => {
+      const run = new Run()
+      class A { }
+      const CA = run.deploy(A)
+      CA.auth()
+      await CA.sync()
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('auth in a static method', () => {
+
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('auth code in a jig method', () => {
+
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('auth multiple in a batch', () => {
+
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('create and auth in same transaction', () => {
+
+    })
   })
 
-  // ------------------------------------------------------------------------
+  // --------------------------------------------------------------------------
+  // Jig
+  // --------------------------------------------------------------------------
 
-  it('cannot auth non-jig children', async () => {
-    const run = new Run()
-
-    class A { }
-    const CA = run.deploy(A)
-    await CA.sync()
-
-    class B extends CA { }
-    expect(() => B.auth()).to.throw('Auth unavailable')
+  describe('Jig', () => {
+    it.skip('auths jig', () => {
+      // TODO
+    })
   })
 
-  // ------------------------------------------------------------------------
+  // --------------------------------------------------------------------------
+  // Errors
+  // --------------------------------------------------------------------------
 
-  it('throws if auth jig destroyed in another transaction', async () => {
-    const run = new Run()
+  describe('errors', () => {
+    it('cannot auth non-code', () => {
+      const error = 'Auth unavailable'
+      expect(() => Code.prototype.auth.call({})).to.throw(error)
+      expect(() => Code.prototype.auth.call(class A { })).to.throw(error)
+      expect(() => Code.prototype.auth.call(null)).to.throw(error)
+    })
 
-    class A { }
-    const CA = run.deploy(A)
-    await CA.sync()
+    // ------------------------------------------------------------------------
 
-    CA.destroy()
-    await CA.sync()
+    it('rollback if error', async () => {
+      const run = new Run()
+      class A { }
+      const CA = run.deploy(A)
+      await CA.sync()
+      stub(run.blockchain, 'broadcast').throwsException()
+      CA.auth()
+      await expect(CA.sync()).to.be.rejected
+      expect(CA.location).to.equal(CA.origin)
+      expect(CA.nonce).to.equal(1)
+    })
 
-    expect(() => CA.auth()).to.throw('Cannot auth destroyed jig')
-  })
+    // ------------------------------------------------------------------------
 
-  // ------------------------------------------------------------------------
+    it.skip('throws if auth jig destroyed in same transaction', () => {
 
-  it('auth jig not synced', async () => {
-    const run = new Run()
-    class A { }
-    const CA = run.deploy(A)
-    CA.auth()
-    await CA.sync()
-  })
+    })
 
-  // ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------
 
-  it('cannot auth non-code', () => {
-    const error = 'Auth unavailable'
-    expect(() => Code.prototype.auth.call({})).to.throw(error)
-    expect(() => Code.prototype.auth.call(class A { })).to.throw(error)
-    expect(() => Code.prototype.auth.call(null)).to.throw(error)
-  })
+    it.skip('throws if auth new jig', () => {
 
-  // ------------------------------------------------------------------------
+    })
 
-  it.skip('throws if auth jig destroyed in same transaction', () => {
+    // ------------------------------------------------------------------------
 
-  })
+    it.skip('throws if auth transferred jig', () => {
 
-  // ------------------------------------------------------------------------
+    })
 
-  it.skip('auth in a static method', () => {
+    // ------------------------------------------------------------------------
 
-  })
-
-  // ------------------------------------------------------------------------
-
-  it.skip('auth code in a jig method', () => {
-
-  })
-
-  // ------------------------------------------------------------------------
-
-  it.skip('auth multiple in a batch', () => {
-
-  })
-
-  // ------------------------------------------------------------------------
-
-  it.skip('create and auth in same transaction', () => {
-
-  })
-
-  // ------------------------------------------------------------------------
-
-  it.skip('throws if auth new jig', () => {
-
-  })
-
-  // ------------------------------------------------------------------------
-
-  it.skip('throws if auth transferred jig', () => {
-
-  })
-
-  // ------------------------------------------------------------------------
-
-  it.skip('rollback if error', () => {
-
-  })
-
-  // ------------------------------------------------------------------------
-
-  it.skip('cannot auth undeployed berry class', () => {
+    it.skip('cannot auth undeployed berry class', () => {
     // TODO
+    })
   })
 })
 
--- test/kernel/destroy.js
+++ test/kernel/destroy.js
@@ -10,6 +10,7 @@ const { expect } = require('chai')
 const Run = require('../env/run')
 const { expectTx } = require('../env/misc')
 const unmangle = require('../env/unmangle')
+const { stub } = require('sinon')
 const { Code, LocalCache } = unmangle(Run)
 
 // ------------------------------------------------------------------------------------------------
@@ -22,121 +23,149 @@ describe('Destroy', () => {
   // Deactivate the current run instance. This stops leaks across tests.
   afterEach(() => Run.instance && Run.instance.deactivate())
 
-  it('destroys code', async () => {
-    const run = new Run()
+  // --------------------------------------------------------------------------
+  // Code
+  // --------------------------------------------------------------------------
 
-    class A { }
-    const CA = run.deploy(A)
-    await CA.sync()
+  describe('Code', () => {
+    it('destroys code', async () => {
+      const run = new Run()
 
-    function test (CA) {
-      expect(CA.location.endsWith('_d0')).to.equal(true)
+      class A { }
+      const CA = run.deploy(A)
+      await CA.sync()
+
+      function test (CA) {
+        expect(CA.location.endsWith('_d0')).to.equal(true)
+        expect(CA.owner).to.equal(null)
+        expect(CA.satoshis).to.equal(0)
+      }
+
+      expectTx({
+        nin: 1,
+        nref: 0,
+        nout: 0,
+        ndel: 1,
+        ncre: 0,
+        exec: [
+          {
+            op: 'DESTROY',
+            data: { $jig: 0 }
+          }
+        ]
+      })
+
+      expect(CA.destroy()).to.equal(CA)
       expect(CA.owner).to.equal(null)
       expect(CA.satoshis).to.equal(0)
-    }
 
-    expectTx({
-      nin: 1,
-      nref: 0,
-      nout: 0,
-      ndel: 1,
-      ncre: 0,
-      exec: [
-        {
-          op: 'DESTROY',
-          data: { $jig: 0 }
-        }
-      ]
+      await CA.sync()
+      test(CA)
+
+      const CA2 = await run.load(CA.location)
+      test(CA2)
+
+      run.cache = new LocalCache()
+      const CA3 = await run.load(CA.location)
+      test(CA3)
     })
 
-    expect(CA.destroy()).to.equal(CA)
-    expect(CA.owner).to.equal(null)
-    expect(CA.satoshis).to.equal(0)
+    // ------------------------------------------------------------------------
 
-    await CA.sync()
-    test(CA)
+    it('destroy twice', async () => {
+      const run = new Run()
+      class A { }
+      const CA = run.deploy(A)
+      await CA.sync()
 
-    const CA2 = await run.load(CA.location)
-    test(CA2)
+      CA.destroy()
+      await CA.sync()
+      const lastLocation = CA.location
 
-    run.cache = new LocalCache()
-    const CA3 = await run.load(CA.location)
-    test(CA3)
+      expect(CA.destroy()).to.equal(CA)
+      await CA.sync()
+      expect(CA.location).to.equal(lastLocation)
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('destroy in a static method', () => {
+
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('destroy code in a jig method', () => {
+
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('destroy multiple in a batch', () => {
+
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('create and destroy in same transaction', () => {
+
+    })
   })
 
-  // ------------------------------------------------------------------------
+  // --------------------------------------------------------------------------
+  // Jig
+  // --------------------------------------------------------------------------
 
-  it('cannot destroy non-jig children', async () => {
-    const run = new Run()
-
-    class A { }
-    const CA = run.deploy(A)
-    await CA.sync()
-
-    class B extends CA { }
-    expect(() => B.destroy()).to.throw('Destroy unavailable')
+  describe('Jig', () => {
+    it.skip('destroys jig', () => {
+      // TODO
+    })
   })
 
-  // ------------------------------------------------------------------------
+  // --------------------------------------------------------------------------
+  // Errors
+  // --------------------------------------------------------------------------
 
-  it('destroy twice', async () => {
-    const run = new Run()
-    class A { }
-    const CA = run.deploy(A)
-    await CA.sync()
+  describe('errors', () => {
+    it('cannot destroy non-code', () => {
+      const error = 'Destroy unavailable'
+      expect(() => Code.prototype.destroy.call({})).to.throw(error)
+      expect(() => Code.prototype.destroy.call(class A { })).to.throw(error)
+      expect(() => Code.prototype.destroy.call(null)).to.throw(error)
+    })
 
-    CA.destroy()
-    await CA.sync()
-    const lastLocation = CA.location
+    // ------------------------------------------------------------------------
 
-    expect(CA.destroy()).to.equal(CA)
-    await CA.sync()
-    expect(CA.location).to.equal(lastLocation)
-  })
+    it('cannot destroy non-code children', async () => {
+      const run = new Run()
 
-  // ------------------------------------------------------------------------
+      class A { }
+      const CA = run.deploy(A)
+      await CA.sync()
 
-  it('cannot destroy non-code', () => {
-    const error = 'Destroy unavailable'
-    expect(() => Code.prototype.destroy.call({})).to.throw(error)
-    expect(() => Code.prototype.destroy.call(class A { })).to.throw(error)
-    expect(() => Code.prototype.destroy.call(null)).to.throw(error)
-  })
+      class B extends CA { }
+      expect(() => B.destroy()).to.throw('Destroy unavailable')
+    })
 
-  // ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------
 
-  it.skip('destroy in a static method', () => {
+    it('rollback if error', async () => {
+      const run = new Run()
+      class A { }
+      const CA = run.deploy(A)
+      await CA.sync()
+      stub(run.blockchain, 'broadcast').throwsException()
+      CA.destroy()
+      await expect(CA.sync()).to.be.rejected
+      expect(CA.location.endsWith('_d0')).to.equal(false)
+      expect(CA.nonce).to.equal(1)
+    })
 
-  })
+    // ------------------------------------------------------------------------
 
-  // ------------------------------------------------------------------------
-
-  it.skip('destroy code in a jig method', () => {
-
-  })
-
-  // ------------------------------------------------------------------------
-
-  it.skip('destroy multiple in a batch', () => {
-
-  })
-
-  // ------------------------------------------------------------------------
-
-  it.skip('create and destroy in same transaction', () => {
-
-  })
-
-  // ------------------------------------------------------------------------
-
-  it.skip('rollback if error', () => {
-
-  })
-
-  // ------------------------------------------------------------------------
-
-  it.skip('cannot auth undeployed berry class', () => {
+    it.skip('cannot auth undeployed berry class', () => {
     // TODO
+    })
   })
 })
 
