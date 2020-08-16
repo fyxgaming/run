e6fabe359561a852317a56822af8dcb6848c18fc
Brenton Gunning <brentongunning@gmail.com>
2020-08-16 00:19:13 -0600

    Add new tests

--- test/util/queue.js
+++ test/util/queue.js
@@ -1,6 +1,6 @@
 /**
  * queue.js
- * 
+ *
  * Tests for lib/util/queue.js
  */
 
@@ -16,48 +16,48 @@ const { _SerialTaskQueue } = unmangle(Run)
 // ------------------------------------------------------------------------------------------------
 
 describe('SerialTaskQueue', () => {
-    it('runs single task', async () => {
-        const queue = unmangle(new _SerialTaskQueue())
-        let done = false
-        const ret = await queue._enqueue(async () => { done = true; return 1 })
-        expect(done).to.equal(true)
-        expect(ret).to.equal(1)
+  it('runs single task', async () => {
+    const queue = unmangle(new _SerialTaskQueue())
+    let done = false
+    const ret = await queue._enqueue(async () => { done = true; return 1 })
+    expect(done).to.equal(true)
+    expect(ret).to.equal(1)
+  })
+
+  // --------------------------------------------------------------------------------------------
+
+  it('serializes multiple tasks', async () => {
+    const queue = unmangle(new _SerialTaskQueue())
+    let first = false
+    let second = false
+    queue._enqueue(async () => {
+      expect(first).to.equal(false)
+      expect(second).to.equal(false)
+      first = true
     })
-
-    // --------------------------------------------------------------------------------------------
-
-    it('serializes multiple tasks', async () => {
-        const queue = unmangle(new _SerialTaskQueue())
-        let first = false
-        let second = false
-        queue._enqueue(async () => {
-            expect(first).to.equal(false)
-            expect(second).to.equal(false)
-            first = true
-        })
-        await queue._enqueue(async () => {
-            expect(first).to.equal(true)
-            expect(second).to.equal(false)
-            second = true
-        })
-        expect(first).to.equal(true)
-        expect(second).to.equal(true)
+    await queue._enqueue(async () => {
+      expect(first).to.equal(true)
+      expect(second).to.equal(false)
+      second = true
     })
+    expect(first).to.equal(true)
+    expect(second).to.equal(true)
+  })
 
-    // --------------------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------------------------
 
-    it('rethrows error', async () => {
-        const queue = unmangle(new _SerialTaskQueue())
-        await expect(queue._enqueue(async () => { throw new Error() })).to.be.rejected
-    })
+  it('rethrows error', async () => {
+    const queue = unmangle(new _SerialTaskQueue())
+    await expect(queue._enqueue(async () => { throw new Error() })).to.be.rejected
+  })
 
-    // --------------------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------------------------
 
-    it('supports non-promise tasks', async () => {
-        const queue = unmangle(new _SerialTaskQueue())
-        const ret = await queue._enqueue(() => 1)
-        expect(ret).to.equal(1)
-    })
+  it('supports non-promise tasks', async () => {
+    const queue = unmangle(new _SerialTaskQueue())
+    const ret = await queue._enqueue(() => 1)
+    expect(ret).to.equal(1)
+  })
 })
 
 // ------------------------------------------------------------------------------------------------
