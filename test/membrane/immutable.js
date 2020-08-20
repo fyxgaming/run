1298114bcfbcaea0e12592e592483d95910a9c64
Brenton Gunning <brentongunning@gmail.com>
2020-08-19 23:08:34 -0600

    Immutable test placeholder

--- test/membrane/immutable.js
+++ test/membrane/immutable.js
@@ -3,3 +3,22 @@
  *
  * Tests for lib/membrane/immutable.js
  */
+
+const { describe, it } = require('mocha')
+const { expect } = require('chai')
+const Run = require('../env/run')
+const unmangle = require('../env/unmangle')
+const Immutable = unmangle(unmangle(Run)._membrane)._Immutable
+
+// ------------------------------------------------------------------------------------------------
+// Immutable
+// ------------------------------------------------------------------------------------------------
+
+describe('Immutable', () => {
+  it('test', () => {
+    expect(Immutable).not.to.equal(undefined)
+    console.log(Immutable)
+  })
+})
+
+// ------------------------------------------------------------------------------------------------
