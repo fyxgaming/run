6106244aca2367bb5ac6578b3257c4073e7a4ffe
Brenton Gunning <brentongunning@gmail.com>
2020-08-19 18:04:05 -0600

    Intrinsics WIP

--- test/membrane/intrinsics.js
+++ test/membrane/intrinsics.js
@@ -9,11 +9,15 @@ const { describe, it } = require('mocha')
 const Run = require('../env/run')
 const unmangle = require('../env/unmangle')
 const Intrinsics = unmangle(unmangle(Run)._membrane)._Intrinsics
+const Proxy = unmangle(Run)._Proxy
 
 describe('Intrinsics', () => {
   describe('Set', () => {
     it('hello', () => {
-      console.log(Intrinsics)
+      const p = new Proxy(new Set(), new Intrinsics())
+      console.log(p, p.has(1), p.size)
+      p.add(1)
+      console.log(p, p.has(1), p.size)
     })
   })
 
