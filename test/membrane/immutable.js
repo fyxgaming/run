64c2c9e510e9825cdb5a31f1b17b21edada3882e
Brenton Gunning <brentongunning@gmail.com>
2020-08-19 23:50:27 -0600

    WIP immutable test

--- test/membrane/immutable.js
+++ test/membrane/immutable.js
@@ -9,15 +9,31 @@ const { expect } = require('chai')
 const Run = require('../env/run')
 const unmangle = require('../env/unmangle')
 const Immutable = unmangle(unmangle(Run)._membrane)._Immutable
+const Intrinsics = unmangle(unmangle(Run)._membrane)._Intrinsics
+const Proxy = unmangle(Run)._Proxy
 
 // ------------------------------------------------------------------------------------------------
 // Immutable
 // ------------------------------------------------------------------------------------------------
 
 describe('Immutable', () => {
-  it('test', () => {
+  it.only('test', () => {
     expect(Immutable).not.to.equal(undefined)
     console.log(Immutable)
+
+    const q = new Proxy({ a: [] }, new Immutable())
+    // q.n = 1
+    // console.log(q.n)
+    // q.a.push(1)
+    console.log(q)
+
+    const p = new Proxy(new Set(), new Immutable(new Intrinsics()))
+    // const p = new Proxy(new Set(), new Immutable())
+    // console.log(p)
+    p.add(1)
+    console.log(p.has(1))
+
+    // console.log(Intrinsics)
   })
 })
 
