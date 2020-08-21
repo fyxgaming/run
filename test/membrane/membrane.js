bcbb6fa28e659857991080d14192e5a8303bbfe8
Brenton Gunning <brentongunning@gmail.com>
2020-08-21 15:09:00 -0600

    TODO

--- test/membrane/membrane.js
+++ test/membrane/membrane.js
@@ -3,15 +3,37 @@
  */
 
 const { describe, it } = require('mocha')
+const { expect } = require('chai')
 const Run = require('../env/run')
 const unmangle = require('../env/unmangle')
-const Membrane = unmangle(Run)._Membrane
+const Proxy2 = unmangle(Run)._Proxy2
 
 describe('Membrane', () => {
   it('test', () => {
-    const p = new Membrane({})
-    p.n = 1
-    console.log(p)
+    const h = {}
+    h._nativeGetMethod = () => { console.log('  native get method') }
+    h._nativeIn = x => { console.log('  native in', x); return x }
+    h._nativeOut = x => { console.log('  native out', x); return x }
+    h._nativeRead = () => { console.log('  native read') }
+    h._nativeUpdate = () => { console.log('  native update') }
+
+    const p = new Proxy2(new Set(), h)
+
+    console.log('add 1')
+    expect(p.add(1)).to.equal(p)
+
+    console.log('for each')
+    p.forEach(x => console.log(' ', x))
+
+    console.log('print set')
+    console.log(' ', p)
+
+    console.log('checking same method')
+    expect(p.set).to.equal(p.set)
+
+    console.log('another object')
+    const addMethod = p.add
+    addMethod.call(new Set(), 1)
 
     // const p = new Membrane(new Set())
     // p.add(1)
