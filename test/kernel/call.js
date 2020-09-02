2c8330cf866c321793350bd5013165db1ab6392e
Brenton Gunning <brentongunning@gmail.com>
2020-09-01 22:45:25 -0700

    first call test that fails

--- test/kernel/call.js
+++ test/kernel/call.js
@@ -1,19 +1,99 @@
 /**
  * call.js
- * 
+ *
  * Tests for the call action
  */
 
 const { describe, it } = require('mocha')
+const { expect } = require('chai')
+const Run = require('../env/run')
+const { Jig } = Run
 
 // ------------------------------------------------------------------------------------------------
-// Call 
+// Call
 // ------------------------------------------------------------------------------------------------
 
 describe('Call', () => {
-    it('test', () => {
-        console.log('hello')
+  it('calls static get method on jig', async () => {
+    const run = new Run()
+    class A extends Jig { static f (x) { return 123 + x } }
+    const C = run.deploy(A)
+    await C.sync()
+    expect(C.f(1)).to.equal(124)
+    expect(C.origin).to.equal(C.location)
+  })
+
+  /*
+    it('calls static set method on jig', async () => {
+      const run = new Run()
+      class A extends Jig { static f (x) { this.x = x } }
+      const C = run.deploy(A)
+      await C.sync()
+      C.f(1)
+      expect(C.x).to.equal(1)
+      await C.sync()
+      expect(C.location).not.to.equal(C.origin)
+
+      run.deactivate()
+      const run2 = new Run({ blockchain: run.blockchain })
+      const C2 = await run2.load(C.location)
+
+      expect(C.location).to.equal(C2.location)
+      expect(C.x).to.equal(C2.x)
     })
+
+    // ------------------------------------------------------------------------
+
+    // TODO: Move to deploy
+    it('allowed to set native class as dependency', async () => {
+      const run = new Run()
+      class A extends Jig { static f (x) { this.x = x } }
+      A.deps = { Jig }
+      const C = run.deploy(A)
+      await C.sync()
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('calls static method with passthrough and without this on arbitrary code', async () => {
+      const run = new Run()
+      class A {
+        static f (x) {
+          if (x !== Symbol.hasInstance) throw new Error()
+          if (this) throw new Error()
+          return Symbol.iterator
+        }
+      }
+      const C = run.deploy(A)
+      await C.sync()
+      expect(C.f(Symbol.hasInstance)).to.equal(Symbol.iterator)
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('can only call static methods on class they are from', async () => {
+      const run = new Run()
+
+      class A extends Jig {
+        static f () { this.f = 'a' }
+        static g () { this.g = 'a' }
+      }
+
+      class B extends A {
+        static g () { this.g = 'b' }
+        static h () { this.h = 'b' }
+      }
+
+      const CA = run.deploy(A)
+      await CA.sync()
+
+      const CB = run.deploy(B)
+      await CB.sync()
+      // CB.h()
+      // await CB.sync()
+      // console.log(CB)
+    })
+    */
 })
 
 // ------------------------------------------------------------------------------------------------
