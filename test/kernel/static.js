14d2ed6b2e1f0af211270bd01eaf9caf1a34ca57
Brenton Gunning <brentongunning@gmail.com>
2020-09-02 00:51:26 -0700

    no this inside

--- test/kernel/static.js
+++ test/kernel/static.js
@@ -4,20 +4,44 @@
  * Tests for static code
  */
 
-const { describe, afterEach } = require('mocha')
+const { describe, it, afterEach } = require('mocha')
+const { expect } = require('chai')
 const Run = require('../env/run')
 
 // ------------------------------------------------------------------------------------------------
-// Static
+// Static Code
 // ------------------------------------------------------------------------------------------------
 
-describe('Static', () => {
+describe('Static Code', () => {
   // Wait for every test to finish. This makes debugging easier.
   afterEach(() => Run.instance && Run.instance.sync())
   // Deactivate the current run instance. This stops leaks across tests.
   afterEach(() => Run.instance && Run.instance.deactivate())
 
-  // TODO
+  it('can return unserializables', () => {
+    const run = new Run()
+    function f () { return /abc/ }
+    const cf = run.deploy(f)
+    expect(() => cf()).not.to.throw()
+  })
+
+  // --------------------------------------------------------------------------
+
+  it('can pass unserializable args', () => {
+    const run = new Run()
+    function f () { }
+    const cf = run.deploy(f)
+    expect(() => cf(Promise.resolve())).not.to.throw()
+  })
+
+  // --------------------------------------------------------------------------
+
+  it('no this inside', () => {
+    const run = new Run()
+    function f () { return this }
+    const cf = run.deploy(f)
+    expect(cf()).to.equal(undefined)
+  })
 })
 
 // ------------------------------------------------------------------------------------------------
