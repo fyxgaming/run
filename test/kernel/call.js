5840b2366c48fadc6fe91b26e3c6d8f2dcba5dba
Brenton Gunning <brentongunning@gmail.com>
2020-09-01 23:54:48 -0700

    static code test

--- test/kernel/call.js
+++ test/kernel/call.js
@@ -19,48 +19,68 @@ describe('Call', () => {
   // Deactivate the current run instance. This stops leaks across tests.
   afterEach(() => Run.instance && Run.instance.deactivate())
 
-  it('calls static get method on jig', async () => {
-    const run = new Run()
-    class A extends Jig { static f (x) { return 123 + x } }
-    const C = run.deploy(A)
-    await C.sync()
-    const location = C.location
-    expect(C.f(1)).to.equal(124)
-    expect(C.origin).to.equal(C.location)
-    expect(C.location).to.equal(location)
-  })
+  // --------------------------------------------------------------------------
+  // Code
+  // --------------------------------------------------------------------------
 
-  // ------------------------------------------------------------------------
-
-  it('calls static set method on jig', async () => {
-    const run = new Run()
-    class A extends Jig { static f (x) { this.x = x } }
-    const C = run.deploy(A)
-    await C.sync()
-    C.f(1)
-    expect(C.x).to.equal(1)
-    await C.sync()
-    expect(C.location).not.to.equal(C.origin)
-
-    function test (C2) {
-      expect(C.location).to.equal(C2.location)
-      expect(C.x).to.equal(C2.x)
-    }
-
-    const C2 = await run.load(C.location)
-    test(C2)
-
-    run.cache = new LocalCache()
-    const C3 = await run.load(C.location)
-    test(C3)
-  })
-
-  /*
+  describe('Code', () => {
+    it('calls static get method on jig', async () => {
+      const run = new Run()
+      class A extends Jig { static f (x) { return 123 + x } }
+      const C = run.deploy(A)
+      await C.sync()
+      const location = C.location
+      expect(C.f(1)).to.equal(124)
+      expect(C.origin).to.equal(C.location)
+      expect(C.location).to.equal(location)
+    })
 
     // ------------------------------------------------------------------------
 
-    it('calls static method with passthrough and without this on arbitrary code', async () => {
+    it('calls static set method on jig', async () => {
       const run = new Run()
+
+      class A extends Jig { static f (x) { this.x = x } }
+      const C = run.deploy(A)
+      await C.sync()
+
+      function test (C2) {
+        expect(C2.location).not.to.equal(C2.origin)
+        expect(C.location).to.equal(C2.location)
+        expect(C.x).to.equal(C2.x)
+      }
+
+      C.f(1)
+      expect(C.x).to.equal(1)
+      await C.sync()
+      test(C)
+
+      const C2 = await run.load(C.location)
+      test(C2)
+
+      run.cache = new LocalCache()
+      const C3 = await run.load(C.location)
+      test(C3)
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('throws for unsupported args', () => {
+      const run = new Run()
+      class A extends Jig { static f () { } }
+      const C = run.deploy(A)
+      expect(() => C.f(Symbol.hasInstance)).to.throw('Cannot clone')
+    })
+  })
+
+  // --------------------------------------------------------------------------
+  // Static Code
+  // --------------------------------------------------------------------------
+
+  describe('Static Code', () => {
+    it('calls method with passthrough and without this on arbitrary code', async () => {
+      const run = new Run()
+
       class A {
         static f (x) {
           if (x !== Symbol.hasInstance) throw new Error()
@@ -68,13 +88,27 @@ describe('Call', () => {
           return Symbol.iterator
         }
       }
+
+      function test (C2) {
+        expect(C2.f(Symbol.hasInstance)).to.equal(Symbol.iterator)
+      }
+
       const C = run.deploy(A)
       await C.sync()
-      expect(C.f(Symbol.hasInstance)).to.equal(Symbol.iterator)
+      test(C)
+
+      const C2 = await run.load(C.location)
+      test(C2)
+
+      run.cache = new LocalCache()
+      const C3 = await run.load(C.location)
+      test(C3)
     })
+  })
 
-    // ------------------------------------------------------------------------
+  // --------------------------------------------------------------------------
 
+  /*
     it('can only call static methods on class they are from', async () => {
       const run = new Run()
 
