571c1cc6805403d7fb448fbb5e40cdad78e91b06
Brenton Gunning <brentongunning@gmail.com>
2020-09-02 00:54:39 -0700

    static functions

--- test/kernel/static.js
+++ test/kernel/static.js
@@ -18,29 +18,73 @@ describe('Static Code', () => {
   // Deactivate the current run instance. This stops leaks across tests.
   afterEach(() => Run.instance && Run.instance.deactivate())
 
-  it('can return unserializables', () => {
-    const run = new Run()
-    function f () { return /abc/ }
-    const cf = run.deploy(f)
-    expect(() => cf()).not.to.throw()
+  // --------------------------------------------------------------------------
+  // Static functions
+  // --------------------------------------------------------------------------
+
+  describe('Functions', () => {
+    it('can return unserializables', () => {
+      const run = new Run()
+      function f () { return /abc/ }
+      const cf = run.deploy(f)
+      expect(() => cf()).not.to.throw()
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('can pass unserializable args', () => {
+      const run = new Run()
+      function f () { }
+      const cf = run.deploy(f)
+      expect(() => cf(Promise.resolve())).not.to.throw()
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('no this inside', () => {
+      const run = new Run()
+      function f () { return this }
+      const cf = run.deploy(f)
+      expect(cf()).to.equal(undefined)
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('cannot set properties', () => {
+      const run = new Run()
+      function f () { f.n = 1 }
+      const cf = run.deploy(f)
+      expect(() => cf()).to.throw('set disabled')
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('cannot define properties', () => {
+      const run = new Run()
+      function f () {
+        const desc = { value: 1, configurable: true, enumerable: true, writable: true }
+        Object.defineProperty(f, 'n', desc)
+      }
+      const cf = run.deploy(f)
+      expect(() => cf()).to.throw('defineProperty disabled')
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('can call other static functions', () => {
+      const run = new Run()
+      function g () { return 1 }
+      function f () { return g() }
+      f.deps = { g }
+      const cf = run.deploy(f)
+      expect(cf()).to.equal(1)
+    })
   })
 
   // --------------------------------------------------------------------------
 
-  it('can pass unserializable args', () => {
-    const run = new Run()
-    function f () { }
-    const cf = run.deploy(f)
-    expect(() => cf(Promise.resolve())).not.to.throw()
-  })
-
-  // --------------------------------------------------------------------------
-
-  it('no this inside', () => {
-    const run = new Run()
-    function f () { return this }
-    const cf = run.deploy(f)
-    expect(cf()).to.equal(undefined)
+  describe('Classes', () => {
+      // TODO
   })
 })
 
