19f82ff30a4e30bb58c03391d21367f7e1ab6e3c
Brenton Gunning <brentongunning@gmail.com>
2020-09-02 17:04:33 -0700

    basic jig fails on replay

--- test/kernel/jig.js
+++ test/kernel/jig.js
@@ -4,33 +4,67 @@
  * Tests for lib/kernel/jig.js
  */
 
-const { describe, it } = require('mocha')
+const { describe, it, afterEach } = require('mocha')
 const Run = require('../env/run')
-const { testRecord } = require('../env/misc')
-const { Jig } = Run
+const { expectTx } = require('../env/misc')
+const { expect } = require('chai')
+const { Jig, LocalCache } = Run
 
 // ------------------------------------------------------------------------------------------------
 // Jig
 // ------------------------------------------------------------------------------------------------
 
 describe('Jig', () => {
+  // Wait for every test to finish. This makes debugging easier.
+  afterEach(() => Run.instance && Run.instance.sync())
+  // Deactivate the current run instance. This stops leaks across tests.
+  afterEach(() => Run.instance && Run.instance.deactivate())
+
+  // --------------------------------------------------------------------------
+  // Constructor
+  // --------------------------------------------------------------------------
+
   describe('constructor', () => {
     it('basic jig', async () => {
-      new Run() // eslint-disable-line
+      const run = new Run()
       class A extends Jig { }
-      testRecord(record => {
-        const a = new A() //eslint-disable-line
-        console.log(record._creates.length)
-      })
-      // await a.sync()
 
-      // TODO
-      /*
-      expectAction(a, 'init', [], [], [a], [])
-      expect(unmangle(run.code)._installs.has(A)).to.equal(true)
-      await run.sync()
-      expect(A.origin.length).to.equal(67)
-      */
+      expectTx({
+        nin: 0,
+        nref: 1,
+        nout: 2,
+        ndel: 0,
+        ncre: 2,
+        exec: [
+          {
+            op: 'DEPLOY',
+            data: [
+              'class A extends Jig { }',
+              { deps: { Jig: { $jig: 0 } } }
+            ]
+          },
+          {
+            op: 'NEW',
+            data: [{ $jig: 1 }, []]
+          }
+        ]
+      })
+
+      function test (a) {
+        expect(a instanceof Jig).to.equal(true)
+        expect(a.origin.length).to.equal(67)
+      }
+
+      const a = new A()
+      await a.sync()
+      test(a)
+
+      const a2 = await run.load(a.location)
+      test(a2)
+
+      run.cache = new LocalCache()
+      const a3 = await run.load(a.location)
+      test(a3)
     })
   })
 })
