26ac8f9ec2e6fab15fca6b819b4b2d20f1b778b6
Brenton Gunning <brentongunning@gmail.com>
2020-09-05 19:34:08 -0700

    jig upgrade inconsistency test

--- test/kernel/upgrade.js
+++ test/kernel/upgrade.js
@@ -1226,6 +1226,25 @@ describe('Upgrade', () => {
       a.f(3)
       expect(a.n).to.equal('error')
     })
+
+    // ------------------------------------------------------------------------
+
+    it('throws if inconsistent worldview', async () => {
+      const run = new Run()
+      class A extends Jig {
+        init (n) { this.n = 1 }
+        f () { return this.n }
+      }
+      const CA = run.deploy(A)
+      CA.auth()
+      await CA.sync()
+      const CO = await run.load(CA.origin)
+      expect(CA.location).not.to.equal(CO.location)
+      const a = new CA()
+      const b = new CO()
+      class C extends Jig { init (a, b) { this.n = a.f() + b.f() } }
+      expect(() => new C(a, b)).to.throw('Inconsistent worldview')
+    })
   })
 })
 
