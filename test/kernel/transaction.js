2694eb24b1bca03cb86599158bb90a979820a9d8
Brenton Gunning <brentongunning@gmail.com>
2020-09-07 02:05:24 -0700

    Transaction tests

--- test/kernel/transaction.js
+++ test/kernel/transaction.js
@@ -5,21 +5,46 @@
  */
 
 const { describe, it } = require('mocha')
+const { expect } = require('chai')
 const Run = require('../env/run')
-const { Jig } = Run
+const { Jig, Transaction } = Run
 
 // ------------------------------------------------------------------------------------------------
 // Transaction
 // ------------------------------------------------------------------------------------------------
 
 describe('Transaction', () => {
-  it('test', async () => {
+  it('basic', async () => {
     const run = new Run()
     class A extends Jig { }
     const [a, b] = run.transaction(() => [new A(), new A()])
     await run.sync()
-    console.log('a', a)
-    console.log('b', b)
+    expect(a.location.slice(0, 64)).to.equal(b.location.slice(0, 64))
+  })
+
+  // --------------------------------------------------------------------------
+
+  it('manual publish', async () => {
+    new Run() // eslint-disable-line
+    class A extends Jig { }
+    const tx = new Transaction()
+    const a = tx.update(() => new A())
+    const b = tx.update(() => new A())
+    await tx.publish()
+    expect(a.location.slice(0, 64)).to.equal(b.location.slice(0, 64))
+  })
+
+  // --------------------------------------------------------------------------
+
+  it('throws if update outside before publish', async () => {
+    new Run() // eslint-disable-line
+    class A extends Jig { f () { this.n = 1 } }
+    const tx = new Transaction()
+    const a = tx.update(() => new A())
+    expect(() => a.f()).to.throw('Cannot link')
+    await tx.publish()
+    a.f()
+    await a.sync()
   })
 })
 
