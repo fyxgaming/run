19cb0e1b86d134c7f6bd5bd09475e6dcad951b61
Brenton Gunning <brentongunning@gmail.com>
2020-09-07 23:50:28 -0700

    import timeout

--- test/kernel/timeout.js
+++ test/kernel/timeout.js
@@ -49,7 +49,17 @@ describe('Timeout', () => {
 
   // ------------------------------------------------------------------------
 
-  it('import', () => {
+  it('import', async () => {
+    const run = new Run()
+    class A extends Jig { inc () { this.n = (this.n || 0) + 1 } }
+    const a = new A()
+    for (let i = 0; i < 10; i++) { a.inc() }
+    await run.sync()
+    run.cache = new LocalCache()
+    run.timeout = 10
+    const txid = a.location.slice(0, 64)
+    const rawtx = await run.blockchain.fetch(txid)
+    await expect(run.import(rawtx)).to.be.rejectedWith('import timeout')
   })
 
   // ------------------------------------------------------------------------
