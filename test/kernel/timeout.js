808dd497506bbf176a9806629ce63bedad694068
Brenton Gunning <brentongunning@gmail.com>
2020-09-08 00:51:23 -0700

    the 5 timeout tests

--- test/kernel/timeout.js
+++ test/kernel/timeout.js
@@ -6,6 +6,7 @@
 
 const { describe, it, afterEach } = require('mocha')
 require('chai').use(require('chai-as-promised'))
+const { stub } = require('sinon')
 const { expect } = require('chai')
 const Run = require('../env/run')
 const { Jig, LocalCache } = Run
@@ -64,7 +65,14 @@ describe('Timeout', () => {
 
   // ------------------------------------------------------------------------
 
-  it('publish', () => {
+  it('publish', async () => {
+    const run = new Run()
+    class A extends Jig { }
+    const sleep = ms => new Promise((resolve, reject) => setTimeout(resolve, ms))
+    stub(run.purse, 'pay').callsFake(x => sleep(1000))
+    run.timeout = 10
+    const C = run.deploy(A)
+    await expect(C.sync()).to.be.rejectedWith('publish timeout')
   })
 
   // ------------------------------------------------------------------------
