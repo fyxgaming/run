fcd2f00221081476aae0f9f555a02c13c72f3166
Brenton Gunning <brentongunning@gmail.com>
2020-09-09 00:56:35 -0700

    first set of trust tests

--- test/kernel/trust.js
+++ test/kernel/trust.js
@@ -15,17 +15,18 @@ const { Jig, LocalCache } = Run
 // ------------------------------------------------------------------------------------------------
 
 describe('Trust', () => {
-  it('code whitelist', async () => {
-    const run = new Run()
-    class A extends Jig { }
-    run.deploy(A)
-    await run.sync()
+  // --------------------------------------------------------------------------
+  // constructor
+  // --------------------------------------------------------------------------
 
-    const run2 = new Run({ trust: [] })
-    run2.cache = new LocalCache()
-    await expect(run2.load(A.location)).to.be.rejectedWith('Cannot load untrusted code')
-    run2.trust(A.location.slice(0, 64))
-    await run2.load(A.location)
+  describe('constructor', () => {
+    it('throws if invalid', () => {
+      expect(() => new Run({ trust: null })).to.throw('Not trustable')
+      expect(() => new Run({ trust: {} })).to.throw('Not trustable')
+      expect(() => new Run({ trust: 'abc' })).to.throw('Not trustable')
+      expect(() => new Run({ trust: '0000000000000000000000000000000000000000000000000000000000000000_o0' })).to.throw('Not trustable')
+      expect(() => new Run({ trust: '**' })).to.throw('Not trustable')
+    })
   })
 
   // --------------------------------------------------------------------------
@@ -33,26 +34,39 @@ describe('Trust', () => {
   // --------------------------------------------------------------------------
 
   describe('trust', () => {
-    it('should trust *', () => {
-      // TODO
+    it('trust *', async () => {
+      const run = new Run()
+      const A = run.deploy(class A extends Jig { })
+      await run.sync()
+      const run2 = new Run({ trust: [] })
+      run2.cache = new LocalCache()
+      await expect(run2.load(A.location)).to.be.rejectedWith('Cannot load untrusted code')
+      run2.trust('*')
+      await run2.load(A.location)
     })
 
     // ------------------------------------------------------------------------
 
-    it('should trust location', () => {
-      // TODO
-    })
-
-    // ------------------------------------------------------------------------
-
-    it('should trust txid', () => {
-      // TODO
+    it('trust txid', async () => {
+      const run = new Run()
+      const A = run.deploy(class A extends Jig { })
+      await run.sync()
+      const run2 = new Run({ trust: [] })
+      run2.cache = new LocalCache()
+      await expect(run2.load(A.location)).to.be.rejectedWith('Cannot load untrusted code')
+      run2.trust(A.location.slice(0, 64))
+      await run2.load(A.location)
     })
 
     // ------------------------------------------------------------------------
 
     it('throws if invalid', () => {
-      // TODO
+      const run = new Run()
+      expect(() => run.trust(null)).to.throw('Not trustable')
+      expect(() => run.trust({})).to.throw('Not trustable')
+      expect(() => run.trust('abc')).to.throw('Not trustable')
+      expect(() => run.trust('0000000000000000000000000000000000000000000000000000000000000000_o0')).to.throw('Not trustable')
+      expect(() => run.trust('')).to.throw('Not trustable')
     })
   })
 
