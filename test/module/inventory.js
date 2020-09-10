29b663bd643e564d815f83403f22786d2ab8d940
Brenton Gunning <brentongunning@gmail.com>
2020-09-10 12:38:45 -0700

    update

--- test/module/inventory.js
+++ test/module/inventory.js
@@ -19,6 +19,10 @@ describe('Inventory', () => {
   // Deactivate the current run instance. This stops leaks across tests.
   afterEach(() => Run.instance && Run.instance.deactivate())
 
+  // --------------------------------------------------------------------------
+  // update
+  // --------------------------------------------------------------------------
+
   describe('update', () => {
     it('adds synced jigs', async () => {
       const run = new Run()
@@ -27,6 +31,16 @@ describe('Inventory', () => {
       await a.sync()
       expect(run.inventory.jigs).to.deep.equal([a])
       expect(run.inventory.code).to.deep.equal([run.install(A)])
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('does not add unsynced jigs', () => {
+      const run = new Run()
+      class A extends Jig { send (to) { this.owner = to } }
+      new A() // eslint-disable-line
+      expect(run.inventory.jigs.length).to.equal(0)
+      expect(run.inventory.code.length).to.equal(0)
     })
   })
 
