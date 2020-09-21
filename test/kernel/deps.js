afc161643c27fafe262400fa64ab79b58bbf645c
Brenton Gunning <brentongunning@gmail.com>
2020-09-20 18:51:55 -0700

    set inner deps from outside

--- test/kernel/deps.js
+++ test/kernel/deps.js
@@ -7,7 +7,7 @@
 const { describe, it, afterEach } = require('mocha')
 const { expect } = require('chai')
 const Run = require('../env/run')
-const { Jig } = Run
+const { Jig, LocalCache } = Run
 
 // ------------------------------------------------------------------------------------------------
 // Deps
@@ -24,7 +24,7 @@ describe('Deps', () => {
   // --------------------------------------------------------------------------
 
   describe('Code', () => {
-    it('set deps from inside', () => {
+    it('set deps from inside', async () => {
       const run = new Run()
       class A extends Jig {
         static f () { return B } // eslint-disable-line
@@ -32,14 +32,32 @@ describe('Deps', () => {
         static h () { A.deps.B = 3 } // eslint-disable-line
       }
       A.deps = { B: 1 }
+
       const CA = run.deploy(A)
       expect(CA.f()).to.equal(1)
+      await CA.sync()
+      expect(CA.nonce).to.equal(1)
+
       CA.g()
+      await CA.sync()
       expect(CA.f()).to.equal(2)
       expect(CA.deps.B).to.equal(2)
+      expect(CA.nonce).to.equal(2)
+
       CA.h()
       expect(CA.f()).to.equal(3)
       expect(CA.deps.B).to.equal(3)
+      await CA.sync()
+      expect(CA.nonce).to.equal(3)
+
+      const CA2 = await run.load(CA.location)
+      expect(CA2.f()).to.equal(3)
+      expect(CA2.deps.B).to.equal(3)
+
+      run.cache = new LocalCache()
+      const CA3 = await run.load(CA.location)
+      expect(CA3.f()).to.equal(3)
+      expect(CA3.deps.B).to.equal(3)
     })
 
     // ------------------------------------------------------------------------
@@ -49,19 +67,55 @@ describe('Deps', () => {
       class A extends Jig { }
       A.deps = { B: 1 }
       const CA = run.deploy(A)
-      expect(() => { CA.deps.B = 2 }).to.throw('Updates must be performed in the jig\'s methods')
+      expect(() => { CA.deps.B = 1 }).to.throw('Updates must be performed in the jig\'s methods')
     })
 
     // ------------------------------------------------------------------------
 
-    it.skip('set inner deps from inside', () => {
-      // TODO
+    it('set inner deps from inside', async () => {
+      const run = new Run()
+      class A extends Jig {
+        static f () { return B.n } // eslint-disable-line
+        static g () { B.n = 2 } // eslint-disable-line
+        static h () { A.deps.B.n = 3 } // eslint-disable-line
+      }
+      A.deps = { B: { n: 1 } }
+
+      const CA = run.deploy(A)
+      expect(CA.f()).to.equal(1)
+      await CA.sync()
+      expect(CA.nonce).to.equal(1)
+
+      CA.g()
+      await CA.sync()
+      expect(CA.f()).to.equal(2)
+      expect(CA.deps.B.n).to.equal(2)
+      expect(CA.nonce).to.equal(2)
+
+      CA.h()
+      expect(CA.f()).to.equal(3)
+      expect(CA.deps.B.n).to.equal(3)
+      await CA.sync()
+      expect(CA.nonce).to.equal(3)
+
+      const CA2 = await run.load(CA.location)
+      expect(CA2.f()).to.equal(3)
+      expect(CA2.deps.B.n).to.equal(3)
+
+      run.cache = new LocalCache()
+      const CA3 = await run.load(CA.location)
+      expect(CA3.f()).to.equal(3)
+      expect(CA3.deps.B.n).to.equal(3)
     })
 
     // ------------------------------------------------------------------------
 
-    it.skip('set inner deps from outside', () => {
-      // TODO
+    it('set inner deps from outside', () => {
+      const run = new Run()
+      class A extends Jig { }
+      A.deps = { B: [0] }
+      const CA = run.deploy(A)
+      expect(() => { CA.deps.B[0] = 1 }).to.throw('Updates must be performed in the jig\'s methods')
     })
 
     // ------------------------------------------------------------------------
