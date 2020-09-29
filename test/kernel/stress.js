3537abb79a43bcef1124999b29a73666d898b4ea
Brenton Gunning <brentongunning@gmail.com>
2020-09-28 18:16:01 -0700

    Fix import performance

--- test/kernel/stress.js
+++ test/kernel/stress.js
@@ -43,8 +43,65 @@ if (STRESS) {
 
     // -------------------------------------------------------------------------
 
-    it.skip('large graph', () => {
-      // TODO
+    it('neural graph', async () => {
+      const run = new Run()
+
+      class A extends Jig {
+        init (n, ...links) {
+          this.n = n
+          links.forEach(link => { this.n += link.n })
+        }
+      }
+
+      const width = 10
+      const cols = []
+      let n = 0
+
+      for (let x = 0; x < width; x++) {
+        const col = []
+        const prev = cols[cols.length - 1]
+        const height = width - x
+
+        n += height
+
+        for (let y = 0; y < height; y++) {
+          if (x === 0) {
+            const a = new A(y + 1)
+            col.push(a)
+          }
+
+          if (x !== 0) {
+            const a = new A(0, ...prev)
+            col.push(a)
+          }
+        }
+
+        cols.push(col)
+        await run.sync()
+      }
+
+      console.log('Loading ' + n + ' interconnected jigs forward')
+      const start = new Date()
+      for (let x = 0; x < width; x++) {
+        for (let y = 0; y < cols[x].length; y++) {
+          await run.load(cols[x][y].location)
+        }
+      }
+      const first = new Date() - start
+      console.log(first + 'ms')
+
+      console.log('Loading ' + n + ' interconnected jigs backward')
+      const start2 = new Date()
+      const last = cols[cols.length - 1][0]
+      run.cache = new LocalCache()
+      await run.load(last.location)
+      const second = new Date() - start2
+      console.log(second + 'ms')
+
+      // Make sure that loading backwards is still relatively fast
+      expect(first < 3000).to.equal(true)
+      expect(second < 3000).to.equal(true)
+      expect((second - first) / first < 10).to.equal(true)
     })
 
     // -------------------------------------------------------------------------
