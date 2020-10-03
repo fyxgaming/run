2300a2356e1ce9eb7981ad46b1b602ca85386cbb
Brenton Gunning <brentongunning@gmail.com>
2020-10-02 18:50:30 -0700

    Failing berry test

--- test/kernel/berry.js
+++ test/kernel/berry.js
@@ -4,16 +4,37 @@
  * Tests for lib/kernel/berry.js
  */
 
-const { describe, it } = require('mocha')
+const { describe, it, afterEach } = require('mocha')
+const Run = require('../env/run')
+const { Berry } = Run
 
 // ------------------------------------------------------------------------------------------------
 // Berry
 // ------------------------------------------------------------------------------------------------
 
 describe('Berry', () => {
-  it('test', () => {
-    console.log('hello')
+  // Wait for every test to finish. This makes debugging easier.
+  afterEach(() => Run.instance && Run.instance.sync())
+  // Deactivate the current run instance. This stops leaks across tests.
+  afterEach(() => Run.instance && Run.instance.deactivate())
+
+  it('basic berry', async () => {
+    const run = new Run()
+    class B extends Berry { static pluck () { return new B() } }
+    const CB = run.deploy(B)
+    await run.sync()
+    const b = await run.load('abc', CB)
+    console.log(b)
   })
+
+  // Tests
+  // - load with undeployed berry class
+  // - load with invalid string throws
+  // - load with non-berry class throws
+  // - immutable externally
+  // - immutable internally
+  // - can call functions
+  // - not a jig - no auth, destroy, sync, etc.
 })
 
 // ------------------------------------------------------------------------------------------------
