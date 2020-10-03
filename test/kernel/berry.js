9629d080b2c1e9a0340c27b4fa238441c629426f
Brenton Gunning <brentongunning@gmail.com>
2020-10-03 16:27:17 -0700

    more placeholder berry tests

--- test/kernel/berry.js
+++ test/kernel/berry.js
@@ -120,38 +120,10 @@ describe('Berry', () => {
     it.skip('caches future plucks', () => {
       // TODO
     })
-  })
-
-  // --------------------------------------------------------------------------
-  // init
-  // --------------------------------------------------------------------------
-
-  describe('fetch', () => {
-    it.skip('fetches raw transaction', () => {
-      // TODO
-    })
 
     // ------------------------------------------------------------------------
 
-    it.skip('sandboxed', () => {
-      // TODO
-    })
-
-    // ------------------------------------------------------------------------
-
-    it.skip('may fetch multiple transactions', () => {
-      // TODO
-    })
-
-    // ------------------------------------------------------------------------
-
-    it.skip('throws if not a transaction', () => {
-      // TODO
-    })
-
-    // ------------------------------------------------------------------------
-
-    it.skip('throws if blockchain timeout', () => {
+    it.skip('throws if init throws', () => {
       // TODO
     })
   })
@@ -211,6 +183,45 @@ describe('Berry', () => {
 
     it.skip('throws if create in a different plucker', () => {
       // TODO
+    })
+  })
+
+  // --------------------------------------------------------------------------
+  // fetch
+  // --------------------------------------------------------------------------
+
+  describe('fetch', () => {
+    it.skip('fetches raw transaction', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('sandboxed', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('may fetch multiple transactions', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('throws if not a transaction', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('throws if blockchain timeout', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('cannot catch fetch errors', () => {
     })
   })
 
@@ -398,6 +409,97 @@ describe('Berry', () => {
     // ------------------------------------------------------------------------
 
     it.skip('returns true for authed class', () => {
+      // TODO
+    })
+  })
+
+  // --------------------------------------------------------------------------
+  // Load
+  // --------------------------------------------------------------------------
+
+  describe('load', () => {
+    it.skip('loads same berry class', () => {
+
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('loads different berry class', () => {
+
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('cannot catch load errors', () => {
+      // TODO
+    })
+  })
+
+  // --------------------------------------------------------------------------
+  // Protocols
+  // --------------------------------------------------------------------------
+
+  describe('Protocols', () => {
+    it.skip('loads twetch post', () => {
+      /*
+      class TwetchPost extends Berry {
+        init (text) {
+          this.text = text
+        }
+
+        static async pluck (location, fetch, pluck) {
+          const txo = await fetch(location)
+          if (txo.out[0].s2 === '19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut') { // B protocol
+            return new TwetchPost(txo.out[0].s3)
+          }
+        }
+      }
+      const run = new Run({ network: 'main' })
+      const txid = '4e146ac161324ef0b388798462867c29ad681ef4624ea4e3f7c775561af3ddd0'
+      const post = await run.load(txid, TwetchPost)
+      expect(post instanceof TwetchPost).to.equal(true)
+      expect(post.text).to.equal('Came for the edgy marketing, stayed for truth & kindness')
+      */
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('loads metanet node', () => {
+      /*
+      class MetanetNode extends Berry {
+        init (pnode, parent, data) {
+          this.pnode = pnode
+          this.parent = parent
+          this.data = data
+        }
+
+        static async pluck (location, fetch, pluck) {
+          const txo = await fetch(location)
+          if (txo.out[0].s1 === 'meta') {
+            const pnode = txo.out[0].s2
+            const txidParent = txo.out[0].s3
+            const data = txo.out[0].s4
+            if (data === 'METANET_ROOT') {
+              return new MetanetNode(pnode, null, data)
+            } else {
+              const parentNode = await pluck(txidParent)
+              return new MetanetNode(pnode, parentNode, data)
+            }
+          }
+        }
+      }
+      const run = new Run({ network: 'main' })
+      const txid = '2f24d7edb8de0ef534d8e0bc2413eddda451b4accc481519a1647d7af79d8e88'
+      const node = await run.load(txid, MetanetNode)
+      expect(node.pnode).to.equal('1FqmFgY45CqSGXRNVpHNRQWqoNVCkRpUau')
+      expect(node.parent instanceof MetanetNode).to.equal(true)
+      expect(!!node.data).to.equal(true)
+      */
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('loads slp token', () => {
       // TODO
     })
   })
