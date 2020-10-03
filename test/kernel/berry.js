f5170f311a4737b499747221d91e3416eb881092
Brenton Gunning <brentongunning@gmail.com>
2020-10-03 15:41:51 -0700

    berry test placeholders

--- test/kernel/berry.js
+++ test/kernel/berry.js
@@ -18,38 +18,202 @@ describe('Berry', () => {
   afterEach(() => Run.instance && Run.instance.sync())
   // Deactivate the current run instance. This stops leaks across tests.
   afterEach(() => Run.instance && Run.instance.deactivate())
-  
+
+  // --------------------------------------------------------------------------
+  // pluck
   // --------------------------------------------------------------------------
 
-  it('basic berry', async () => {
-    const run = new Run()
-    class B extends Berry { static async pluck () { return new B() } }
-    const CB = run.deploy(B)
-    await run.sync()
-    const b = await run.load('abc', { berry: CB })
-    expect(b instanceof B).to.equal(true)
-    expect(b.location).to.equal(CB.location + '_abc')
+  describe('pluck', () => {
+    it('basic berry', async () => {
+      const run = new Run()
+      class B extends Berry { static async pluck () { return new B() } }
+      const CB = run.deploy(B)
+      await run.sync()
+      const b = await run.load('abc', { berry: CB })
+      expect(b instanceof B).to.equal(true)
+      expect(b.location).to.equal(CB.location + '_abc')
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('deploying berry', async () => {
+      const run = new Run()
+      class B extends Berry { static async pluck () { return new B() } }
+      const CB = run.deploy(B)
+      const b = await run.load('abc', { berry: CB })
+      expect(b instanceof B).to.equal(true)
+      expect(() => b.location).to.throw()
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('undeployed berry', async () => {
+      const run = new Run()
+      class B extends Berry { static async pluck () { return new B() } }
+      const b = await run.load('abc', { berry: B })
+      expect(b instanceof B).to.equal(true)
+      expect(() => b.location).to.throw()
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('berry with parent', () => {
+    // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('berry with deps', () => {
+    // TODO
+    })
   })
 
   // --------------------------------------------------------------------------
+  // init
+  // --------------------------------------------------------------------------
 
-  it('deploying berry', async () => {
-    const run = new Run()
-    class B extends Berry { static async pluck () { return new B() } }
-    const CB = run.deploy(B)
-    const b = await run.load('abc', { berry: CB })
-    expect(b instanceof B).to.equal(true)
-    expect(() => b.location).to.throw()
+  describe('init', () => {
+    it.skip('set properties', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('define properties', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('delete properties', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('get own properties', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('throws if set location', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('throws if set prototype', () => {
+      // TODO
+    })
   })
 
   // --------------------------------------------------------------------------
+  // Method
+  // --------------------------------------------------------------------------
 
-  it('undeployed berry', async () => {
-    const run = new Run()
-    class B extends Berry { static async pluck () { return new B() } }
-    const b = await run.load('abc', { berry: B })
-    expect(b instanceof B).to.equal(true)
-    expect(() => b.location).to.throw()
+  describe('Method', () => {
+    it.skip('get', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('getOwnPropertyDescriptor', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('throws if set', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('throws if define', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('throws if delete', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('throws if set prototype', () => {
+      // TODO
+    })
+  })
+
+  // --------------------------------------------------------------------------
+  // Jig
+  // --------------------------------------------------------------------------
+
+  describe('Jig', () => {
+    it.skip('assigns to jig', () => {
+
+    })
+  })
+
+  // --------------------------------------------------------------------------
+  // Code
+  // --------------------------------------------------------------------------
+
+  describe('Code', () => {
+    it.skip('assigns to code', () => {
+
+    })
+  })
+
+  // --------------------------------------------------------------------------
+  // Sync
+  // --------------------------------------------------------------------------
+
+  describe('Sync', () => {
+    // TODO
+  })
+
+  // --------------------------------------------------------------------------
+  // Instanceof
+  // --------------------------------------------------------------------------
+
+  describe('instanceof', () => {
+    it.skip('returns true for Berry', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('returns true for berry class', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('returns true for parent class', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('returns false for another class', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('returns true for local class', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('berry class instanceof code', () => {
+      // TODO
+    })
   })
 
   // Tests
@@ -57,12 +221,9 @@ describe('Berry', () => {
   // - load with non-berry class throws
   // - immutable externally
   // - immutable internally
-  // - can call functions
   // - not a jig - no auth, destroy, sync, etc.
   // - cannot be upgraded
   // - if dependencies are upgraded, when berry is loaded, it uses original
-  // - instanceof code
-  // - instanceof local
   // - instanceof authd, destroyd
   // - sync destroyed/authd berry in jig
 })
