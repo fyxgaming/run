63025b471b7bca92427bafcb0f09a0e324827a67
Brenton Gunning <brentongunning@gmail.com>
2020-10-05 00:29:03 -0700

    Move berry cache tests to cache.js for later

---
+++ test/kernel/cache.js
@@ -0,0 +1,11 @@
+/**
+ * cache.js
+ *
+ * Tests for cache functionality
+ */
+
+// TODO
+// - Both load and set
+// - Berry
+// - Berry with circular dependency
+// - Does not cache berry if undeployed
