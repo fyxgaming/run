5a42aae0e03165e7fc29d631cb0d8f7398691c06
Brenton Gunning <brentongunning@gmail.com>
2020-10-05 02:36:24 -0700

    throws if return unrelated berry

--- test/kernel/berry.js
+++ test/kernel/berry.js
@@ -305,8 +305,16 @@ Line 3`
 
     // ------------------------------------------------------------------------
 
-    it.skip('throws if return unrelated berry', () => {
-      // TODO
+    it('throws if return unrelated berry', async () => {
+      const run = new Run()
+      class B extends Berry { static async pluck () { return new B() } }
+      run.deploy(B)
+      await run.sync()
+      const b = await run.load('', { berry: B })
+      class C extends Berry { static async pluck () { return C.b } }
+      C.b = b
+      const error = 'Berry must be an instance of C'
+      await expect(run.load('', { berry: C })).to.be.rejectedWith(error)
     })
 
     // ------------------------------------------------------------------------
