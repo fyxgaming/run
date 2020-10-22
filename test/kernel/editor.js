dec396d2e9f523bbcfe52aa0503345329ad44b5c
Brenton Gunning <brentongunning@gmail.com>
2020-10-22 10:53:08 -0700

    Separate code and editor tests

---
+++ test/kernel/editor.js
@@ -0,0 +1,231 @@
+/**
+ * editor.js
+ *
+ * Tests for lib/kernel/editor.js
+ */
+
+const { describe, it, afterEach } = require('mocha')
+require('chai').use(require('chai-as-promised'))
+const { expect } = require('chai')
+const Run = require('../env/run')
+const { Code, Jig, Berry } = Run
+const unmangle = require('../env/unmangle')
+const { _sudo } = unmangle(Run)
+
+// ------------------------------------------------------------------------------------------------
+// Editor
+// ------------------------------------------------------------------------------------------------
+
+describe('Editor', () => {
+  // Wait for every test to finish. This makes debugging easier.
+  afterEach(() => Run.instance && Run.instance.sync())
+  // Deactivate the current run instance. This stops leaks across tests.
+  afterEach(() => Run.instance && Run.instance.deactivate())
+
+  // --------------------------------------------------------------------------
+  // Activate
+  // --------------------------------------------------------------------------
+
+  describe('activate', () => {
+    it('deactivate removes bindings', async () => {
+      const run = new Run()
+      class A { }
+      run.deploy(A)
+      await run.sync()
+      run.deactivate()
+      expect(typeof A.location).to.equal('undefined')
+      expect(typeof A.origin).to.equal('undefined')
+      expect(typeof A.nonce).to.equal('undefined')
+      expect(typeof A.owner).to.equal('undefined')
+      expect(typeof A.satoshis).to.equal('undefined')
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('activate adds bindings', async () => {
+      const run = new Run()
+      class A { }
+      run.deploy(A)
+      await run.sync()
+      const location = A.location
+      const origin = A.origin
+      const nonce = A.nonce
+      const owner = A.owner
+      const satoshis = A.satoshis
+      run.deactivate()
+      run.activate()
+      expect(A.location).to.equal(location)
+      expect(A.origin).to.equal(origin)
+      expect(A.nonce).to.equal(nonce)
+      expect(A.owner).to.equal(owner)
+      expect(A.satoshis).to.equal(satoshis)
+    })
+  })
+
+  // --------------------------------------------------------------------------
+  // Preinstall
+  // --------------------------------------------------------------------------
+
+  describe('Preinstall', () => {
+    it('creates code without bindings', () => {
+      const C = Run.install(class A { })
+      expect(C instanceof Code).to.equal(true)
+      _sudo(() => {
+        expect(C.location).to.equal(undefined)
+        expect(C.origin).to.equal(undefined)
+        expect(C.nonce).to.equal(undefined)
+        expect(C.owner).to.equal(undefined)
+        expect(C.satoshis).to.equal(undefined)
+      })
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('throws if read bindings', () => {
+      const C = Run.install(class A { })
+      expect(() => C.location).to.throw('Cannot read location')
+      expect(() => C.origin).to.throw('Cannot read origin')
+      expect(() => C.nonce).to.throw('Cannot read nonce')
+      expect(() => C.owner).to.throw('Cannot read owner')
+      expect(() => C.satoshis).to.throw('Cannot read satoshis')
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('only preinstalls once', () => {
+      class A { }
+      const C1 = Run.install(A)
+      const C2 = Run.install(A)
+      expect(C1).to.equal(C2)
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('create jigs using preinstalled code', async () => {
+      class A extends Jig { }
+      const CA = Run.install(A)
+      new Run() // eslint-disable-line
+      const a = new CA()
+      await a.sync()
+      expect(typeof A.location).to.equal('string')
+      expect(typeof CA.location).to.equal('string')
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('pass as args preinstalled code', async () => {
+      class A extends Jig { }
+      const CA = Run.install(A)
+      new Run() // eslint-disable-line
+      class B extends Jig { init (A) { this.A = A } }
+      const b = new B(CA)
+      await b.sync()
+      expect(typeof A.location).to.equal('string')
+      expect(typeof CA.location).to.equal('string')
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('use preinstalled code as code props', async () => {
+      class A extends Jig { }
+      const CA = Run.install(A)
+      const run = new Run()
+      class B {}
+      B.A = CA
+      const CB = await run.deploy(B)
+      await CB.sync()
+      expect(typeof A.location).to.equal('string')
+      expect(typeof CA.location).to.equal('string')
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('locks onto network once used', async () => {
+      class A extends Jig { }
+      const CA = Run.install(A)
+      const run = new Run()
+      run.deploy(CA)
+      await run.sync()
+      const location = CA.location
+      run.deactivate()
+      expect(CA.location).to.equal(location)
+    })
+  })
+
+  // --------------------------------------------------------------------------
+  // uninstall
+  // --------------------------------------------------------------------------
+
+  describe('uninstall', () => {
+    it('remove bindings and presets from local', async () => {
+      const run = new Run()
+      class A { }
+      const C = run.deploy(A)
+      await C.sync()
+      Run.uninstall(A)
+      expect('presets' in A).to.equal(false)
+      expect('location' in A).to.equal(false)
+      expect('origin' in A).to.equal(false)
+      expect('nonce' in A).to.equal(false)
+      expect('owner' in A).to.equal(false)
+      expect('satoshis' in A).to.equal(false)
+      expect('location' in C).to.equal(true)
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('can be deployed again', async () => {
+      const run = new Run()
+      class A { }
+      const C = run.deploy(A)
+      Run.uninstall(C)
+      const D = run.deploy(A)
+      await run.sync()
+      expect(C.location).not.to.equal(D.location)
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('can use uninstalled code', async () => {
+      const run = new Run()
+      class A extends Jig { static f () { this.n = 1 } }
+      const C = run.deploy(A)
+      Run.uninstall(A)
+      C.auth()
+      C.f()
+      C.destroy()
+      new C() // eslint-disable-line
+      await run.sync()
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('throws for native code', () => {
+      expect(() => Run.uninstall(Jig)).to.throw('Cannot uninstall native code')
+      expect(() => Run.uninstall(Berry)).to.throw('Cannot uninstall native code')
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('reinstall with presets', async () => {
+      const run = new Run()
+      class A { }
+      const A2 = run.deploy(A)
+      await run.sync()
+      const location = A.location
+
+      const presets = A.presets
+      Run.uninstall(A)
+      expect(A.location).to.equal(undefined)
+      A.presets = presets
+      const A3 = run.deploy(A)
+      await run.sync()
+
+      expect(A2).not.to.equal(A3)
+      expect(A.location).to.equal(location)
+      expect(A3.location).to.equal(location)
+    })
+  })
+})
+
+// ------------------------------------------------------------------------------------------------
