e4d2cd791b6d81deb7f7afd760ffaacfe8804bc7
Brenton Gunning <brentongunning@gmail.com>
2020-10-28 21:14:44 -0700

    version tests wip

--- test/util/version.js
+++ test/util/version.js
@@ -5,6 +5,7 @@
  */
 
 const { describe, it } = require('mocha')
+const { expect } = require('chai')
 const Run = require('../env/run')
 const unmangle = require('../env/unmangle')
 const {
@@ -15,15 +16,26 @@ const {
   _getStateVersion
 } = unmangle(unmangle(Run)._version)
 
-console.log(_version, _parsePayloadVersion, _parseStateVersion, _getPayloadVersion, _getStateVersion)
+console.log(_parsePayloadVersion, _parseStateVersion, _getPayloadVersion, _getStateVersion)
 
 // ------------------------------------------------------------------------------------------------
 // _version
 // ------------------------------------------------------------------------------------------------
 
 describe('_version', () => {
-  it('hello', () => {
-    console.log('world')
+  it('returns version if supported', () => {
+    expect(_version(5)).to.equal(5)
+    expect(_version(Run.protocol)).to.equal(5)
+  })
+
+  // --------------------------------------------------------------------------
+
+  it('throws if unsupported', () => {
+    expect(() => _version(4)).to.throw('Unsupported version: 4')
+    expect(() => _version(6)).to.throw('Unsupported version: 6')
+    expect(() => _version(null)).to.throw('Unsupported version: null')
+    expect(() => _version()).to.throw('Unsupported version: undefined')
+    expect(() => _version('5')).to.throw('Unsupported version: 5')
   })
 })
 
