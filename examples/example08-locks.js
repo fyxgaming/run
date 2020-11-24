0fa28cdedb0f76540e883181796485b5a2a28f7f
Brenton Gunning <brentongunning@gmail.com>
2020-11-23 23:47:02 -0800

    WIP Lock test

--- examples/example08-locks.js
+++ examples/example08-locks.js
@@ -1,6 +1,6 @@
 const bsv = require('bsv')
 const Run = require('../dist/run.node.min')
-const { asm } = Run
+const { asm } = Run.extra
 
 /**
  * Define a lock
@@ -11,11 +11,26 @@ const { asm } = Run
  */
 
 class TwoPlusTwoLock {
-  script () { return asm('OP_2 OP_2 OP_ADD OP_EQUAL') }
+  script () {
+    const x= asm('OP_2 OP_2 OP_ADD OP_EQUAL')
+    console.log('---', x, typeof x)
+    return x
+  }
   domain () { return 1 }
 }
 
 TwoPlusTwoLock.deps = { asm }
+
+// Locks must be deployed on their own before they can be used 
+async function deployLockClass() {
+  const lockRun = new Run({ network: 'mock' })
+  console.log('1')
+  lockRun.deploy(TwoPlusTwoLock)
+  console.log('2')
+  await lockRun.sync()
+  console.log('3')
+  console.log(TwoPlusTwoLock.location)
+}
 
 /**
  * Define a key
@@ -26,7 +41,7 @@ TwoPlusTwoLock.deps = { asm }
  */
 
 class TwoPlusTwoKey {
-  owner () { return new TwoPlusTwoLock() }
+  async nextOwner () { return new TwoPlusTwoLock() }
 
   async sign (rawtx, parents, locks) {
     const tx = new bsv.Transaction(rawtx)
@@ -43,18 +58,27 @@ class TwoPlusTwoKey {
 /**
  * Create a jig assigned to the TwoPlusTwoLock and update it with the TwoPlusTwoKey
  */
-
 async function main () {
+  await deployLockClass()
+
   const run = new Run({ network: 'mock', owner: new TwoPlusTwoKey() })
 
+  run.logger = console
+
+  // TODO
+  run.trust('*')
+
+  console.log('a')
   class Dragon extends Jig {
     setName(name) { this.name = name }
   }
   const dragon = new Dragon()
   await dragon.sync()
+  console.log('b')
 
   dragon.setName('Victoria')
   await dragon.sync()
+  console.log('c')
 
   console.log('Unlocked the custom lock')
 }
