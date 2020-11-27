2243d4daa07a0bde851322da3b9c0c4f3b122136
Brenton Gunning <brentongunning@gmail.com>
2020-11-27 09:42:09 -0800

    Fix example

--- examples/example08-locks.js
+++ examples/example08-locks.js
@@ -11,26 +11,11 @@ const { asm } = Run.extra
  */
 
 class TwoPlusTwoLock {
-  script () {
-    const x= asm('OP_2 OP_2 OP_ADD OP_EQUAL')
-    console.log('---', x, typeof x)
-    return x
-  }
+  script () { return asm('OP_2 OP_2 OP_ADD OP_EQUAL') }
   domain () { return 1 }
 }
 
 TwoPlusTwoLock.deps = { asm }
-
-// Locks must be deployed on their own before they can be used 
-async function deployLockClass() {
-  const lockRun = new Run({ network: 'mock' })
-  console.log('1')
-  lockRun.deploy(TwoPlusTwoLock)
-  console.log('2')
-  await lockRun.sync()
-  console.log('3')
-  console.log(TwoPlusTwoLock.location)
-}
 
 /**
  * Define a key
@@ -46,9 +31,12 @@ class TwoPlusTwoKey {
   async sign (rawtx, parents, locks) {
     const tx = new bsv.Transaction(rawtx)
 
+    // We install the lock to get the deployed code that is assigned as a lock
+    const InstalledLock = Run.install(TwoPlusTwoLock)
+
     // Sign any TwoPlusTwoLock
     tx.inputs
-      .filter((input, n) => locks[n] instanceof TwoPlusTwoLock)
+      .filter((input, n) => locks[n] instanceof InstalledLock)
       .forEach(input => input.setScript('OP_4'))
     
     return tx.toString('hex')
@@ -59,26 +47,14 @@ class TwoPlusTwoKey {
  * Create a jig assigned to the TwoPlusTwoLock and update it with the TwoPlusTwoKey
  */
 async function main () {
-  await deployLockClass()
-
   const run = new Run({ network: 'mock', owner: new TwoPlusTwoKey() })
 
-  run.logger = console
-
-  // TODO
-  run.trust('*')
-
-  console.log('a')
-  class Dragon extends Jig {
-    setName(name) { this.name = name }
-  }
+  class Dragon extends Jig { setName(name) { this.name = name } }
   const dragon = new Dragon()
   await dragon.sync()
-  console.log('b')
 
   dragon.setName('Victoria')
   await dragon.sync()
-  console.log('c')
 
   console.log('Unlocked the custom lock')
 }
