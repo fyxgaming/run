09783dc57d9f737a45886087af79e27b077621f7
Brenton Gunning <brentongunning@gmail.com>
2020-11-27 08:09:07 -0800

    WIP lock tests

--- test/kernel/lock.js
+++ test/kernel/lock.js
@@ -21,9 +21,11 @@ describe('Lock', () => {
   // Deactivate the current run instance. This stops leaks across tests.
   afterEach(() => Run.instance && Run.instance.deactivate())
 
-  // ------------------------------------------------------------------------
+  // --------------------------------------------------------------------------
+  // Create
+  // --------------------------------------------------------------------------
 
-  describe('create', () => {
+  describe('Create', () => {
     it('simple lock deployed', async () => {
       const run = new Run()
       const L = await run.deploy(class L {
@@ -98,7 +100,10 @@ describe('Lock', () => {
       const A = run.deploy(class A { })
       await run.sync()
       const CL = Run.install(L)
-      function test (A) { expect(A.owner instanceof CL).to.equal(true) }
+      function test (A) {
+        expect(A.owner instanceof CL).to.equal(true)
+        expect(A.owner.constructor.owner instanceof CL).to.equal(true)
+      }
       test(A)
       const A2 = await run.load(A.location)
       test(A2)
@@ -107,96 +112,84 @@ describe('Lock', () => {
       test(A3)
     })
 
-    // Assign owner from inside jig
-
-    // TODO: Second nextOwner is undeployed! Throw
-    // script() reads, readonly, etc.
-
     // ------------------------------------------------------------------------
 
-    it.skip('fails to deploy if lock class undeployed', () => {
+    it.skip('script() reads a jig', () => {
       // TODO
     })
 
     // ------------------------------------------------------------------------
 
-    it.skip('updates jig with custom key', () => {
-
+    it.skip('script() reads an deploying jig', () => {
+      // TODO
     })
 
-    /*
-    it('load non-standard owner', async () => {
-      class CustomLock {
-        script () { return new Uint8Array([1, 2, 3]) }
-        domain () { return 1 }
-      }
-      class CustomOwner {
-        nextOwner () { return new CustomLock() }
-        async sign (rawtx) { return rawtx }
-      }
-      const run = new Run({ owner: new CustomOwner() })
-      class A extends Jig { }
-      const a = new A()
-      await a.sync()
-      run.deactivate()
-      const run2 = new Run({ blockchain: run.blockchain })
-      await run2.load(a.location)
+    // ------------------------------------------------------------------------
+
+    it.skip('script() reads an inner jig', () => {
+      // TODO
     })
 
-    it('copy non-standard owner to another jig', async () => {
-      createHookedRun()
-      class CustomLock {
-        script () { return new Uint8Array([1, 2, 3]) }
-        domain () { return 1 }
-      }
-      class A extends Jig { init () { this.owner = new CustomLock() } }
-      A.deps = { CustomLock }
-      class B extends Jig { init (a) { this.owner = a.owner } }
-      expect(() => new B(new A())).not.to.throw()
+    // ------------------------------------------------------------------------
+
+    it.skip('script() reads an deploying inner jig', () => {
+      // TODO
     })
 
-    it('return a copy of owners to outside', async () => {
-      createHookedRun()
-      class CustomLock {
-        constructor (n) { this.n = n }
-        script () { return new Uint8Array([this.n]) }
-        domain () { return 1 }
-      }
-      class A extends Jig { init (n) { this.owner = new CustomLock(n) } }
-      A.deps = { CustomLock }
-      class B extends Jig {
-        init (a, n) { this.owner = a.owner; this.owner.n = n }
-      }
-      const a = new A(1)
-      const b = new B(a, 2)
-      expect(a.owner.n).to.equal(1)
-      expect(b.owner.n).to.equal(2)
+    // ------------------------------------------------------------------------
+
+    it.skip('assigns sandboxed owner', () => {
+      // TODO
     })
 
-    it('return the original owner inside', async () => {
-      createHookedRun()
-      class A extends Jig {
-        init (owner) { this.owner = owner }
-        copyOwner () { this.owner2 = this.owner; this.owner2.n = 2 }
-      }
-      class CustomLock {
-        constructor () { this.n = 1 }
-        script () { return new Uint8Array([this.n]) }
-        domain () { return 1 }
-      }
-      const a = new A(new CustomLock())
-      a.copyOwner()
-      expect(a.owner).to.deep.equal(a.owner2)
-      await expect(a.sync()).to.be.rejected
+    // ------------------------------------------------------------------------
+
+    it.skip('throws if nextOwner() returns non-lock', () => {
+      // TODO
     })
-    */
+
+    // ------------------------------------------------------------------------
+
+    it.skip('throws if script() updates a jig', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('throws if script() creates a jig', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('throws if script() deletes a jig', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('throws if script() auths a jig', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('throws if nextOwner() for lock is undeployed', () => {
+      // TODO
+    })
   })
 
-  // ------------------------------------------------------------------------
+  // --------------------------------------------------------------------------
   // Method
-  // ------------------------------------------------------------------------
+  // --------------------------------------------------------------------------
 
   describe('Method', () => {
+    it.skip('assign owner from inside jig', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
     it('cloned when assigned from another jig', async () => {
       const run = new Run()
 
@@ -235,6 +228,12 @@ describe('Lock', () => {
       const a3 = await run.load(a.location)
       const CB3 = await run.load(CB.location)
       test(a3, CB3)
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('updates jig with custom key', () => {
+      // TODO
     })
   })
 })
