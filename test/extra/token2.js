f2d5cee424fe77413cd54205a6bb47a214f4c3bc
Brenton Gunning <brentongunning@gmail.com>
2020-12-03 11:08:15 -0800

    Token2 deploy test

--- test/extra/token2.js
+++ test/extra/token2.js
@@ -566,9 +566,9 @@ describe('Token', () => {
   it.skip('deploy', async () => {
     // Hint: Run with env NETWORK=<network> to deploy with keys
     const run = new Run()
-    run.deploy(Run.extra.Token)
+    run.deploy(Run.extra.Token2)
     await run.sync()
-    console.log(Run.extra.Token)
+    console.log(Run.extra.Token2)
   })
 })
 
