58a7f5b138902850595c4ebc72abb488feb0c6a4
Brenton Gunning <brentongunning@gmail.com>
2021-01-01 11:47:35 -0800

    make environment tests unit testable

--- test/run.js
+++ test/run.js
@@ -8,8 +8,6 @@ const { describe, it } = require('mocha')
 require('chai').use(require('chai-as-promised'))
 const { expect } = require('chai')
 const Run = require('./env/run')
-const unmangle = require('./env/unmangle')
-const bsv = require('bsv')
 const { Jig } = Run
 
 // ------------------------------------------------------------------------------------------------
@@ -41,97 +39,6 @@ describe('Run', () => {
       expect(metadata.del.length).to.equal(0)
       expect(metadata.cre).to.deep.equal([run.owner.address])
       expect(metadata.exec).to.deep.equal(exec)
-    })
-  })
-
-  // --------------------------------------------------------------------------
-  // _checkEnvironment
-  // --------------------------------------------------------------------------
-
-  describe('_checkEnvironment', () => {
-    // ------------------------------------------------------------------------
-    // node
-    // ------------------------------------------------------------------------
-
-    describe('node', () => {
-      function testNodeVersion (version) {
-        const oldVersionDesc = Object.getOwnPropertyDescriptor(process, 'version')
-        try {
-          Object.defineProperty(process, 'version', {
-            value: version,
-            writable: false,
-            enumerable: true,
-            configurable: true
-          })
-          unmangle(Run)._checkEnvironment()
-        } finally {
-          Object.defineProperty(process, 'version', oldVersionDesc)
-        }
-      }
-
-      // ----------------------------------------------------------------------
-
-      it('node 10-14 supported', () => {
-        testNodeVersion('v10.15.3')
-        testNodeVersion('v11.15.0')
-        testNodeVersion('v12.3.1')
-        testNodeVersion('v13.11.0')
-        testNodeVersion('v14.15.1')
-      })
-
-      // ----------------------------------------------------------------------
-
-      it('node >= 15 not supported', () => {
-        const error = 'Run is not yet supported on Node 15 and above'
-        expect(() => testNodeVersion('v15.3.0')).to.throw(error)
-        expect(() => testNodeVersion('v16.0.0')).to.throw(error)
-      })
-
-      // ----------------------------------------------------------------------
-
-      it('node <= 9 not supported', () => {
-        const error = 'Run is supported only on Node v10 and above'
-        expect(() => testNodeVersion('v9.11.2')).to.throw(error)
-        expect(() => testNodeVersion('v8.16.0')).to.throw(error)
-      })
-    })
-
-    // ------------------------------------------------------------------------
-    // bsv
-    // ------------------------------------------------------------------------
-
-    describe('bsv', () => {
-      function testBsvVersion (version) {
-        const oldVersion = bsv.version
-        try {
-          bsv.version = version
-          unmangle(Run)._checkEnvironment()
-        } finally {
-          bsv.version = oldVersion
-        }
-      }
-
-      // ----------------------------------------------------------------------
-
-      it('bsv 1.x does not throw an error', () => {
-        expect(() => testBsvVersion('v1.5.4')).not.to.throw()
-      })
-
-      // ----------------------------------------------------------------------
-
-      it('bsv 2.x throws an error', () => {
-        const error = 'Run requires version 1.x of the bsv library'
-        expect(() => testBsvVersion('2.0.0')).to.throw(error)
-        expect(() => testBsvVersion('v2.0.0')).to.throw(error)
-      })
-
-      // ----------------------------------------------------------------------
-
-      it('invalid bsv version throws an error', () => {
-        const error = 'Run requires version 1.x of the bsv library'
-        expect(() => testBsvVersion('0.1')).to.throw(error)
-        expect(() => testBsvVersion(undefined)).to.throw(error)
-      })
     })
   })
 })
---
+++ test/util/environment.js
@@ -0,0 +1,111 @@
+/**
+ * environment.js
+ *
+ * Tests for lib/util/environment.js
+ */
+
+const bsv = require('bsv')
+const { describe, it } = require('mocha')
+const { expect } = require('chai')
+const Run = require('../env/run')
+const unmangle = require('../env/unmangle')
+const { _check } = unmangle(unmangle(Run)._environment)
+
+// --------------------------------------------------------------------------
+// _check
+// --------------------------------------------------------------------------
+
+describe.only('_check', () => {
+  // ------------------------------------------------------------------------
+  // bsv
+  // ------------------------------------------------------------------------
+
+  describe('bsv', () => {
+    function testBsvVersion (version) {
+      const oldVersion = bsv.version
+      try {
+        bsv.version = version
+        _check()
+      } finally {
+        bsv.version = oldVersion
+      }
+    }
+
+    // ----------------------------------------------------------------------
+
+    it('bsv 1.x does not throw an error', () => {
+      expect(() => testBsvVersion('v1.5.4')).not.to.throw()
+    })
+
+    // ----------------------------------------------------------------------
+
+    it('bsv 2.x throws an error', () => {
+      const error = 'Run requires version 1.x of the bsv library'
+      expect(() => testBsvVersion('2.0.0')).to.throw(error)
+      expect(() => testBsvVersion('v2.0.0')).to.throw(error)
+    })
+
+    // ----------------------------------------------------------------------
+
+    it('invalid bsv version throws an error', () => {
+      const error = 'Run requires version 1.x of the bsv library'
+      expect(() => testBsvVersion('0.1')).to.throw(error)
+      expect(() => testBsvVersion(undefined)).to.throw(error)
+    })
+  })
+
+  // ------------------------------------------------------------------------
+  // node
+  // ------------------------------------------------------------------------
+
+  describe('node', () => {
+    function testNodeVersion (version) {
+      const oldVersionDesc = Object.getOwnPropertyDescriptor(process, 'version')
+      try {
+        Object.defineProperty(process, 'version', {
+          value: version,
+          writable: false,
+          enumerable: true,
+          configurable: true
+        })
+        _check()
+      } finally {
+        Object.defineProperty(process, 'version', oldVersionDesc)
+      }
+    }
+
+    // ----------------------------------------------------------------------
+
+    it('node 10-14 supported', () => {
+      testNodeVersion('v10.15.3')
+      testNodeVersion('v11.15.0')
+      testNodeVersion('v12.3.1')
+      testNodeVersion('v13.11.0')
+      testNodeVersion('v14.15.1')
+    })
+
+    // ----------------------------------------------------------------------
+
+    it('node >= 15 not supported', () => {
+      const error = 'Run is not yet supported on Node 15 and above'
+      expect(() => testNodeVersion('v15.3.0')).to.throw(error)
+      expect(() => testNodeVersion('v16.0.0')).to.throw(error)
+    })
+
+    // ----------------------------------------------------------------------
+
+    it('node <= 9 not supported', () => {
+      const error = 'Run is supported only on Node v10 and above'
+      expect(() => testNodeVersion('v9.11.2')).to.throw(error)
+      expect(() => testNodeVersion('v8.16.0')).to.throw(error)
+    })
+  })
+
+  // ------------------------------------------------------------------------
+  // browser
+  // ------------------------------------------------------------------------
+
+  describe('browser', () => {
+    // TODO
+  })
+})
