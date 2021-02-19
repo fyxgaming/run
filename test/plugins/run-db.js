41be9c5548902bbe3296838f19bf0a2ff992333c
Brenton Gunning <brentongunning@gmail.com>
2021-02-19 03:06:44 -0700

    Update RunDB cache

---
+++ test/plugins/run-db.js
@@ -0,0 +1,26 @@
+/**
+ * run-db.js
+ *
+ * Tests for lib/plugins/run-db.js
+ */
+
+const { describe, it } = require('mocha')
+const Run = require('../env/run')
+const { RunDB } = Run.plugins
+
+// ------------------------------------------------------------------------------------------------
+// RunDB
+// ------------------------------------------------------------------------------------------------
+
+describe('RunDB', () => {
+  it.only('tests get', async () => {
+    const rundb = new RunDB('http://localhost:8000')
+    const run = new Run({ cache: rundb, client: true, network: 'main' })
+    const jig = await run.load('c07f9f41fc8a4d78be44a6c3e3d38d2ae58ae62f3ac6f33e567f4f4a653b5f20_o1')
+    console.log(jig)
+    // Not found
+    await run.load('c07f9f41fc8a4d78be44a6c3e3d38d2ae58ae62f3ac6f33e567f4f4a653b5f21_o1')
+  })
+})
+
+// ------------------------------------------------------------------------------------------------
