660d2a884ec1115b3bac3e4ec36470bc6e5589c3
Brenton Gunning <brentongunning@gmail.com>
2021-02-25 03:56:10 -0700

    Get trust from RunDB and reproduce failure

--- test/plugins/run-db.js
+++ test/plugins/run-db.js
@@ -5,21 +5,43 @@
  */
 
 const { describe, it } = require('mocha')
+require('chai').use(require('chai-as-promised'))
+const { expect } = require('chai')
 const Run = require('../env/run')
 const { RunDB } = Run.plugins
+
+// ------------------------------------------------------------------------------------------------
+// Globals
+// ------------------------------------------------------------------------------------------------
+
+const HOST = 'http://localhost:8000'
 
 // ------------------------------------------------------------------------------------------------
 // RunDB
 // ------------------------------------------------------------------------------------------------
 
 describe('RunDB', () => {
-  it.skip('tests get', async () => {
-    const rundb = new RunDB('http://localhost:8000')
-    const run = new Run({ cache: rundb, client: true, network: 'main' })
+  it('load from cache', async () => {
+    const rundb = new RunDB(HOST)
+    const run = new Run({ cache: rundb, client: true, network: 'main', trust: 'cache' })
     const jig = await run.load('c07f9f41fc8a4d78be44a6c3e3d38d2ae58ae62f3ac6f33e567f4f4a653b5f20_o1')
-    console.log(jig)
-    // Not found
-    await run.load('c07f9f41fc8a4d78be44a6c3e3d38d2ae58ae62f3ac6f33e567f4f4a653b5f21_o1')
+  })
+
+  // --------------------------------------------------------------------------
+
+  it('load missing', async () => {
+    const rundb = new RunDB(HOST)
+    const run = new Run({ cache: rundb, client: true, network: 'main', trust: 'cache' })
+    await expect(run.load('1111111111111111111111111111111111111111111111111111111111111111_o1')).to.be.rejected
+  })
+
+  // --------------------------------------------------------------------------
+
+  it.skip('sync and replay', async () => {
+    const rundb = new RunDB(HOST)
+    const run = new Run({ cache: rundb, client: true, network: 'main', trust: [] })
+    const ShuaCoin = await run.load('ce8629aa37a1777d6aa64d0d33cd739fd4e231dc85cfe2f9368473ab09078b78_o1')
+    await ShuaCoin.sync()
   })
 })
 
