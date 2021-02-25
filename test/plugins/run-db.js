502d054215856b780f504248366132cb87dd6e27
Brenton Gunning <brentongunning@gmail.com>
2021-02-25 04:10:31 -0700

    Pull trust from RunDB

--- test/plugins/run-db.js
+++ test/plugins/run-db.js
@@ -20,11 +20,11 @@ const HOST = 'http://localhost:8000'
 // RunDB
 // ------------------------------------------------------------------------------------------------
 
-describe('RunDB', () => {
+describe.skip('RunDB', () => {
   it('load from cache', async () => {
     const rundb = new RunDB(HOST)
     const run = new Run({ cache: rundb, client: true, network: 'main', trust: 'cache' })
-    const jig = await run.load('c07f9f41fc8a4d78be44a6c3e3d38d2ae58ae62f3ac6f33e567f4f4a653b5f20_o1')
+    await run.load('c07f9f41fc8a4d78be44a6c3e3d38d2ae58ae62f3ac6f33e567f4f4a653b5f20_o1')
   })
 
   // --------------------------------------------------------------------------
@@ -37,7 +37,7 @@ describe('RunDB', () => {
 
   // --------------------------------------------------------------------------
 
-  it.skip('sync and replay', async () => {
+  it('sync and replay', async () => {
     const rundb = new RunDB(HOST)
     const run = new Run({ cache: rundb, client: true, network: 'main', trust: [] })
     const ShuaCoin = await run.load('ce8629aa37a1777d6aa64d0d33cd739fd4e231dc85cfe2f9368473ab09078b78_o1')
