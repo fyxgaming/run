741ca587b7bedca8bc011ec0c95d328b7975c7af
Brenton Gunning <brentongunning@gmail.com>
2021-03-18 13:39:11 -0600

    Get inventory unspent from RunDB

--- test/plugins/run-db.js
+++ test/plugins/run-db.js
@@ -20,7 +20,7 @@ const HOST = 'http://localhost:8000'
 // RunDB
 // ------------------------------------------------------------------------------------------------
 
-describe.skip('RunDB', () => {
+describe('RunDB', () => {
   it('load from cache', async () => {
     const rundb = new RunDB(HOST)
     const run = new Run({ cache: rundb, client: true, network: 'main', trust: 'cache' })
@@ -42,6 +42,17 @@ describe.skip('RunDB', () => {
     const run = new Run({ cache: rundb, client: true, network: 'main', trust: [] })
     const ShuaCoin = await run.load('ce8629aa37a1777d6aa64d0d33cd739fd4e231dc85cfe2f9368473ab09078b78_o1')
     await ShuaCoin.sync()
+  })
+
+  // --------------------------------------------------------------------------
+
+  it('gets unspent from inventory', async () => {
+    const rundb = new RunDB(HOST)
+    const run = new Run({ cache: rundb, client: true, network: 'main', trust: [] })
+    run.owner = '1NERTLQqq1MwJSq31DW2MQXEtJsb6TyX3y'
+    await run.inventory.sync()
+    expect(run.inventory.jigs.length > 0).to.equal(true)
+    expect(run.inventory.code.length > 0).to.equal(true)
   })
 })
 
