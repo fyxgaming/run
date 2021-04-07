0a0e824ec7fa181c070175345076563bbc490ce2
Brenton Gunning <brentongunning@gmail.com>
2021-04-07 11:21:55 -0600

    Tests for cache option

--- test/plugins/run-connect.js
+++ test/plugins/run-connect.js
@@ -8,6 +8,7 @@ const { describe, it } = require('mocha')
 const { expect } = require('chai')
 const Run = require('../env/run')
 const { RunConnect2: RunConnect } = Run.plugins
+const { Cache } = Run.api
 
 // ------------------------------------------------------------------------------------------------
 // RunConnect
@@ -15,17 +16,20 @@ const { RunConnect2: RunConnect } = Run.plugins
 
 describe('RunConnect', () => {
   describe('constructor', () => {
-    it('defaults to mainnet', () => {
-      expect(new RunConnect().network).to.equal('main')
+    it('creates with defaults', () => {
+      const connect = new RunConnect()
+      expect(connect.network).to.equal('main')
+      expect(connect.cache instanceof Cache).to.equal(true)
+      expect(connect.api).to.equal('run')
     })
 
     // --------------------------------------------------------------------------------------------
 
     it('create on supported network', () => {
-      const mainnetRun = new RunConnect({ network: 'main' })
-      expect(mainnetRun.network).to.equal('main')
-      const testnetRun = new RunConnect({ network: 'test' })
-      expect(testnetRun.network).to.equal('test')
+      const mainnetConnect = new RunConnect({ network: 'main' })
+      expect(mainnetConnect.network).to.equal('main')
+      const testnetConnect = new RunConnect({ network: 'test' })
+      expect(testnetConnect.network).to.equal('test')
     })
 
     // --------------------------------------------------------------------------------------------
@@ -35,6 +39,27 @@ describe('RunConnect', () => {
       expect(() => new RunConnect({ network: 'stn' })).to.throw('Unsupported network: stn')
       expect(() => new RunConnect({ network: null })).to.throw('Unsupported network: null')
       expect(() => new RunConnect({ network: 0 })).to.throw('Unsupported network: 0')
+    })
+
+    // --------------------------------------------------------------------------------------------
+
+    it('creates with custom cache', () => {
+      class CustomCache {
+        async set (key, value) { }
+        async get (key) { }
+      }
+      const cache = new CustomCache()
+      const connect = new RunConnect({ cache })
+      expect(connect.cache).to.equal(cache)
+    })
+
+    // --------------------------------------------------------------------------------------------
+
+    it('throws if create with invalid cache', () => {
+      expect(() => new RunConnect({ cache: '' })).to.throw('Unsupported cache: ')
+      expect(() => new RunConnect({ cache: null })).to.throw('Unsupported cache: null')
+      expect(() => new RunConnect({ cache: {} })).to.throw('Unsupported cache: [object Object]')
+      expect(() => new RunConnect({ cache: { set: () => { } } })).to.throw('Unsupported cache: [object Object]')
     })
   })
 })
