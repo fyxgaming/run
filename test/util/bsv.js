6c8728725dee60149c5e2b37ee82519a62095f99
Brenton Gunning <brentongunning@gmail.com>
2021-04-19 12:12:54 -0600

    Finish porting remote-blockchain tests

--- test/util/bsv.js
+++ test/util/bsv.js
@@ -76,6 +76,18 @@ describe('bsv', () => {
       const b = { txid: '4', vout: 5, script: '6', satoshis: 7 }
       expect(_dedupUtxos([a, b, b])).to.deep.equal([a, b])
     })
+
+    // ------------------------------------------------------------------------
+
+    it('logs warning', () => {
+      let lastWarning = null
+      const Log = unmangle(unmangle(Run)._Log)
+      Log._logger = { warn: (time, tag, ...warning) => { lastWarning = warning.join(' ') } }
+      const a = { txid: 'abc', vout: 1, script: '2', satoshis: 3 }
+      expect(_dedupUtxos([a, a])).to.deep.equal([a])
+      expect(lastWarning).to.equal('[bsv] Duplicate utxo returned from server: abc_o1')
+      Log._logger = Log._defaultLogger
+    })
   })
 })
 
