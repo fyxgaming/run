c02fc7a626682bc70930bc12b469e782b9c88293
Brenton Gunning <brentongunning@gmail.com>
2021-04-19 10:36:23 -0600

    Get recent broadcast tests passing

--- test/util/recent-broadcasts.js
+++ test/util/recent-broadcasts.js
@@ -17,118 +17,108 @@ const RecentBroadcasts = unmangle(unmangle(Run)._RecentBroadcasts)
 // ------------------------------------------------------------------------------------------------
 
 describe('RecentBroadcasts', () => {
-  console.log(RecentBroadcasts, it, expect, bsv)
   // ----------------------------------------------------------------------------------------------
-  // _addToCache
+  // _add
   // ----------------------------------------------------------------------------------------------
 
   describe('_addToCache', () => {
-    /*
-    it('adds new transaction', () => {
+    it('adds new transaction', async () => {
       const cache = new Map()
       const tx = new bsv.Transaction()
-      tx.to(new bsv.PrivateKey().toAddress(), 100)
-      _updateRecentBroadcasts(tx, tx.hash, cache)
-      expect(broadcasts.length).to.equal(1)
-      expect(cache.get('broadcasts://recent')[0].txid).to.equal(tx.hash)
-      expect(broadcasts[0].tx).to.equal(tx)
-    })
-
-    // ------------------------------------------------------------------------
-
-    it('does not add duplicate', () => {
-      const broadcasts = []
-      const tx = new bsv.Transaction()
-      tx.to(new bsv.PrivateKey().toAddress(), 100)
-      _addToBroadcastCache(broadcasts, 1000, tx.hash, tx)
-      _addToBroadcastCache(broadcasts, 1000, tx.hash, tx)
-      expect(broadcasts.length).to.equal(1)
-      expect(broadcasts[0].txid).to.equal(tx.hash)
-      expect(broadcasts[0].tx).to.equal(tx)
+      const address = new bsv.PrivateKey().toAddress()
+      const script = bsv.Script.fromAddress(address).toHex()
+      const satoshis = 100
+      tx.to(address, satoshis)
+      const prevTxId = '1111111111111111111111111111111111111111111111111111111111111111'
+      tx.from({ txid: prevTxId, vout: 123, script: 'abc', satoshis: 456 })
+      const date = Date.now()
+      await RecentBroadcasts._addToCache(cache, tx, tx.hash)
+      const recentBroadcasts = cache.get('broadcasts://recent')
+      expect(recentBroadcasts.length).to.equal(1)
+      expect(recentBroadcasts[0].time >= date).to.equal(true)
+      expect(recentBroadcasts[0].inputs).to.deep.equal([{ txid: prevTxId, vout: 123 }])
+      expect(recentBroadcasts[0].outputs).to.deep.equal([{ txid: tx.hash, vout: 0, script, satoshis }])
     })
 
     // ------------------------------------------------------------------------
 
     it('filters expired', async () => {
-      const broadcasts = []
+      const cache = new Map()
       const tx1 = new bsv.Transaction()
       tx1.to(new bsv.PrivateKey().toAddress(), 100)
-      _addToBroadcastCache(broadcasts, 10, tx1.hash, tx1)
+      await RecentBroadcasts._addToCache(cache, tx1, tx1.hash)
       await new Promise((resolve, reject) => setTimeout(resolve, 100))
       const tx2 = new bsv.Transaction()
       tx2.to(new bsv.PrivateKey().toAddress(), 100)
-      _addToBroadcastCache(broadcasts, 10, tx2.hash, tx2)
-      expect(broadcasts.length).to.equal(1)
-      expect(broadcasts[0].txid).to.equal(tx2.hash)
-      expect(broadcasts[0].tx).to.equal(tx2)
+      await RecentBroadcasts._addToCache(cache, tx2, tx2.hash, 10)
+      const recentBroadcasts = cache.get('broadcasts://recent')
+      expect(recentBroadcasts.length).to.equal(1)
+      expect(recentBroadcasts[0].outputs[0].txid).to.equal(tx2.hash)
     })
-    */
   })
 
   // ----------------------------------------------------------------------------------------------
-  // _correctServerUtxos
+  // _correctUtxosUsingCache
   // ----------------------------------------------------------------------------------------------
 
-  describe('_correctServerUtxos', () => {
-    /*
-    it('removes spent utxos', () => {
-      const broadcasts = []
+  describe('_correctUtxosUsingCache', () => {
+    it('removes spent utxos', async () => {
+      const cache = new Map()
       const address1 = new bsv.PrivateKey().toAddress()
       const address2 = new bsv.PrivateKey().toAddress()
       const tx1 = new bsv.Transaction()
       tx1.to(address1, 100)
-      const utxos = [{ txid: tx1.hash, vout: 0, script: tx1.outputs[0].script, satoshis: tx1.outputs[0].satoshis }]
+      const utxos = [{ txid: tx1.hash, vout: 0, script: tx1.outputs[0].script.toHex(), satoshis: tx1.outputs[0].satoshis }]
       const tx2 = new bsv.Transaction()
       tx2.from(utxos[0])
       tx2.to(address2, 200)
-      _addToBroadcastCache(broadcasts, 100, tx2.hash, tx2)
-      const utxos2 = _updateUtxosWithBroadcasts(broadcasts, 1000, utxos, new bsv.Script(address1))
-      expect(utxos2.length).to.equal(0)
+      await RecentBroadcasts._addToCache(cache, tx2, tx2.hash)
+      await RecentBroadcasts._correctUtxosUsingCache(cache, utxos, bsv.Script.fromAddress(address1).toHex())
+      expect(utxos.length).to.equal(0)
     })
 
     // ------------------------------------------------------------------------
 
-    it('adds unspent utxos in broadcasts', () => {
+    it('adds unspent utxos', async () => {
+      const cache = new Map()
       const utxos = []
-      const broadcasts = []
       const address = new bsv.PrivateKey().toAddress()
       const tx = new bsv.Transaction()
       tx.to(address, 100)
-      _addToBroadcastCache(broadcasts, 100, tx.hash, tx)
-      const utxos2 = _updateUtxosWithBroadcasts(broadcasts, 1000, utxos, new bsv.Script(address))
-      expect(utxos2.length).to.equal(1)
+      await RecentBroadcasts._addToCache(cache, tx, tx.hash)
+      await RecentBroadcasts._correctUtxosUsingCache(cache, utxos, bsv.Script.fromAddress(address).toHex())
+      expect(utxos.length).to.equal(1)
     })
 
     // ------------------------------------------------------------------------
 
-    it('does not add spent utxos in broadcasts', () => {
-      const broadcasts = []
+    it('does not add spent utxos', async () => {
+      const cache = new Map()
       const address = new bsv.PrivateKey().toAddress()
       const tx1 = new bsv.Transaction()
       tx1.to(address, 100)
-      const utxos = [{ txid: tx1.hash, vout: 0, script: tx1.outputs[0].script, satoshis: tx1.outputs[0].satoshis }]
+      const utxos = [{ txid: tx1.hash, vout: 0, script: tx1.outputs[0].script.toHex(), satoshis: tx1.outputs[0].satoshis }]
       const tx2 = new bsv.Transaction()
       tx2.from(utxos[0])
       tx2.to(address, 200)
-      _addToBroadcastCache(broadcasts, 100, tx2.hash, tx2)
-      const utxos2 = _updateUtxosWithBroadcasts(broadcasts, 1000, utxos, new bsv.Script(address))
-      expect(utxos2.length).to.equal(1)
+      await RecentBroadcasts._addToCache(cache, tx2, tx2.hash)
+      await RecentBroadcasts._correctUtxosUsingCache(cache, utxos, bsv.Script.fromAddress(address).toHex())
+      expect(utxos.length).to.equal(1)
     })
 
     // ------------------------------------------------------------------------
 
     it('filters expired', async () => {
+      const cache = new Map()
       const utxos = []
-      const broadcasts = []
       const address = new bsv.PrivateKey().toAddress()
       const tx = new bsv.Transaction()
       tx.to(address, 100)
-      _addToBroadcastCache(broadcasts, 10, tx.hash, tx)
+      await RecentBroadcasts._addToCache(cache, tx, tx.hash)
       await new Promise((resolve, reject) => setTimeout(resolve, 100))
-      const utxos2 = _updateUtxosWithBroadcasts(broadcasts, 10, utxos, new bsv.Script(address))
-      expect(utxos2.length).to.equal(0)
+      await RecentBroadcasts._correctUtxosUsingCache(cache, utxos, bsv.Script.fromAddress(address).toHex(), 10)
+      expect(utxos.length).to.equal(0)
     })
-    */
   })
 })
 
