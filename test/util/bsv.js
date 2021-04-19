bed36e55b74c1ecc30f596c7e1ff40af9afe5bca
Brenton Gunning <brentongunning@gmail.com>
2021-04-19 11:57:57 -0600

    Improve _scripthash api and performance

--- test/util/bsv.js
+++ test/util/bsv.js
@@ -4,11 +4,12 @@
  * Tests for lib/util/bsv.js
  */
 
+const bsv = require('bsv')
 const Run = require('../env/run')
 const unmangle = require('../env/unmangle')
 const { describe, it } = require('mocha')
 const { expect } = require('chai')
-const { _calculateDust, _dedupUtxos } = unmangle(unmangle(Run)._bsv)
+const { _calculateDust, _scripthash, _dedupUtxos } = unmangle(unmangle(Run)._bsv)
 
 // ------------------------------------------------------------------------------------------------
 // bsv
@@ -34,6 +35,34 @@ describe('bsv', () => {
 
     it('custom script', () => {
       expect(_calculateDust(1000, 1000)).to.equal(3477)
+    })
+  })
+
+  // ----------------------------------------------------------------------------------------------
+  // _scripthash
+  // ----------------------------------------------------------------------------------------------
+
+  describe('_scripthash', () => {
+    it('calculates script hash', async () => {
+      const script = bsv.Script.fromAddress('1Kc8XRNryDycwvfEQiFF2TZwD1CVhgwGy2').toHex()
+      const scripthash = await _scripthash(script)
+      expect(scripthash).to.equal('e3cc7609f70142ba04cc5af3ca5b189e75abe0c48c1623b49535dd28f32e530e')
+    })
+
+    // ------------------------------------------------------------------------
+
+    it('caches result', async () => {
+      const script = new bsv.PrivateKey().toAddress().toHex()
+      const t0 = new Date()
+      await _scripthash(script)
+      const t1 = new Date()
+      const first = t1 - t0
+      for (let i = 0; i < 100; i++) {
+        await _scripthash(script)
+      }
+      const t2 = new Date()
+      const subsequent = (t2 - t1) / 100
+      expect(subsequent < first)
     })
   })
 
