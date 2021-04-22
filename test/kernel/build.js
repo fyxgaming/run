eb4412ac3123277c4a9c200d11ab0d765c7b4923
Brenton Gunning <brentongunning@gmail.com>
2021-04-22 17:25:49 -0600

    Create placeholder build tests

--- test/kernel/build.js
+++ test/kernel/build.js
@@ -4,68 +4,156 @@
  * Tests that check properties of the transactions RUN builds
  */
 
-const { describe } = require('mocha')
+const { describe, it } = require('mocha')
 
 // ------------------------------------------------------------------------------------------------
 // Build
 // ------------------------------------------------------------------------------------------------
 
 describe('Build', () => {
-  // TODO
+  // --------------------------------------------------------------------------
+  // scripts
+  // --------------------------------------------------------------------------
 
-  /*
-  // export & publish
-  it('should correctly set owners on code and jig outputs', async () => {
-    const run = hookRun(new Run())
-    const address = new bsv.PrivateKey().toAddress()
-    class A extends Jig { f (owner) { this.owner = owner } }
-    const a = new A()
-    await a.sync()
-    expect(tx.outputs[1].script.toAddress().toString()).to.equal(run.owner.address)
-    expect(tx.outputs[2].script.toAddress().toString()).to.equal(run.owner.address)
-    a.f(address.toString())
-    await a.sync()
-    expect(tx.outputs[1].script.toAddress().toString()).to.equal(address.toString())
+  describe('output scripts', () => {
+    it.skip('output scripts are correct for address owners', () => {
+    // TODO - jig and code
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('output scripts are correct for pubkey owners', () => {
+    // TODO - jig
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('output scripts are correct for custom locks', () => {
+    // TODO - jig
+    })
   })
 
-  // export & publish
-  it('should correctly set satoshis on code and jig outputs', async () => {
-    const run = hookRun(new Run())
-    class A extends Jig { f (satoshis) { this.satoshis = satoshis } }
-    const a = new A()
-    await a.sync()
-    expect(tx.outputs[1].satoshis).to.equal(bsv.Transaction.DUST_AMOUNT)
-    expect(tx.outputs[2].satoshis).to.equal(bsv.Transaction.DUST_AMOUNT)
-    a.f(1)
-    await a.sync()
-    expect(tx.outputs[1].satoshis).to.equal(bsv.Transaction.DUST_AMOUNT)
-    a.f(0)
-    await a.sync()
-    expect(tx.outputs[1].satoshis).to.equal(bsv.Transaction.DUST_AMOUNT)
-    a.f(bsv.Transaction.DUST_AMOUNT)
-    await a.sync()
-    expect(tx.outputs[1].satoshis).to.equal(bsv.Transaction.DUST_AMOUNT)
-    a.f(bsv.Transaction.DUST_AMOUNT + 1)
-    await a.sync()
-    expect(tx.outputs[1].satoshis).to.equal(bsv.Transaction.DUST_AMOUNT + 1)
-    run.blockchain.fund(run.purse.address, 300000000)
-    run.transaction.begin()
-    new A().f(1000)
-    a.f(100000000)
-    run.transaction.end()
-    await run.sync()
-    expect(tx.outputs[1].satoshis).to.equal(1000)
-    expect(tx.outputs[2].satoshis).to.equal(100000000)
+  // --------------------------------------------------------------------------
+  // satoshis
+  // --------------------------------------------------------------------------
+
+  describe('satoshis', () => {
+    it.skip('output satoshis are correct for 0', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('output satoshis are correct for below dust', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('output satoshis are correct for above dust', () => {
+      // TODO
+    })
   })
 
-  // export & publish
-  it('should store custom app name', async () => {
-    const run = hookRun(new Run({ app: 'biz' }))
-    class A extends Jig { }
-    await run.deploy(A)
-    expect(tx.outputs[0].script.chunks[4].buf.toString('utf8')).to.equal('biz')
+  // --------------------------------------------------------------------------
+  // custom base
+  // --------------------------------------------------------------------------
+
+  describe('custom base', () => {
+    it.skip('output scripts are correct for custom base', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('output satoshis are correct for custom base', () => {
+      // TODO
+    })
   })
-  */
+
+  // --------------------------------------------------------------------------
+  // app name
+  // --------------------------------------------------------------------------
+
+  describe('app name', () => {
+    it.skip('utf8 app name is correctly set', () => {
+      // TODO
+      /*
+      const run = hookRun(new Run({ app: 'biz' }))
+      class A extends Jig { }
+      await run.deploy(A)
+      expect(tx.outputs[0].script.chunks[4].buf.toString('utf8')).to.equal('biz')
+      */
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('empty app name is correctly set', () => {
+      // TODO
+      /*
+      const run = hookRun(new Run({ app: 'biz' }))
+      class A extends Jig { }
+      await run.deploy(A)
+      expect(tx.outputs[0].script.chunks[4].buf.toString('utf8')).to.equal('biz')
+      */
+    })
+  })
+
+  // --------------------------------------------------------------------------
+  // prefix
+  // --------------------------------------------------------------------------
+
+  describe('prefix', () => {
+    it.skip('has run tag', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('has version', () => {
+      // TODO
+    })
+  })
+
+  // --------------------------------------------------------------------------
+  // metadata
+  // --------------------------------------------------------------------------
+
+  describe('metadata', () => {
+    it.skip('deploy', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('instantiate from ref', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('call method with berry ref', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('destroy code', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('auth jig', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('multiple actions', () => {
+      // TODO
+    })
+  })
 })
 
 // ------------------------------------------------------------------------------------------------
