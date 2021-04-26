d912769090ef29362b99fdacfd0c3c8e67653863
Brenton Gunning <brentongunning@gmail.com>
2021-04-26 17:15:08 -0600

    Test: invalid - call - throws if missing method name data

--- test/kernel/invalid.js
+++ test/kernel/invalid.js
@@ -645,8 +645,16 @@ describe('Invalid', () => {
 
     // ------------------------------------------------------------------------
 
-    it.skip('throws if missing method name data', () => {
-      // TODO
+    it('throws if missing method name data', async () => {
+      const run = new Run()
+      const deployConfig = buildDeployConfig()
+      const deployRawtx = createRunTransaction(deployConfig)
+      const deployTxid = new bsv.Transaction(deployRawtx).hash
+      run.blockchain.fetch = txid => txid === deployTxid ? deployRawtx : undefined
+      const callConfig = buildCallConfig(deployRawtx)
+      callConfig.metadata.exec[0].data[1] = null
+      const callRawtx = createRunTransaction(callConfig)
+      await expect(run.import(callRawtx)).to.be.rejectedWith('CALL method must be a string: null')
     })
 
     // ------------------------------------------------------------------------
