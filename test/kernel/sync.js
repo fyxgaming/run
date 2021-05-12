0176c1b7bcaf09ef5c87762e35b058c3d6441650
Brenton Gunning <brentongunning@gmail.com>
2021-05-12 16:23:53 -0600

    Move old syncer tests over to syncer.js

--- test/kernel/sync.js
+++ test/kernel/sync.js
@@ -434,6 +434,73 @@ describe('Sync', () => {
       await expect(a2.sync()).not.to.be.rejected
     })
   })
+
+  // TODO
+  /*
+  describe('sync', () => {
+    it('should broadcast transactions', async () => {
+      const run = new Run()
+      spy(run.blockchain)
+      class A extends Jig { }
+      const a = new A()
+      await a.sync()
+      expect(run.blockchain.broadcast.callCount).to.equal(1)
+    })
+
+    it('should not force-fetch transactions just broadcasted', async () => {
+      const run = new Run()
+      spy(run.blockchain)
+      class A extends Jig { }
+      const a = new A()
+      await a.sync()
+      const txid = a.location.slice(0, 64)
+      const vout = parseInt(a.location.slice(66))
+      expect(run.blockchain.spends.calledWith(txid, vout)).to.equal(true)
+    })
+
+    it('should not force-fetch transactions just broadcasted (payment channels)', async () => {
+      const run = new Run()
+      spy(run.blockchain)
+      class A extends Jig { set (n) { this.n = n } }
+      const a = new A()
+      await run.sync()
+
+      run.transaction.begin()
+      a.set(1)
+      await run.transaction.pay()
+      await run.transaction.sign()
+      const tx = run.transaction.export()
+      run.transaction.rollback()
+
+      await run.transaction.import(tx)
+      const jig = run.transaction.actions[0].target
+      run.transaction.end()
+      await jig.sync()
+
+      const txid = jig.location.slice(0, 64)
+      const vout = parseInt(jig.location.slice(66))
+      expect(run.blockchain.spends.calledWith(txid, vout)).to.equal(true)
+    })
+
+    it('should not fetch transactions just broadcasted when forward = false', async () => {
+      const run = new Run()
+      spy(run.blockchain)
+      class A extends Jig { }
+      const a = new A()
+      await a.sync({ forward: false })
+      const txid = a.location.slice(0, 64)
+      expect(run.blockchain.fetch.calledWith(txid, false)).to.equal(false)
+    })
+
+    it('should return immediately when nothing is queued', async () => {
+      const run = new Run()
+      await run.sync()
+      run.deploy(class A {})
+      await run.sync()
+      await run.sync()
+    })
+  })
+  */
 })
 
 // ------------------------------------------------------------------------------------------------
