3ec16188b0127fc92ad4707e5251200cfc675dc1
Brenton Gunning <brentongunning@gmail.com>
2021-09-13 13:40:57 -0600

    Correct utxos in wrapper layer

--- test/plugins/whatsonchain.js
+++ test/plugins/whatsonchain.js
@@ -8,7 +8,6 @@ const { describe, it } = require('mocha')
 const { expect } = require('chai')
 const Run = require('../env/run')
 const { WhatsOnChain } = Run.plugins
-const { Cache } = Run.api
 
 // ------------------------------------------------------------------------------------------------
 // WhatsOnChain
@@ -23,7 +22,6 @@ describe('WhatsOnChain', () => {
     it('with defaults', () => {
       const connect = new WhatsOnChain()
       expect(connect.network).to.equal('main')
-      expect(connect.cache instanceof Cache).to.equal(true)
       expect(connect.api).to.equal('whatsonchain')
     })
 
@@ -36,18 +34,6 @@ describe('WhatsOnChain', () => {
       expect(testnet.network).to.equal('test')
       const stn = new WhatsOnChain({ network: 'stn' })
       expect(stn.network).to.equal('stn')
-    })
-
-    // --------------------------------------------------------------------------------------------
-
-    it('with custom cache', () => {
-      class CustomCache {
-        async set (key, value) { }
-        async get (key) { }
-      }
-      const cache = new CustomCache()
-      const connect = new WhatsOnChain({ cache })
-      expect(connect.cache).to.equal(cache)
     })
 
     // --------------------------------------------------------------------------------------------
@@ -78,15 +64,6 @@ describe('WhatsOnChain', () => {
     it('throws if invalid network', () => {
       expect(() => new WhatsOnChain({ network: null })).to.throw('Invalid network: null')
       expect(() => new WhatsOnChain({ network: 0 })).to.throw('Invalid network: 0')
-    })
-
-    // --------------------------------------------------------------------------------------------
-
-    it('throws if invalid cache', () => {
-      expect(() => new WhatsOnChain({ cache: '' })).to.throw('Unsupported cache: ')
-      expect(() => new WhatsOnChain({ cache: null })).to.throw('Unsupported cache: null')
-      expect(() => new WhatsOnChain({ cache: {} })).to.throw('Unsupported cache: [object Object]')
-      expect(() => new WhatsOnChain({ cache: { set: () => { } } })).to.throw('Unsupported cache: [object Object]')
     })
   })
 })
