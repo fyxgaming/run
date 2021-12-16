575eb75dd7e04cf34d56b16c8e783b4e1643d5fc
Brenton Gunning <brentongunning@gmail.com>
2021-12-15 23:37:52 -0700

    throws if sign returns invalid tx

--- test/kernel/owner-api.js
+++ test/kernel/owner-api.js
@@ -5,7 +5,11 @@
  */
 
 const { describe, it, afterEach } = require('mocha')
+const { expect } = require('chai')
+require('chai').use(require('chai-as-promised'))
 const Run = require('../env/run')
+const { Jig } = Run
+const bsv = require('bsv')
 
 // ------------------------------------------------------------------------------------------------
 // Owner API
@@ -22,8 +26,24 @@ describe('Owner API', () => {
   // --------------------------------------------------------------------------
 
   describe('sign', () => {
-    it.skip('throws if sign returns invalid tx', () => {
-      // TODO
+    it('throws if sign returns invalid tx', async () => {
+      const run = new Run()
+      class A extends Jig {}
+      run.deploy(A)
+      await run.sync()
+      async function testFail (f) {
+        run.owner.sign = f
+        const error = 'Invalid raw transaction returned by owner'
+        const tx = new Run.Transaction()
+        tx.update(() => new A())
+        await expect(tx.sign()).to.be.rejectedWith(error)
+      }
+      await testFail(() => undefined)
+      await testFail(() => null)
+      await testFail(() => new bsv.Transaction())
+      await testFail(() => true)
+      await testFail(() => 'abc')
+      await testFail(rawtx => new bsv.Transaction(rawtx))
     })
 
     // ------------------------------------------------------------------------
