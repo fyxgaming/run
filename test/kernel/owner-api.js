3e5a1bffe6b13fa2a8b32e0549f18277641e788c
Brenton Gunning <brentongunning@gmail.com>
2021-12-15 23:33:57 -0700

    Add test file structure

--- test/kernel/owner-api.js
+++ test/kernel/owner-api.js
@@ -4,15 +4,43 @@
  * Tests for the Owner plugin
  */
 
-const { describe, it } = require('mocha')
+const { describe, it, afterEach } = require('mocha')
+const Run = require('../env/run')
 
 // ------------------------------------------------------------------------------------------------
 // Owner API
 // ------------------------------------------------------------------------------------------------
 
 describe('Owner API', () => {
-  it('test', () => {
-    // TODO
+  // Wait for every test to finish. This makes debugging easier.
+  afterEach(() => Run.instance && Run.instance.sync())
+  // Deactivate the current run instance. This stops leaks across tests.
+  afterEach(() => Run.instance && Run.instance.deactivate())
+
+  // --------------------------------------------------------------------------
+  // sign
+  // --------------------------------------------------------------------------
+
+  describe('sign', () => {
+    it.skip('throws if sign returns invalid tx', () => {
+      // TODO
+    })
+
+    // ------------------------------------------------------------------------
+
+    it.skip('throws if sign returns different tx', () => {
+      // TODO
+    })
+
+    // TODO - Add tests
+  })
+
+  // --------------------------------------------------------------------------
+  // nextOwner
+  // --------------------------------------------------------------------------
+
+  describe('nextOwner', () => {
+    // TODO - Add tests
   })
 })
 
