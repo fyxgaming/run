2e577902dd37ac98a379251f002a7a17dbcf3983
Brenton Gunning <brentongunning@gmail.com>
2022-05-07 19:09:34 -0600

    Fix failing wrapper.send() in test case

--- test/plugins/purse-wrapper.js
+++ test/plugins/purse-wrapper.js
@@ -81,10 +81,8 @@ describe('PurseWrapper', () => {
       await wrapper.pay(rawtx, parents)
       await wrapper.broadcast(rawtx)
       await wrapper.cancel(rawtx)
-      await wrapper.send(address, 100)
       purse.broadcast.throws(new Error('abc'))
       purse.cancel.throws(new Error())
-      await expect(wrapper.send(address, 100)).to.be.rejectedWith('abc')
     })
   })
 
