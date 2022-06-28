dc0e0eb2529cd1ee8c55f706343faa5cf9c5178a
Brenton Gunning <brentongunning@gmail.com>
2022-06-28 16:21:09 -0600

    Build tests

--- test/kernel/build.js
+++ test/kernel/build.js
@@ -9,7 +9,7 @@ const { expect } = require('chai')
 const bsv = require('bsv')
 const Run = require('../env/run')
 const unmangle = require('../env/unmangle')
-const { getExtrasBlockchain } = require('../env/misc')
+const { createExtrasRun } = require('../env/misc')
 const { Jig, Berry } = Run
 const { asm } = Run.extra
 const { _calculateDust } = unmangle(unmangle(Run)._bsv)
@@ -66,13 +66,13 @@ describe('Build', () => {
     // ------------------------------------------------------------------------
 
     it('custom scripts for custom locks', async () => {
-      const run = new Run({ blockchain: await getExtrasBlockchain() })
+      const run = await createExtrasRun()
       class A extends Jig { static send (owner) { this.owner = owner } }
       class L {
         script () { return asm('OP_1 abcd') }
         domain () { return 100 }
       }
-      L.deps = { asm: Run.extra.asm }
+      L.deps = { asm: Run.extra.test.asm }
       const CA = run.deploy(A)
       await CA.sync()
       const tx = new Run.Transaction()
